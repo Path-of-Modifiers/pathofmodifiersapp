f0d87ae03ae8553d284bd43056cf620453b13c6e
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-01-18 17:20:20 +0100

    Added postgresql database, pydantic data validation with examples of data, docker setup, FastAPI setup config for  setup files

---
+++ fastapi-postgres-docker/database.py
@@ -0,0 +1,12 @@
+import sqlalchemy as _sql
+import sqlalchemy.ext.declarative as _declarative
+import sqlalchemy.orm as _orm
+
+
+DATABASE_URL = "postgresql://pathofmodifiersdbadmin:sjukebarna123@localhost/pathofmodifiers_db"
+
+engine = _sql.create_engine(DATABASE_URL)
+
+SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = _declarative.declarative_base()
