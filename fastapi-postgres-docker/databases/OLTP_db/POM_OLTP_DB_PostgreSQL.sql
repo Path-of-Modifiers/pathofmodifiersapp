ebc6f899c0bf2edef1cd27c4bd7bb95dbb3b1a02
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-01-26 15:22:55 +0100

    26 Made some updates to the db for correct formatting

--- fastapi-postgres-docker/databases/OLTP_db/POM_OLTP_DB_PostgreSQL.sql
+++ fastapi-postgres-docker/databases/OLTP_db/POM_OLTP_DB_PostgreSQL.sql
@@ -1,14 +1,13 @@
 CREATE TABLE "Currency" (
-  "currency_name" varchar PRIMARY KEY, -- Self generated
+  "currency_name" varchar PRIMARY KEY,
   "value_in_chaos" float NOT NULL,
   "icon_url" varchar NOT NULL,
   "created_at" datetime,
   "updated_at" datetime
-  FOREIGN KEY (icon_url) REFERENCES Icon(icon_url) ON DELETE SET NULL
 );
 
 CREATE TABLE "Item" (
-  "item_id" varchar PRIMARY KEY, -- Field "id" in Item POE API object
+  "item_id" varchar PRIMARY KEY, 
   "stash_id" varchar NOT NULL,
   "name" varchar,
   "icon_url" varchar,
@@ -17,7 +16,7 @@ CREATE TABLE "Item" (
   "type_line" varchar NOT NULL,
   "rarity" varchar NOT NULL,
   "identified" boolean NOT NULL,
-  "item_level" tinyint NOT NULL,
+  "item_level" smallint NOT NULL,
   "forum_note" varchar,
   "currency_amount" float(24),
   "currency_name" varchar,
@@ -32,19 +31,18 @@ CREATE TABLE "Item" (
   "tangled" boolean,
   "influences" jsonb,
   "is_relic" boolean,
-  "prefixes" tinyint,
-  "suffixes" tinyint,
+  "prefixes" smallint,
+  "suffixes" smallint,
   "foil_variation" int,
   "inventory_id" varchar,
   "created_at" datetime,
   "updated_at" datetime,
-  FOREIGN KEY (icon_url) REFERENCES Icon(icon_url) ON DELETE SET NULL,
   FOREIGN KEY (currency_name) REFERENCES Currency(currency_name) ON DELETE RESTRICT,
   FOREIGN KEY (stash_id) REFERENCES Stash(stash_id) ON DELETE CASCADE
 );
 
 CREATE TABLE "Transaction" (
-  "transaction_id" serial PRIMARY KEY, -- Self generated
+  "transaction_id" serial PRIMARY KEY, 
   "item_id" varchar NOT NULL,
   "account_name" varchar NOT NULL,
   "currency_amount" float(24) NOT NULL,
@@ -66,18 +64,18 @@ CREATE TABLE "Item_Categories" (
 
 CREATE TABLE "Item_Modifiers" (
   "modifier_id" varchar PRIMARY KEY,
-  "item_id" varchar PRIMARY KEY
+  "item_id" varchar PRIMARY KEY,
   FOREIGN KEY (modifier_id) REFERENCES Modifier(modifier_id) ON DELETE CASCADE,
   FOREIGN KEY (item_id) REFERENCES Item(item_id) ON DELETE CASCADE
 );
 
 CREATE TABLE "Category" (
-  "category_name" varchar PRIMARY KEY, -- Field "extended" -> "category" and "subcategory" in Item POE API object
+  "category_name" varchar PRIMARY KEY, 
   "is_sub_category" boolean
 );
 
 CREATE TABLE "Modifier" (
-  "modifier_id" varchar PRIMARY KEY, -- Self generated with Modifier.effect for sensible uniqueness
+  "modifier_id" varchar PRIMARY KEY, 
   "effect" varchar NOT NULL,
   "implicit" boolean,
   "explicit" boolean,
@@ -99,24 +97,24 @@ CREATE TABLE "Modifier_Stats" (
 );
 
 CREATE TABLE "Stat" (
-  "stat_id" varchar PRIMARY KEY, -- Generated with "local_" + "_" replaced for whitespace in Modifier.effect for sensible uniqueness
-  "position" tinyint NOT NULL PRIMARY KEY, -- If it's the first/second/third... number in the Modifier.effect text
-  "stat_value" smallint NOT NULL, -- Value of the # in Modifier.effect text
+  "stat_id" varchar PRIMARY KEY, 
+  "position" smallint NOT NULL PRIMARY KEY, 
+  "stat_value" smallint NOT NULL, 
   "mininum_value" smallint,
   "maximum_value" smallint,
-  "stat_tier" smallint, -- Tier 1 is highest tier 
+  "stat_tier" smallint, 
   "created_at" datetime,
   "updated_at" datetime
 );
 
 CREATE TABLE "Stash" (
-  "stash_id" varchar PRIMARY KEY, -- Field "id" in PublicStashChange POE API object
+  "stash_id" varchar PRIMARY KEY, 
   "account_name" varchar NOT NULL,
   "public" boolean NOT NULL,
   "league" varchar NOT NULL,
   "created_at" datetime,
   "updated_at" datetime,
-  FOREIGN KEY (account_name) REFERENCES Account(account_name) ON DELETE CASCADE,
+  FOREIGN KEY (account_name) REFERENCES Account(account_name) ON DELETE CASCADE
 );
 
 CREATE TABLE "Account" (
@@ -125,6 +123,3 @@ CREATE TABLE "Account" (
   "created_at" datetime,
   "updated_at" datetime
 );
-
-INSERT INTO Currency (currency_name, value_in_chaos, icon_url) VALUES ('Chaos Orb', 1, 'https://web.poecdn.com/image/Art/2DItems/Currency/CurrencyRerollRare.png');
-
