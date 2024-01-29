fbace92331fc760e7577b8e755968e0530b38cd2
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-01-29 16:41:04 +0100

    26 Added models and schemas for POM OLTP database. Not done yet

--- fastapi-postgres-docker/databases/OLTP_db/models.py
+++ fastapi-postgres-docker/databases/OLTP_db/models.py
@@ -1,147 +1,148 @@
 import database as _database
+import sqlalchemy as _sql
 from sqlalchemy.dialects.postgresql import JSONB
 
 class Currency(_database.Base):
 
     __tablename__ = 'Currency'
 
-    currency_name = _database.Column(_database.String(), primary_key=True)
-    value_in_chaos = _database.Column(_database.Float(), nullable=False)
-    icon_url = _database.Column(_database.String(), nullable=False)
-    created_at = _database.Column(_database.DateTime())
-    updated_at = _database.Column(_database.DateTime())
+    currency_name = _sql.Column(_sql.String(), primary_key=True)
+    value_in_chaos = _sql.Column(_sql.Float(), nullable=False)
+    icon_url = _sql.Column(_sql.String(), nullable=False)
+    created_at = _sql.Column(_sql.DateTime())
+    updated_at = _sql.Column(_sql.DateTime())
 
 
 class Item(_database.Base):
 
     __tablename__ = 'Item'
 
-    item_id = _database.Column(_database.String(), primary_key=True)
-    stash_id = _database.Column(_database.String(), _database.ForeignKey('Stash.stash_id'), ondelete="CASCADE", nullable=False)
-    name = _database.Column(_database.String())
-    icon_url = _database.Column(_database.String())
-    league = _database.Column(_database.String(), nullable=False)
-    base_type = _database.Column(_database.String(), nullable=False)
-    type_line = _database.Column(_database.String(), nullable=False)
-    rarity = _database.Column(_database.String(), nullable=False)
-    identified = _database.Column(_database.Boolean(), nullable=False)
-    item_level = _database.Column(_database.SmallInteger(), nullable=False)
-    forum_note = _database.Column(_database.String())
-    currency_amount = _database.Column(_database.Float(24))
-    currency_name = _database.Column(_database.String(), _database.ForeignKey('Currency.currency_name'), ondelete="RESTRICT")
-    corrupted = _database.Column(_database.Boolean())
-    delve = _database.Column(_database.Boolean())
-    fractured = _database.Column(_database.Boolean())
-    synthesized = _database.Column(_database.Boolean())
-    replica = _database.Column(_database.Boolean())
-    elder = _database.Column(_database.Boolean())
-    shaper = _database.Column(_database.Boolean())
-    searing = _database.Column(_database.Boolean())
-    tangled = _database.Column(_database.Boolean())
-    influences = _database.Column(JSONB())
-    is_relic = _database.Column(_database.Boolean())
-    prefixes = _database.Column(_database.SmallInteger())
-    suffixes = _database.Column(_database.SmallInteger())
-    foil_variation = _database.Column(_database.Integer())
-    inventory_id = _database.Column(_database.String())
-    created_at = _database.Column(_database.DateTime())
-    updated_at = _database.Column(_database.DateTime())
+    item_id = _sql.Column(_sql.String(), primary_key=True)
+    stash_id = _sql.Column(_sql.String(), _sql.ForeignKey('Stash.stash_id'), ondelete="CASCADE", nullable=False)
+    name = _sql.Column(_sql.String())
+    icon_url = _sql.Column(_sql.String())
+    league = _sql.Column(_sql.String(), nullable=False)
+    base_type = _sql.Column(_sql.String(), nullable=False)
+    type_line = _sql.Column(_sql.String(), nullable=False)
+    rarity = _sql.Column(_sql.String(), nullable=False)
+    identified = _sql.Column(_sql.Boolean(), nullable=False)
+    item_level = _sql.Column(_sql.SmallInteger(), nullable=False)
+    forum_note = _sql.Column(_sql.String())
+    currency_amount = _sql.Column(_sql.Float(24))
+    currency_name = _sql.Column(_sql.String(), _sql.ForeignKey('Currency.currency_name'), ondelete="RESTRICT")
+    corrupted = _sql.Column(_sql.Boolean())
+    delve = _sql.Column(_sql.Boolean())
+    fractured = _sql.Column(_sql.Boolean())
+    synthesized = _sql.Column(_sql.Boolean())
+    replica = _sql.Column(_sql.Boolean())
+    elder = _sql.Column(_sql.Boolean())
+    shaper = _sql.Column(_sql.Boolean())
+    searing = _sql.Column(_sql.Boolean())
+    tangled = _sql.Column(_sql.Boolean())
+    influences = _sql.Column(JSONB())
+    is_relic = _sql.Column(_sql.Boolean())
+    prefixes = _sql.Column(_sql.SmallInteger())
+    suffixes = _sql.Column(_sql.SmallInteger())
+    foil_variation = _sql.Column(_sql.Integer())
+    inventory_id = _sql.Column(_sql.String())
+    created_at = _sql.Column(_sql.DateTime())
+    updated_at = _sql.Column(_sql.DateTime())
 
 
 class Transaction(_database.Base):
 
     __tablename__ = 'Transaction'
 
-    transaction_id = _database.Column(_database.Integer(), autoincrement=True, primary_key=True)
-    item_id = _database.Column(_database.String(), _database.ForeignKey('Item.item_id'), ondelete="CASCADE", nullable=False)
-    account_name = _database.Column(_database.String(), _database.ForeignKey('Account.account_name'), ondelete="CASCADE", nullable=False)
-    currency_amount = _database.Column(_database.Float(24), nullable=False)
-    currency_name = _database.Column(_database.String(), _database.ForeignKey('Currency.currency_name'), ondelete="RESTRICT", nullable=False)
-    created_at = _database.Column(_database.DateTime())
-    updated_at = _database.Column(_database.DateTime())
+    transaction_id = _sql.Column(_sql.Integer(), autoincrement=True, primary_key=True)
+    item_id = _sql.Column(_sql.String(), _sql.ForeignKey('Item.item_id'), ondelete="CASCADE", nullable=False)
+    account_name = _sql.Column(_sql.String(), _sql.ForeignKey('Account.account_name'), ondelete="CASCADE", nullable=False)
+    currency_amount = _sql.Column(_sql.Float(24), nullable=False)
+    currency_name = _sql.Column(_sql.String(), _sql.ForeignKey('Currency.currency_name'), ondelete="RESTRICT", nullable=False)
+    created_at = _sql.Column(_sql.DateTime())
+    updated_at = _sql.Column(_sql.DateTime())
 
 
 class ItemCategories(_database.Base):
 
     __tablename__ = 'Item_Categories'
 
-    category_name = _database.Column(_database.String(), _database.ForeignKey('Category.category_name'), ondelete="CASCADE", primary_key=True)
-    item_id = _database.Column(_database.String(), _database.ForeignKey('Item.item_id'), ondelete="CASCADE", primary_key=True)
+    category_name = _sql.Column(_sql.String(), _sql.ForeignKey('Category.category_name'), ondelete="CASCADE", primary_key=True)
+    item_id = _sql.Column(_sql.String(), _sql.ForeignKey('Item.item_id'), ondelete="CASCADE", primary_key=True)
 
 
 class ItemModifiers(_database.Base):
 
     __tablename__ = 'Item_Modifiers'
 
-    modifier_id = _database.Column(_database.String(), _database.ForeignKey('Modifier.modifier_id'), ondelete="CASCADE", primary_key=True)
-    item_id = _database.Column(_database.String(), _database.ForeignKey('Item.item_id'), ondelete="CASCADE", primary_key=True)
+    modifier_id = _sql.Column(_sql.String(), _sql.ForeignKey('Modifier.modifier_id'), ondelete="CASCADE", primary_key=True)
+    item_id = _sql.Column(_sql.String(), _sql.ForeignKey('Item.item_id'), ondelete="CASCADE", primary_key=True)
 
 
 class Category(_database.Base):
 
     __tablename__ = 'Category'
 
-    category_name = _database.Column(_database.String(), primary_key=True)
-    is_sub_category = _database.Column(_database.Boolean())
+    category_name = _sql.Column(_sql.String(), primary_key=True)
+    is_sub_category = _sql.Column(_sql.Boolean())
 
 
 class Modifier(_database.Base):
 
     __tablename__ = 'Modifier'
 
-    modifier_id = _database.Column(_database.String(), primary_key=True)
-    effect = _database.Column(_database.String(), nullable=False)
-    implicit = _database.Column(_database.Boolean())
-    explicit = _database.Column(_database.Boolean())
-    delve = _database.Column(_database.Boolean())
-    fractured = _database.Column(_database.Boolean())
-    synthesized = _database.Column(_database.Boolean())
-    corrupted = _database.Column(_database.Boolean())
-    enchanted = _database.Column(_database.Boolean())
-    veiled = _database.Column(_database.Boolean())
-    created_at = _database.Column(_database.DateTime())
-    updated_at = _database.Column(_database.DateTime())
+    modifier_id = _sql.Column(_sql.String(), primary_key=True)
+    effect = _sql.Column(_sql.String(), nullable=False)
+    implicit = _sql.Column(_sql.Boolean())
+    explicit = _sql.Column(_sql.Boolean())
+    delve = _sql.Column(_sql.Boolean())
+    fractured = _sql.Column(_sql.Boolean())
+    synthesized = _sql.Column(_sql.Boolean())
+    corrupted = _sql.Column(_sql.Boolean())
+    enchanted = _sql.Column(_sql.Boolean())
+    veiled = _sql.Column(_sql.Boolean())
+    created_at = _sql.Column(_sql.DateTime())
+    updated_at = _sql.Column(_sql.DateTime())
 
 
 class ModifierStats(_database.Base):
 
     __tablename__ = 'Modifier_Stats'
 
-    modifier_id = _database.Column(_database.String(), _database.ForeignKey('Modifier.modifier_id'), ondelete="CASCADE", primary_key=True)
-    stat_id = _database.Column(_database.String(), _database.ForeignKey('Stat.stat_id'), ondelete="CASCADE", primary_key=True)
+    modifier_id = _sql.Column(_sql.String(), _sql.ForeignKey('Modifier.modifier_id'), ondelete="CASCADE", primary_key=True)
+    stat_id = _sql.Column(_sql.String(), _sql.ForeignKey('Stat.stat_id'), ondelete="CASCADE", primary_key=True)
 
 
 class Stat(_database.Base):
 
     __tablename__ = 'Stat'
 
-    stat_id = _database.Column(_database.String(), primary_key=True)
-    position = _database.Column(_database.SmallInteger(), primary_key=True)
-    stat_value = _database.Column(_database.SmallInteger(), nullable=False)
-    mininum_value = _database.Column(_database.SmallInteger())
-    maximum_value = _database.Column(_database.SmallInteger())
-    stat_tier = _database.Column(_database.SmallInteger())
-    created_at = _database.Column(_database.DateTime())
-    updated_at = _database.Column(_database.DateTime())
+    stat_id = _sql.Column(_sql.String(), primary_key=True)
+    position = _sql.Column(_sql.SmallInteger(), primary_key=True)
+    stat_value = _sql.Column(_sql.SmallInteger(), nullable=False)
+    mininum_value = _sql.Column(_sql.SmallInteger())
+    maximum_value = _sql.Column(_sql.SmallInteger())
+    stat_tier = _sql.Column(_sql.SmallInteger())
+    created_at = _sql.Column(_sql.DateTime())
+    updated_at = _sql.Column(_sql.DateTime())
 
 
 class Stash(_database.Base):
 
     __tablename__ = 'Stash'
 
-    stash_id = _database.Column(_database.String(), primary_key=True)
-    account_name = _database.Column(_database.String(), _database.ForeignKey('Account.account_name'), ondelete="CASCADE", nullable=False)
-    public = _database.Column(_database.Boolean(), nullable=False)
-    league = _database.Column(_database.String(), nullable=False)
-    created_at = _database.Column(_database.DateTime())
-    updated_at = _database.Column(_database.DateTime())
+    stash_id = _sql.Column(_sql.String(), primary_key=True)
+    account_name = _sql.Column(_sql.String(), _sql.ForeignKey('Account.account_name'), ondelete="CASCADE", nullable=False)
+    public = _sql.Column(_sql.Boolean(), nullable=False)
+    league = _sql.Column(_sql.String(), nullable=False)
+    created_at = _sql.Column(_sql.DateTime())
+    updated_at = _sql.Column(_sql.DateTime())
 
 
 class Account(_database.Base):
 
     __tablename__ = 'Account'
 
-    account_name = _database.Column(_database.String(), primary_key=True)
-    is_banned = _database.Column(_database.Boolean())
-    created_at = _database.Column(_database.DateTime())
-    updated_at = _database.Column(_database.DateTime())
+    account_name = _sql.Column(_sql.String(), primary_key=True)
+    is_banned = _sql.Column(_sql.Boolean())
+    created_at = _sql.Column(_sql.DateTime())
+    updated_at = _sql.Column(_sql.DateTime())
--- fastapi-postgres-docker/databases/OLTP_db/schemas.py
+++ fastapi-postgres-docker/databases/OLTP_db/schemas.py
@@ -1,21 +1,156 @@
-import datetime as _dt
+import datetime
+from typing import Optional, Json
 import pydantic as _pydantic
 
-class _BaseContact(_pydantic.BaseModel):
-    model_config = _pydantic.ConfigDict(from_attributes=True)
 
-    name: str
-    email: str
-    phone: str
+class _BaseCurrency(_pydantic.BaseModel):
+
+    currency_name: str
+    value_in_chaos: float
+    icon_url: str
 
 
-class Contact(_BaseContact):
-    id: int
-    created_at: _dt.datetime
-    updated_at: _dt.datetime
+class Currency(_BaseCurrency):
+
+    created_at: Optional[datetime.datetime]
+    updated_at: Optional[datetime.datetime]
 
     class Config:
         orm_mode = True
 
-class CreateContact(_BaseContact):
+class CreateCurrency(_BaseCurrency):
     pass
+        
+
+class _BaseItem(_pydantic.BaseModel):
+    
+    item_id: str
+    stash_id: str
+    name: Optional[str]
+    icon_url: Optional[str]
+    league: str
+    base_type: str
+    type_line: str
+    rarity: str
+    identified: bool
+    item_level: int
+    forum_note: Optional[str]
+    currency_amount: Optional[float]
+    currency_name: Optional[str]
+    corrupted: Optional[bool]
+    delve: Optional[bool]
+    fractured: Optional[bool]
+    synthesized: Optional[bool]
+    replica: Optional[bool]
+    elder: Optional[bool]
+    shaper: Optional[bool]
+    searing: Optional[bool]
+    tangled: Optional[bool]
+    influences: Optional[_pydantic.Json]
+    is_relic: Optional[bool]
+    prefixes: Optional[int]
+    suffixes: Optional[int]
+    foil_variation: Optional[int]
+    inventory_id: Optional[str]
+
+
+class Item(_BaseItem):
+    created_at: Optional[datetime.datetime]
+    updated_at: Optional[datetime.datetime]
+
+    class Config:
+        orm_mode = True
+
+
+class CreateItem(_BaseItem):
+    pass
+
+
+class _BaseTransaction(_pydantic.BaseModel):
+    item_id: str
+    account_name: str
+    currency_amount: float
+    currency_name: str
+
+
+class Transaction(_BaseTransaction):
+    transaction_id: int
+    created_at: Optional[datetime.datetime]
+    updated_at: Optional[datetime.datetime]
+
+    class Config:
+        orm_mode = True
+
+
+class CreateTransaction(_BaseTransaction):
+    pass
+
+
+class ItemCategories(_pydantic.BaseModel):
+
+    category_name: str
+    item_id: str
+
+
+class ItemModifiers(_pydantic.BaseModel):
+
+    modifier_id: str
+    item_id: str
+
+
+class Category(_pydantic.BaseModel):
+
+    category_name: str
+    is_sub_category: Optional[bool]
+
+
+class Modifier(_pydantic.BaseModel):
+
+    modifier_id: str
+    effect: str
+    implicit: Optional[bool]
+    explicit: Optional[bool]
+    delve: Optional[bool]
+    fractured: Optional[bool]
+    synthesized: Optional[bool]
+    corrupted: Optional[bool]
+    enchanted: Optional[bool]
+    veiled: Optional[bool]
+    created_at: Optional[datetime.datetime]
+    updated_at: Optional[datetime.datetime]
+
+
+class ModifierStats(_pydantic.BaseModel):
+
+    modifier_id: str
+    stat_id: str
+
+
+class Stat(_pydantic.BaseModel):
+
+    stat_id: str
+    position: int
+    stat_value: int
+    mininum_value: Optional[int]
+    maximum_value: Optional[int]
+    stat_tier: Optional[int]
+    created_at: Optional[datetime.datetime]
+    updated_at: Optional[datetime.datetime]
+
+
+class Stash(_pydantic.BaseModel):
+
+    stash_id: str
+    account_name: str
+    public: bool
+    league: str
+    created_at: Optional[datetime.datetime]
+    updated_at: Optional[datetime.datetime]
+
+
+class Account(_pydantic.BaseModel):
+
+    account_name: str
+    is_banned: Optional[bool]
+    created_at: Optional[datetime.datetime]
+    updated_at: Optional[datetime.datetime]
