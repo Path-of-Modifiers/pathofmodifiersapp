9014926df23cea7b9e8c04f423375fdc733e9906
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-04 11:37:32 +0100

    26 Debug of models.py

--- fastapi-postgres-docker/databases/OLTP_db/models.py
+++ fastapi-postgres-docker/databases/OLTP_db/models.py
@@ -7,7 +7,7 @@ import database as _database
 
 class Currency(_database.Base):
 
-    _Tablename__ = 'Currency'
+    __tablename__ = 'Currency'
 
     currencyName = _sql.Column(_sql.String(), primary_key=True, index=True, nullable=False)
     valueInChaos = _sql.Column(_sql.Float(), nullable=False)
@@ -18,21 +18,21 @@ class Currency(_database.Base):
 
 class Item(_database.Base):
 
-    _Tablename__ = 'Item'
+    __tablename__ = 'Item'
 
     itemId = _sql.Column(_sql.String(), primary_key=True, index=True, nullable=False)
-    stashId = _sql.Column(_sql.String(), _sql.ForeignKey('Stash.stashId'), ondelete="CASCADE", nullable=False)
+    stashId = _sql.Column(_sql.String(), _sql.ForeignKey('Stash.stashId', ondelete="CASCADE"), nullable=False)
     name = _sql.Column(_sql.String())
     iconUrl = _sql.Column(_sql.String())
     league = _sql.Column(_sql.String(), nullable=False)
     typeLine = _sql.Column(_sql.String(), nullable=False)
-    baseType = _sql.Column(_sql.String(), _sql.ForeignKey('ItemBaseType.baseType'), ondelete="RESTRICT", nullable=False)
+    baseType = _sql.Column(_sql.String(), _sql.ForeignKey('ItemBaseType.baseType', ondelete="RESTRICT"), nullable=False)
     rarity = _sql.Column(_sql.String(), nullable=False)
     identified = _sql.Column(_sql.Boolean(), nullable=False)
     itemLevel = _sql.Column(_sql.SmallInteger(), nullable=False)
     forumNote = _sql.Column(_sql.String())
     currencyAmount = _sql.Column(_sql.Float(24))
-    currencyName = _sql.Column(_sql.String(), _sql.ForeignKey('Currency.currencyName'), ondelete="RESTRICT")
+    currencyName = _sql.Column(_sql.String(), _sql.ForeignKey('Currency.currencyName', ondelete="RESTRICT"))
     corrupted = _sql.Column(_sql.Boolean())
     delve = _sql.Column(_sql.Boolean())
     fractured = _sql.Column(_sql.Boolean())
@@ -54,20 +54,20 @@ class Item(_database.Base):
 
 class Transaction(_database.Base):
 
-    _Tablename__ = 'Transaction'
+    __tablename__ = 'Transaction'
 
     transactionId = _sql.Column(_sql.Integer(), autoincrement=True, primary_key=True, index=True, nullable=False)
-    itemId = _sql.Column(_sql.String(), _sql.ForeignKey('Item.itemId'), ondelete="CASCADE", nullable=False)
-    accountName = _sql.Column(_sql.String(), _sql.ForeignKey('Account.accountName'), ondelete="CASCADE", nullable=False)
+    itemId = _sql.Column(_sql.String(), _sql.ForeignKey('Item.itemId', ondelete="CASCADE"), nullable=False)
+    accountName = _sql.Column(_sql.String(), _sql.ForeignKey('Account.accountName', ondelete="CASCADE"), nullable=False)
     currencyAmount = _sql.Column(_sql.Float(24), nullable=False)
-    currencyName = _sql.Column(_sql.String(), _sql.ForeignKey('Currency.currencyName'), ondelete="RESTRICT", nullable=False)
+    currencyName = _sql.Column(_sql.String(), _sql.ForeignKey('Currency.currencyName', ondelete="RESTRICT"), nullable=False)
     createdAt = _sql.Column(_sql.DateTime(), default=_dt.datetime.utcnow)
     updatedAt = _sql.Column(_sql.DateTime(), default=_dt.datetime.utcnow)
 
 
 class ItemBaseType(_database.Base):
 
-    _Tablename__ = 'ItemBaseType'
+    __tablename__ = 'ItemBaseType'
 
     baseType = _sql.Column(_sql.String(), nullable=False, primary_key=True, index=True)
     category = _sql.Column(_sql.String(), nullable=False, unique=True)
@@ -80,9 +80,9 @@ class ItemModifier(_database.Base):
 
     __tablename__ = 'ItemModifier'
 
-    itemId = _sql.Column(_sql.String(), _sql.ForeignKey('Item.itemId'), ondelete="CASCADE", nullable=False, index=True)
-    modifierId = _sql.Column(_sql.String(), _sql.ForeignKey('Modifier.modifierId'), ondelete="CASCADE", nullable=False, index=True)
-    position = _sql.Column(_sql.SmallInteger(), _sql.ForeignKey('Modifier.position'), ondelete="CASCADE", nullable=False, index=True)
+    itemId = _sql.Column(_sql.String(), _sql.ForeignKey('Item.itemId', ondelete="CASCADE"), nullable=False, index=True)
+    modifierId = _sql.Column(_sql.String(), _sql.ForeignKey('Modifier.modifierId', ondelete="CASCADE"), nullable=False, index=True)
+    position = _sql.Column(_sql.SmallInteger(), _sql.ForeignKey('Modifier.position', ondelete="CASCADE"), nullable=False, index=True)
     range = _sql.Column(_sql.Float(24))
 
     __table_args__ = (
@@ -111,16 +111,16 @@ class Modifier(_database.Base):
     updatedAt = _sql.Column(_sql.DateTime(), default=_dt.datetime.utcnow)
 
     __table_args__ = (
-        _sql.PrimaryKeyConstraint('Modifier.modifierId', 'Modifier.position', name='pk_Modifier'),
+        _sql.PrimaryKeyConstraint(modifierId, position, name='pk_Modifier'),
     )
 
 
 class Stash(_database.Base):
 
-    _Tablename__ = 'Stash'
+    __tablename__ = 'Stash'
 
     stashId = _sql.Column(_sql.String(), primary_key=True, index=True, nullable=False)
-    accountName = _sql.Column(_sql.String(), _sql.ForeignKey('Account.accountName'), ondelete="CASCADE", nullable=False)
+    accountName = _sql.Column(_sql.String(), _sql.ForeignKey('Account.accountName', ondelete="CASCADE"), nullable=False)
     public = _sql.Column(_sql.Boolean(), nullable=False)
     league = _sql.Column(_sql.String(), nullable=False)
     createdAt = _sql.Column(_sql.DateTime(), default=_dt.datetime.utcnow)
@@ -129,7 +129,7 @@ class Stash(_database.Base):
 
 class Account(_database.Base):
 
-    _Tablename__ = 'Account'
+    __tablename__ = 'Account'
 
     accountName = _sql.Column(_sql.String(), primary_key=True, index=True, nullable=False)
     isBanned = _sql.Column(_sql.Boolean())
