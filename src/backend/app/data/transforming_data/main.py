0fe7203d2baff507a08a25353d07dedc097f3e0c
Bogadisa <magnus@hoddevik.eu>
2024-02-11 15:04:45 +0100

    20 Update: finished item_modifier table + Added: temporary modifierId

--- src/backend/app/data/transforming_data/main.py
+++ src/backend/app/data/transforming_data/main.py
@@ -37,9 +37,6 @@ class DataTransformer:
     def _create_item_modifier_table(self, json_data: list) -> None:
         raise NotImplementedError("Only available in child classes")
 
-    def _create_modifier_table(self, json_data: list) -> None:
-        raise NotImplementedError("Only available in child classes")
-
     def _transform_item_table(self) -> None:
         item_df = self.item_df
         currency_series = item_df["note"].str.split(" ")
@@ -64,14 +61,11 @@ class DataTransformer:
     def _transform_item_modifier_table(self) -> None:
         raise NotImplementedError("Only available in child classes")
 
-    def _transform_modifier_table(self) -> None:
-        raise NotImplementedError("Only available in child classes")
-
     def _clean_stash_table(self):
         self.stash_df.drop(["items", "stashType"], axis=1, inplace=True)
 
     def _clean_item_table(self):
-        item_df = self.item_df.drop(
+        self.item_df.drop(
             [
                 "verified",
                 "w",
@@ -85,29 +79,23 @@ class DataTransformer:
                 "requirements",
             ],
             axis=1,
+            inplace=True,
         )
 
-        item_df.to_csv("test.csv", index=False)
-
     def _clean_item_modifier_table(self):
-        raise NotImplementedError("Only available in child classes")
-
-    def _clean_modifier_table(self):
         raise NotImplementedError("Only available in child classes")
 
     def transform_into_tables(self, json_data):
         self._create_stash_table(json_data=json_data)
         self._create_item_table(json_data=json_data)
         self._create_item_modifier_table(json_data=json_data)
-        # self._create_modifier_table(json_data=json_data)
 
         self._transform_item_table()
         self._transform_item_modifier_table()
-        # self._transform_modifier_table()
 
         self._clean_stash_table()
         self._clean_item_table()
-        self._clean_modifier_table()
+        self._clean_item_modifier_table()
 
     @staticmethod
     def _expand_df(df: pd.DataFrame, json_column: str, target_column: str):
@@ -122,74 +110,64 @@ class DataTransformer:
 
     @staticmethod
     def _get_rolls_data(df, modifier_df):
+        df.loc[:, "modifier"] = df["modifier"].replace(r"\\n|\n", " ", regex=True)
+
         static_modifier_mask = modifier_df["static"] == "True"
 
         dynamic_modifier_df = modifier_df.loc[~static_modifier_mask]
         static_modifier_df = modifier_df.loc[static_modifier_mask]
 
+        # ---- Static modifier processing ----
         static_df = df.loc[df["modifier"].isin(static_modifier_df["effect"])]
-        static_df["position"] = "0"
-        # static_df["modifier"] = ""
+        static_df.loc[:, "position"] = "0"
+        static_df.loc[:, "effect"] = static_df.loc[:, "modifier"]
+
+        merged_static_df = static_df.merge(
+            static_modifier_df, on=["effect", "position"], how="left"
+        )
+        failed_df = merged_static_df.loc[merged_static_df["static"].isna()]
+
+        try:
+            assert failed_df.empty
+        except AssertionError:
+            print(failed_df)
+            quit()
+
+        # ---- Dynamic modifier processing ----
 
         dynamic_df = df.loc[~df["modifier"].isin(static_modifier_df["effect"])]
 
-        dynamic_df.loc[:, "modifier"] = dynamic_df["modifier"].replace(
-            r"\\n|\n", " ", regex=True
-        )
-        dynamic_modifier_df.loc[:, "effect"] = dynamic_modifier_df["effect"].replace(
-            r"\\n|\n", " ", regex=True
-        )
+        dynamic_df.loc[:, "effect"] = dynamic_df.loc[:, "modifier"]
+        for regex, effect in dynamic_modifier_df[["regex", "effect"]].itertuples(
+            index=False
+        ):
+            dynamic_df.loc[:, "effect"] = dynamic_df.loc[:, "effect"].str.replace(
+                regex, effect, regex=True
+            )
 
-        
-        
-        dynamic_modifier_df = dynamic_modifier_df.groupby("effect", as_index=False).agg(
-            {
-                "position": lambda positions: [position for position in set(positions)],
-                "minRoll": lambda x: [element for element in x if not pd.isna(element)],
-                "textRolls": lambda x: [element for element in x if not pd.isna(element)],
-            }
-        )
-        dynamic_modifier_df["rolls"] = (
-            dynamic_modifier_df["minRoll"] + dynamic_modifier_df["textRolls"]
-        )
+        def add_alternate_effect(df):
+            df.loc[:, "alternateEffect"] = df["effect"]
+            df["alternateEffect"] = df["alternateEffect"].str.replace("+#", "-#")
+            df["alternateEffect"] = df["alternateEffect"].str.replace(
+                "increased", "reduced"
+            )
+            df.loc[
+                df["alternateEffect"] == df["effect"],
+                "alternateEffect",
+            ] = ""  # Gets rid of unneccesary alternate effects
 
-        dynamic_modifier_df["effect"] = dynamic_modifier_df["effect"].str.replace("+", r"\+")
-        dynamic_modifier_df["effect"] = dynamic_modifier_df["effect"].str.replace("-", r"\-")
+            return df
 
-        def func(row):
-            effect = row["effect"]
-            positions = row["position"]
-            rolls = row["rolls"]
-            effect_parts = effect.split("#")
-            final_effect = ""
-            for i, (position, roll, part) in enumerate(zip(positions, rolls, effect_parts)):
-                final_effect += part
-                if roll.isnumeric():
-                    final_effect += f"(\d+)"
-                else:
-                    try:
-                        final_effect += f"({row["textRolls"][0].replace("-","|")})"
-                    except IndexError: # In cases of roll is a float
-                        final_effect += f"(\d+)"
+        dynamic_df = add_alternate_effect(df=dynamic_df)
 
-            final_effect += "".join(effect_parts[i+1:]) # adds the final part of the effect if there is one
-            return final_effect
-        
-        dynamic_modifier_df["regex"] = dynamic_modifier_df.apply(func, axis=1)
-        
-
-        dynamic_df["effect"] = dynamic_df.loc[:, "modifier"]
-        for regex, effect in dynamic_modifier_df[["regex", "effect"]].itertuples(index=False):
-            dynamic_df["effect"] = dynamic_df.loc[:,"effect"].str.replace(regex, effect, regex=True)
-
-        
-        dynamic_df["effect"] = dynamic_df["effect"].str.replace(r"\+", "+")
-        
-        def func(row):
+        def add_range_roll(row):
             modifier = row["modifier"]
             effect = row["effect"]
             effect_parts = [part for part in effect.split("#") if part]
-            
+
+            if not pd.isna(row["alternateEffect"]):
+                alternate_effect = row["alternateEffect"]
+                effect_parts += [part for part in alternate_effect.split("#") if part]
 
             for part in effect_parts:
                 modifier = modifier.replace(part, "---")
@@ -197,11 +175,63 @@ class DataTransformer:
             ranges = modifier.split("---")
 
             return [range_roll for range_roll in ranges if range_roll]
-        
-        dynamic_df["range"] = dynamic_df.apply(func, axis=1)
+
+        dynamic_df.loc[:, "range"] = dynamic_df.apply(add_range_roll, axis=1)
         failed_df = dynamic_df.loc[dynamic_df["range"].str.len() == 0]
-        print(failed_df.iloc[0])
-        quit()
+        try:
+            assert failed_df.empty
+        except AssertionError:
+            print(failed_df)
+            quit()
+
+        dynamic_df.loc[:, "position"] = dynamic_df.loc[:, "range"].apply(
+            lambda x: [str(i) for i in range(len(x))]
+        )
+
+        dynamic_df = dynamic_df.explode(["range", "position"])
+
+        merged_dynamic_df = dynamic_df.merge(
+            dynamic_modifier_df, on=["effect", "position"], how="left"
+        )
+
+        failed_df = merged_dynamic_df.loc[
+            merged_dynamic_df[["minRoll", "maxRoll", "textRolls"]].isna().all(axis=1)
+        ]
+
+        try:
+            assert failed_df.empty
+        except AssertionError:
+            print(failed_df)
+            quit()
+
+        def convert_range_roll_to_range(row):
+            un_processed_range = row["range"]
+            if not pd.isna(row["textRolls"]):
+                text_rolls = row["textRolls"].split("-")
+                min_roll = 0
+                max_roll = len(text_rolls)
+                x = text_rolls.index(un_processed_range)
+            else:
+                min_roll = float(row["minRoll"])
+                max_roll = float(row["maxRoll"])
+                x = float(un_processed_range)
+
+            converted_range = (x - min_roll) / (max_roll - min_roll)
+
+            return converted_range
+
+        merged_dynamic_df["range"] = merged_dynamic_df.apply(
+            convert_range_roll_to_range, axis=1
+        )
+
+        # ---- Finishing touches ----
+        processed_df = pd.concat(
+            (merged_dynamic_df, merged_static_df), axis=0, ignore_index=True
+        )
+
+        finished_df = processed_df.loc[:, ["itemId", "position", "range", "modifierId"]]
+
+        return processed_df
 
 
 class UniqueDataTransformer(DataTransformer):
@@ -220,9 +250,6 @@ class UniqueDataTransformer(DataTransformer):
 
         self.item_modifier_df = item_modifier_df
 
-    def _create_modifier_table(self, json_data: list) -> None:
-        pass
-
     def _transform_item_modifier_table(self) -> None:
         item_modifier_df = self.item_modifier_df
 
@@ -231,28 +258,51 @@ class UniqueDataTransformer(DataTransformer):
         )
 
         modifier_df = pd.DataFrame(
-            columns=["minRoll", "maxRoll", "textRolls", "position", "effect", "static"]
+            columns=[
+                "minRoll",
+                "maxRoll",
+                "textRolls",
+                "position",
+                "effect",
+                "static",
+                "modifierId",
+            ]
         )
         for unique in item_modifier_df["roll_file_name"].unique():
-            if unique != "WatchersEye":
-                continue
-            temp_df = pd.read_csv(f"base_data/{unique}.csv", dtype=str)
+            # if unique != "WatchersEye":
+            #     continue
+            temp_df = pd.read_csv(f"new_base_data/{unique}.csv", dtype=str)
             modifier_df = pd.concat((modifier_df, temp_df), axis=0, ignore_index=True)
 
         item_modifier_df = self._get_rolls_data(
-            df=item_modifier_df.loc[item_modifier_df["name"] == "Watcher's Eye"],
-            # df=item_modifier_df,
+            # df=item_modifier_df.loc[item_modifier_df["name"] == "Watcher's Eye"],
+            df=item_modifier_df,
             modifier_df=modifier_df,
         )
 
-    def _transform_modifier_table(self) -> None:
-        pass
+        self.item_modifier_df = item_modifier_df
 
     def _clean_item_modifier_table(self):
-        pass
+        self.item_modifier_df.drop(
+            [
+                "modifier",
+                "name",
+                "roll_file_name",
+                "effect",
+                "alternateEffect",
+                "minRoll",
+                "maxRoll",
+                "textRolls",
+                "static",
+                "regex",
+            ],
+            axis=1,
+            inplace=True,
+        )
 
-    def _clean_modifier_table(self):
-        pass
+        # item_modifier_df = item_modifier_df.loc[
+        #     :, ["itemId", "position", "range", "modifierId"]
+        # ]
 
 
 def main():
