53c7866d5714fb7cb6064c8796f6efcb51d38c19
Bogadisa <magnus@hoddevik.eu>
2024-02-11 11:57:24 +0100

    20 Updated: filestructure inline with main

---
+++ src/backend/app/data/transforming_data/test.py
@@ -0,0 +1,20 @@
+# mod = r"Commanded leadership over 17692 warriors under Rakiata\nPassives in radius are Conquered by the Karui"
+# effect_str = r"Commanded leadership over # warriors under #\nPassives in radius are Conquered by the Karui"
+# effect_parts = effect_str.split("#")
+# for part in effect_parts:
+#     print()
+#     print(part)
+#     print()
+#     print(mod)
+#     mod = mod.replace(part, " ")
+#     print(mod)
+
+# mod_rolls = mod.split(" ")
+
+# print(mod_rolls)
+
+# a = [0, 1, 2]
+# a =
+# print(a[3:])
+a = "dasd"
+float(a)
