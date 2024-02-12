11f4a1ded0d78c3257113f1498c198d038854219
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-12 17:42:16 +0100

    35 Didn't need backend_cors_origins

--- src/backend/app/app/core/config.py
+++ src/backend/app/app/core/config.py
@@ -7,7 +7,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
 
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost:8080"]
     DATABASE_URL: Optional[PostgresDsn] = "postgresql://pom_oltp_superuser:sjukebarna123@localhost/pom_oltp_db"
 
     model_config = SettingsConfigDict(env_prefix='POM_MODEL_')
