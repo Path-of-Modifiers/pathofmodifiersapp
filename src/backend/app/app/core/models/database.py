5be7e0f2d05eeec627df1bca70e1d494abc9ace5
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-12 16:35:16 +0100

    35 Changed to import app.core...

--- src/backend/app/app/core/models/database.py
+++ src/backend/app/app/core/models/database.py
@@ -2,7 +2,7 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-from core.config import settings 
+from app.core.config import settings 
 
 engine = _sql.create_engine(settings.DATABASE_URL.unicode_string())
 
