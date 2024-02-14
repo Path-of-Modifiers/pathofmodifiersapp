b7dd94ffe62f8667e2f05800c72176b5b5228857
Bogadisa <magnus@hoddevik.eu>
2024-02-14 16:40:49 +0100

    40 Debugged "is the server running on host "localhost" (127.0.0.1) and accepting TCP/IP connections on port 5432?"

--- src/backend/app/app/core/config.py
+++ src/backend/app/app/core/config.py
@@ -3,13 +3,14 @@ from pydantic import PostgresDsn, AnyHttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
 
-    DATABASE_URL: Optional[PostgresDsn] = "postgresql://pom_oltp_superuser:sjukebarna123@localhost:5432/pom_oltp_db"
+    DATABASE_URL: Optional[PostgresDsn] = (
+        "postgresql://pom_oltp_superuser:sjukebarna123@db:5432/pom_oltp_db"
+    )
 
-    model_config = SettingsConfigDict(env_prefix='POM_MODEL_')
+    model_config = SettingsConfigDict(env_prefix="POM_MODEL_")
 
 
 settings = Settings()
