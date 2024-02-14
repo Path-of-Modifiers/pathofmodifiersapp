78a0a15be34e2d1093bee4d1fb4b6f1154daeb14
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-14 12:43:29 +0100

    40 Debugging docker compose files, added ports in alembic and config.py

--- src/backend/app/app/core/config.py
+++ src/backend/app/app/core/config.py
@@ -7,7 +7,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
 
-    DATABASE_URL: Optional[PostgresDsn] = "postgresql://pom_oltp_superuser:sjukebarna123@localhost/pom_oltp_db"
+    DATABASE_URL: Optional[PostgresDsn] = "postgresql://pom_oltp_superuser:sjukebarna123@localhost:5432/pom_oltp_db"
 
     model_config = SettingsConfigDict(env_prefix='POM_MODEL_')
 
