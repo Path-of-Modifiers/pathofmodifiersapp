f0a5ae5999453a9fd5b9bf4a0b16069dd1a88702
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-15 16:22:56 +0100

    35 Updated env.py with correct configure

--- src/backend/app/migrations/env.py
+++ src/backend/app/migrations/env.py
@@ -53,6 +53,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        compare_type=False
     )
 
     with context.begin_transaction():
@@ -75,7 +76,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(connection=connection, target_metadata=target_metadata, compare_type=False)
 
         with context.begin_transaction():
             context.run_migrations()
