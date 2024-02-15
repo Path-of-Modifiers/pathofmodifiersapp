e1203730148211b4545a3d9a6236cd5c325f3ee9
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-15 15:53:47 +0100

    35 Updated url config in alembic env.py

--- src/backend/app/migrations/env.py
+++ src/backend/app/migrations/env.py
@@ -12,10 +12,10 @@ from app.core.models.models import Base
 def get_url():
     return os.getenv("DATABASE_URL")
 
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", get_url())  
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -47,7 +47,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -66,16 +66,16 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section)
+    configuration["sqlalchemy.url"] = get_url()
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
