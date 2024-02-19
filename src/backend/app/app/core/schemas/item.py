a69ff8b26178619b381554c9a60533e85790eb59
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-19 15:01:23 +0100

    54 Created own schemas file for each class

---
+++ src/backend/app/app/core/schemas/item.py
@@ -0,0 +1,65 @@
+import datetime as _dt
+from typing import Optional
+import pydantic as _pydantic
+from pydantic import Json
+
+
+# Shared item props
+class _BaseItem(_pydantic.BaseModel):
+    model_config = _pydantic.ConfigDict(from_attributes=True)
+
+    itemId: int
+    gameItemId: str
+    stashId: str
+    name: Optional[str]
+    iconUrl: Optional[str]
+    league: str
+    typeLine: str
+    baseType: str
+    rarity: str
+    identified: bool
+    itemLevel: int
+    forumNote: Optional[str]
+    currencyAmount: Optional[float]
+    currencyId: Optional[int]
+    corrupted: Optional[bool]
+    delve: Optional[bool]
+    fractured: Optional[bool]
+    synthesized: Optional[bool]
+    replica: Optional[bool]
+    elder: Optional[bool]
+    shaper: Optional[bool]
+    influences: Optional[Json]
+    searing: Optional[bool]
+    tangled: Optional[bool]
+    isRelic: Optional[bool]
+    prefixes: Optional[int]
+    suffixes: Optional[int]
+    foilVariation: Optional[int]
+    inventoryId: Optional[str]
+
+
+# Properties to receive on item creation
+class CreateItem(_BaseItem):
+    pass
+
+
+# Properties to receive on update
+class UpdateItem(_BaseItem):
+    pass
+
+
+# Properties shared by models stored in DB
+class ItemInDBBase(_BaseItem):
+    createdAt: Optional[_dt.datetime]
+    id: int
+
+
+# Properties to return to client
+class Item(ItemInDBBase):
+    pass
+
+
+# Properties stored in DB
+class ItemInDB(ItemInDBBase):
+    pass
