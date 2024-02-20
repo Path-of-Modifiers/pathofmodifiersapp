e85c8ab79b08a16dae81c7808b8a265cb18834b5
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-20 09:40:02 +0100

    54 Removed uncessary functions and import in services

--- src/backend/app/app/core/services/services.py
+++ src/backend/app/app/core/services/services.py
@@ -1,16 +1,5 @@
-from typing import TYPE_CHECKING, List
-
-import app.core.models.models as _models
-import app.core.schemas as _schemas
 import app.core.models.database as _database
-import pydantic as _pydantic
 
-if TYPE_CHECKING:
-    from sqlalchemy.orm import Session
-
-
-def _add_tables():
-    return _database.Base.metadata.create_all(bind=_database.engine)
 
 
 def get_db():
@@ -19,9 +8,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-def add_commit_refresh(field: _pydantic.BaseModel, db: "Session"):
-    db.add(field)
-    db.commit()
-    db.refresh(field)
