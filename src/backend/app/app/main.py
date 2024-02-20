4e6488f2a2f098515b29bef3fbe83af97b8c31cc
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-20 12:08:17 +0100

    54 Added more objects from models/schemas to api main

--- src/backend/app/app/main.py
+++ src/backend/app/app/main.py
@@ -199,3 +199,268 @@ async def update_itemBaseType(
     return await itemBaseTypeCRUD.update(
         db_obj=itemBaseType, obj_in=itemBaseTypeUpdate, db=db
     )
+
+
+itemCRUD = _crud.CRUDBase[
+    _models.Item,
+    _schemas.Item,
+    _schemas.ItemCreate,
+    _schemas.ItemUpdate,
+](model=_models.Item, schema=_schemas.Item)
+
+
+@app.post(
+    "/api/item/",
+    response_model=Union[_schemas.Item, List[_schemas.Item]],
+)
+async def create_item(
+    item: Union[_schemas.ItemCreate, List[_schemas.ItemCreate]],
+    db: _orm.Session = _fastapi.Depends(_deps.get_db),
+):
+    return await itemCRUD.create(db=db, obj_in=item)
+
+
+@app.get("/api/item/{gameItemId}", response_model=_schemas.Item)
+async def get_item(gameItemId: str, db: _orm.Session = _fastapi.Depends(_deps.get_db)):
+    item_map = {"gameItemId": gameItemId}
+    item = await itemCRUD.get(db=db, id=item_map)
+    return item
+
+
+@app.get("/api/item/", response_model=List[_schemas.Item])
+async def get_all_items(db: _orm.Session = _fastapi.Depends(_deps.get_db)):
+    all_items = await itemCRUD.get_all(db=db)
+    return all_items
+
+
+@app.delete("/api/item/{gameItemId}")
+async def delete_item(
+    gameItemId: str, db: _orm.Session = _fastapi.Depends(_deps.get_db)
+):
+    item_map = {"gameItemId": gameItemId}
+    await itemCRUD.remove(db=db, id=item_map)
+
+    return "Item deleted successfully"
+
+
+@app.put("/api/item/{gameItemId}", response_model=_schemas.Item)
+async def update_item(
+    gameItemId: str,
+    item_update: _schemas.ItemUpdate,
+    db: _orm.Session = _fastapi.Depends(_deps.get_db),
+):
+    item_mapped = {"gameItemId": gameItemId}
+    item = await itemCRUD.get(
+        db=db,
+        id=item_mapped,
+    )
+
+    return await itemCRUD.update(db_obj=item, obj_in=item_update, db=db)
+
+
+stashCRUD = _crud.CRUDBase[
+    _models.Stash,
+    _schemas.Stash,
+    _schemas.StashCreate,
+    _schemas.StashUpdate,
+](model=_models.Stash, schema=_schemas.Stash)
+
+
+@app.post(
+    "/api/stash/",
+    response_model=Union[_schemas.StashCreate, List[_schemas.StashCreate]],
+)
+async def create_stash(
+    stash: Union[_schemas.StashCreate, List[_schemas.StashCreate]],
+    db: _orm.Session = _fastapi.Depends(_deps.get_db),
+):
+    return await stashCRUD.create(db=db, obj_in=stash)
+
+
+@app.get("/api/stash/{stashId}", response_model=_schemas.Stash)
+async def get_stash(stashId: str, db: _orm.Session = _fastapi.Depends(_deps.get_db)):
+    stash_map = {"stashId": stashId}
+    stash = await stashCRUD.get(db=db, id=stash_map)
+    return stash
+
+
+@app.get("/api/stash/", response_model=List[_schemas.Stash])
+async def get_all_stashes(db: _orm.Session = _fastapi.Depends(_deps.get_db)):
+    all_stashes = await stashCRUD.get_all(db=db)
+    return all_stashes
+
+
+@app.delete("/api/stash/{stashId}")
+async def delete_stash(stashId: str, db: _orm.Session = _fastapi.Depends(_deps.get_db)):
+    stash_map = {"stashId": stashId}
+    await stashCRUD.remove(db=db, id=stash_map)
+
+    return "Stash deleted successfully"
+
+
+@app.put("/api/stash/{stashId}", response_model=_schemas.Stash)
+async def update_stash(
+    stashId: str,
+    stash_update: _schemas.StashUpdate,
+    db: _orm.Session = _fastapi.Depends(_deps.get_db),
+):
+    stash_mapped = {"stashId": stashId}
+    stash = await stashCRUD.get(
+        db=db,
+        id=stash_mapped,
+    )
+
+    return await stashCRUD.update(db_obj=stash, obj_in=stash_update, db=db)
+
+
+modifierCRUD = _crud.CRUDBase[
+    _models.Modifier,
+    _schemas.Modifier,
+    _schemas.ModifierCreate,
+    _schemas.ModifierUpdate,
+](model=_models.Modifier, schema=_schemas.Modifier)
+
+
+@app.post(
+    "/api/modifier/",
+    response_model=Union[_schemas.ModifierCreate, List[_schemas.ModifierCreate]],
+)
+async def create_modifier(
+    modifier: Union[_schemas.ModifierCreate, List[_schemas.ModifierCreate]],
+    db: _orm.Session = _fastapi.Depends(_deps.get_db),
+):
+    return await modifierCRUD.create(db=db, obj_in=modifier)
+
+
+@app.get("/api/modifier/{modifierId}/{position}", response_model=_schemas.Modifier)
+async def get_modifier(
+    modifierId: int, position: int, db: _orm.Session = _fastapi.Depends(_deps.get_db)
+):
+    modifier_map = {"modifierId": modifierId, "position": position}
+    modifier = await modifierCRUD.get(db=db, id=modifier_map)
+    return modifier
+
+
+@app.get("/api/modifier/", response_model=List[_schemas.Modifier])
+async def get_all_modifiers(db: _orm.Session = _fastapi.Depends(_deps.get_db)):
+    all_modifiers = await modifierCRUD.get_all(db=db)
+    return all_modifiers
+
+
+@app.delete("/api/modifier/{modifierId}/{position}")
+async def delete_modifier(
+    modifierId: int, position: int, db: _orm.Session = _fastapi.Depends(_deps.get_db)
+):
+    modifier_map = {"modifierId": modifierId, "position": position}
+    await modifierCRUD.remove(db=db, id=modifier_map)
+
+    return "Modifier deleted successfully"
+
+
+@app.put("/api/modifier/{modifierId}/{position}", response_model=_schemas.Modifier)
+async def update_modifier(
+    modifierId: int,
+    position: int,
+    modifier_update: _schemas.ModifierUpdate,
+    db: _orm.Session = _fastapi.Depends(_deps.get_db),
+):
+    modifier_mapped = {"modifierId": modifierId, "position": position}
+    modifier = await modifierCRUD.get(
+        db=db,
+        id=modifier_mapped,
+    )
+
+    return await modifierCRUD.update(db_obj=modifier, obj_in=modifier_update, db=db)
+
+
+itemModifierCRUD = _crud.CRUDBase[
+    _models.ItemModifier,
+    _schemas.ItemModifier,
+    _schemas.ItemModifierCreate,
+    _schemas.ItemModifierUpdate,
+](model=_models.ItemModifier, schema=_schemas.ItemModifier)
+
+
+@app.post(
+    "/api/itemModifier/",
+    response_model=Union[_schemas.Item, List[_schemas.Item]],
+)
+async def create_item_modifier(
+    itemModifier: Union[_schemas.ItemCreate, List[_schemas.ItemCreate]],
+    db: _orm.Session = _fastapi.Depends(_deps.get_db),
+):
+    return await itemModifierCRUD.create(db=db, obj_in=itemModifier)
+
+
+@app.get(
+    "/api/itemModifier/{itemId}/{gameItemId}/{modifierId}/{position}",
+    response_model=_schemas.ItemModifier,
+)
+async def get_item_modifier(
+    itemId: int,
+    gameItemId: str,
+    modifierId: int,
+    position: int,
+    db: _orm.Session = _fastapi.Depends(_deps.get_db),
+):
+    itemModifier_map = {
+        "itemId": itemId,
+        "gameItemId": gameItemId,
+        "modifierId": modifierId,
+        "position": position,
+    }
+    itemModifier = await itemModifierCRUD.get(db=db, id=itemModifier_map)
+    return itemModifier
+
+
+@app.get("/api/itemModifier/", response_model=List[_schemas.ItemModifier])
+async def get_all_item_modifiers(db: _orm.Session = _fastapi.Depends(_deps.get_db)):
+    all_item_modifiers = await itemModifierCRUD.get_all(db=db)
+    return all_item_modifiers
+
+
+@app.delete("/api/itemModifier/{itemId}/{gameItemId}/{modifierId}/{position}")
+async def delete_item_modifier(
+    itemId: int,
+    gameItemId: str,
+    modifierId: int,
+    position: int,
+    db: _orm.Session = _fastapi.Depends(_deps.get_db),
+):
+    itemModifier_map = {
+        "itemId": itemId,
+        "gameItemId": gameItemId,
+        "modifierId": modifierId,
+        "position": position,
+    }
+    await itemModifierCRUD.remove(db=db, id=itemModifier_map)
+
+    return "ItemModifier deleted successfully"
+
+
+@app.put(
+    "/api/itemModifier/{itemId}/{gameItemId}/{modifierId}/{position}",
+    response_model=_schemas.ItemModifier,
+)
+async def update_item_modifier(
+    itemId: int,
+    gameItemId: str,
+    modifierId: int,
+    position: int,
+    itemModifier_update: _schemas.Item,
+    db: _orm.Session = _fastapi.Depends(_deps.get_db),
+):
+    itemModifier_mapped = {
+        "itemId": itemId,
+        "gameItemId": gameItemId,
+        "modifierId": modifierId,
+        "position": position,
+    }
+    itemModifier = await itemModifierCRUD.get(
+        db=db,
+        id=itemModifier_mapped,
+    )
+
+    return await itemModifierCRUD.update(
+        db_obj=itemModifier, obj_in=itemModifier_update, db=db
+    )
