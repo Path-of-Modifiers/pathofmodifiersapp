a3bf19e36c121b19c171215296976d8b2b0c04c2
Bogadisa <magnus@hoddevik.eu>
2024-02-21 13:12:25 +0100

    54 Fixed update, now uses jsonable_encoder

--- src/backend/app/app/crud/base.py
+++ src/backend/app/app/crud/base.py
@@ -65,11 +65,12 @@ class CRUDBase(Generic[ModelType, SchemaType, CreateSchemaType, UpdateSchemaType
         db_obj: ModelType,
         obj_in: Union[UpdateSchemaType, Dict[str, Any]],
     ) -> ModelType:
+        obj_data = jsonable_encoder(db_obj)
         if isinstance(obj_in, dict):
             update_data = obj_in
         else:
             update_data = obj_in.model_dump()
-        for field in obj_in:
+        for field in obj_data:
             if field in update_data:
                 setattr(db_obj, field, update_data[field])
         db.add(db_obj)
