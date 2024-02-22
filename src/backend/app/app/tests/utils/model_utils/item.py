25ec1bed412a8d9fc1d2b66664b9b28ad083a0be
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-22 20:10:39 +0100

    77 Beginning of item utils test

--- src/backend/app/app/tests/utils/model_utils/item.py
+++ src/backend/app/app/tests/utils/model_utils/item.py
@@ -1,2 +1,17 @@
 from app import crud
+from typing import Optional
+
+from sqlalchemy.orm import Session
+
+from app import crud
+from app.core.models.models import Item
+from app.core.schemas.item import ItemCreate
+from app.tests.utils.utils import random_lower_string
+
+
+def create_random_item(db: Session) -> Item:
+    title = random_lower_string()
+    description = random_lower_string()
+    item_in = ItemCreate(title=title, description=description, id=id)
+    return crud.Item.create(db=db, obj_in=item_in)
 
