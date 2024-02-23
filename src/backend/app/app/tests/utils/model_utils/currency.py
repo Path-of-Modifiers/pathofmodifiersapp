9ae2105328b3a6dd0c4147c18765527967f36e0b
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-23 09:01:29 +0100

    77 Added random url function to item and currency iconURL

--- src/backend/app/app/tests/utils/model_utils/currency.py
+++ src/backend/app/app/tests/utils/model_utils/currency.py
@@ -7,13 +7,13 @@ from app.core.models.models import Currency
 from app.core.schemas import CurrencyCreate
 from app.tests.utils.utils import random_lower_string
 from app.tests.utils.utils import random_float
-
+from app.tests.utils.utils import random_url
 
 
 def create_random_currency(db: Session) -> Currency:
     currencyName = random_lower_string()
     valueInChaos = random_float()
-    iconUrl = random_lower_string()
+    iconUrl = random_url()
     currency = CurrencyCreate(
         currencyName=currencyName,
         valueInChaos=valueInChaos,
