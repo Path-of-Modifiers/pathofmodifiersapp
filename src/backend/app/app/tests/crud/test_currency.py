c2034b62e11493e404775cb181bc6a087d02ace7
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-23 14:01:29 +0100

    77 Formatted test_currency crud

--- src/backend/app/app/tests/crud/test_currency.py
+++ src/backend/app/app/tests/crud/test_currency.py
@@ -3,7 +3,11 @@ from sqlalchemy.orm import Session
 from app import crud
 from app.core.schemas.currency import CurrencyUpdate
 from backend.app.app.tests.utils.model_utils.currency import create_random_currency
-from backend.app.app.tests.utils.utils import random_float, random_lower_string, random_url
+from backend.app.app.tests.utils.utils import (
+    random_float,
+    random_lower_string,
+    random_url,
+)
 
 
 async def test_create_currency(db: Session) -> None:
