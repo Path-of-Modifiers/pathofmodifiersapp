35c7d4bcf23aa7a5bf90eb143dc9d67851d98132
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-24 12:50:04 +0100

    77 Created conftest.py with db generator

---
+++ src/backend/app/app/tests/conftest.py
@@ -0,0 +1,12 @@
+from typing import Generator
+import pytest
+from sqlalchemy.orm import Session
+from app.core.models.database import engine
+
+
+@pytest.fixture(scope="session")
+def db() -> Generator:
+    with Session(engine) as session:
+        yield session
+    session.rollback()
+    session.close()
