271bad790c0443310f2444fb96e28595c9b6a17d
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-24 13:33:58 +0100

    77 added some more tests for crud models

--- src/backend/app/app/tests/crud/crud_models/test_account.py
+++ src/backend/app/app/tests/crud/crud_models/test_account.py
@@ -1,87 +1,51 @@
-from fastapi import HTTPException
 from sqlalchemy.orm import Session
-
+from typing import Dict, Generator
 import pytest
-from app import crud
-from app.core.schemas.account import AccountUpdate
-from backend.app.app.tests.utils.model_utils.account import (
-    create_random_account,
-    create_random_account_list,
+
+from app.crud import CRUD_account
+from app.core.models.database import engine
+from app.crud.base import CRUDBase
+from app.tests.utils.utils import (
+    random_lower_string,
+    random_bool,
 )
-from backend.app.app.tests.utils.utils import random_bool, random_lower_string
+from app.tests.crud.test_crud import TestCRUD
 
 
-async def test_create_account(db: Session) -> None:
-    account = await create_random_account(db)
-    stored_created_account = await crud.CRUD_account.create(db, obj_in=account)
-    assert stored_created_account
-    assert stored_created_account.accountName == account.accountName
-    assert stored_created_account.isBanned == account.isBanned
+@pytest.fixture(scope="session")
+def db() -> Generator:
+    with Session(engine) as session:
+        yield session
+    session.rollback()
+    session.close()
 
 
-async def test_get_account(db: Session) -> None:
-    account = await create_random_account(db)
-    account_map = {"accountName": account.accountName}
-    stored_account = await crud.CRUD_account.get(db, filter=account_map)
-    assert stored_account
-    assert stored_account.accountName == account.accountName
-    assert stored_account.isBanned == account.isBanned
+def generate_random_account() -> Dict:
+    accountName = random_lower_string()
+    isBanned = random_bool()
+
+    account_dict = {
+        "accountName": accountName,
+        "isBanned": isBanned,
+    }
+
+    return account_dict
 
 
-async def test_create_multiple_accounts(db: Session) -> None:
-    # Get the initial count of stored accounts
-    initial_account_count = len(await crud.CRUD_account.get(db))
-
-    # Create random accounts
-    accounts = await create_random_account_list(db=db, count=5)
-
-    # Get the final count of stored accounts
-    stored_accounts = await crud.CRUD_account.get(db)
-    final_account_count = len(stored_accounts)
-
-    # Ensure the total count matches the expected count
-    assert final_account_count == initial_account_count + 5
-
-    # Check that the newly created accounts are in the stored accounts
-    for stored_account in stored_accounts:
-        if stored_account not in accounts:
-            assert stored_account
-            assert stored_account in await crud.CRUD_account.get(db)
+@pytest.fixture(scope="class")
+def main_key() -> str:
+    return None
 
 
-
-async def test_get_all_account(db: Session) -> None:
-    account = await create_random_account(db)
-    account_name_map = {"accountName": account.accountName}
-    stored_account = await crud.CRUD_account.get(db, filter=account_name_map)
-    assert stored_account
-    all_accounts = await crud.CRUD_account.get(db)
-    assert stored_account in all_accounts
+@pytest.fixture(scope="class")
+def object_generator_func() -> Dict:
+    return generate_random_account
 
 
-async def test_update_account(db: Session) -> None:
-    account = await create_random_account(db)
-    account_name_map = {"accountName": account.accountName}
-    stored_account = await crud.CRUD_account.get(db, filter=account_name_map)
-    random_account_name = random_lower_string()
-    account_update = AccountUpdate(accountName=random_account_name, isBanned=True)
-    updated_account = await crud.CRUD_account.update(
-        db, db_obj=stored_account, obj_in=account_update
-    )
-    assert updated_account
-    assert updated_account.accountName == random_account_name
-    assert updated_account.isBanned == True
+@pytest.fixture(scope="class")
+def crud_instance() -> CRUDBase:
+    return CRUD_account
 
 
-async def test_delete_account(db: Session) -> None:
-    account = await create_random_account(db)
-    account_name_map = {"accountName": account.accountName}
-    stored_account = await crud.CRUD_account.get(db, filter=account_name_map)
-    deleted_account = await crud.CRUD_account.remove(db, filter=account_name_map)
-    
-    with pytest.raises(HTTPException) as error_info:
-        await crud.CRUD_account.get(db, filter=account_name_map)
-        assert error_info.value.status_code == 404
-    assert deleted_account
-    assert deleted_account.accountName == stored_account.accountName
-    assert deleted_account.isBanned == stored_account.isBanned
+# Instantiate TestCRUD class
+test_crud_instance = TestCRUD()
