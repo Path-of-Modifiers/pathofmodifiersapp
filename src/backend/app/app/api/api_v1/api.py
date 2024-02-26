d2661195188ceef640e3da60522729c057c437f5
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-26 17:28:24 +0100

    61 Renamed /v1/ to /api_v1

--- src/backend/app/app/api/api_v1/api.py
+++ src/backend/app/app/api/api_v1/api.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter
 
-# from app.api.api_v1.endpoints import items, login, users, utils
-
 api_router = APIRouter()
 # api_router.include_router(login.router, tags=["login"])
 # api_router.include_router(users.router, prefix="/users", tags=["users"])
