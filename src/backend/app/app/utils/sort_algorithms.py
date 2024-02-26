f018ea9fe8eb03393834d437c804c93dac447c05
Bogadisa <magnus@hoddevik.eu>
2024-02-26 17:34:52 +0100

    77 Added sorting algorithms to utils

---
+++ src/backend/app/app/utils/sort_algorithms.py
@@ -0,0 +1,24 @@
+def quickSort(array):
+    if len(array) > 1:
+        pivot = array.pop()
+        grtr_lst, equal_lst, smlr_lst = [], [pivot], []
+        for item in array:
+            if item == pivot:
+                equal_lst.append(item)
+            elif item > pivot:
+                grtr_lst.append(item)
+            else:
+                smlr_lst.append(item)
+        return quickSort(smlr_lst) + equal_lst + quickSort(grtr_lst)
+    else:
+        return array
+
+
+def sort_with_refrence(items, refrence):
+    sorted_refrence = quickSort(refrence.copy())
+    sorted_items = ["filler"] * len(items)
+    for obj, column_value in zip(items, refrence):
+        ind = sorted_refrence.index(column_value)
+        sorted_items[ind] = obj
+
+    return sorted_items
