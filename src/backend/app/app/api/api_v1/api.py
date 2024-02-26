6e4c1e9a307490e60190c746d65948516a9a99b9
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-26 17:39:27 +0100

    61 Setup config in main to api

--- src/backend/app/app/api/api_v1/api.py
+++ src/backend/app/app/api/api_v1/api.py
@@ -1,7 +1,25 @@
 from fastapi import APIRouter
 
+from app.api.api_v1.endpoints import (
+    account,
+    currency,
+    item_base_type,
+    item_modifier,
+    item,
+    modifier,
+    stash,
+)
+
 api_router = APIRouter()
-# api_router.include_router(login.router, tags=["login"])
-# api_router.include_router(users.router, prefix="/users", tags=["users"])
-# api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-# api_router.include_router(items.router, prefix="/items", tags=["items"])
+
+api_router.include_router(account.router, prefix="/account", tags=["accounts"])
+api_router.include_router(currency.router, prefix="/currency", tags=["currencies"])
+api_router.include_router(
+    item_base_type.router, prefix="/itemBaseType", tags=["itemBaseTypes"]
+)
+api_router.include_router(
+    item_modifier.router, prefix="/itemModifier", tags=["itemModifiers"]
+)
+api_router.include_router(item.router, prefix="/item", tags=["items"])
+api_router.include_router(modifier.router, prefix="/modifier", tags=["modifiers"])
+api_router.include_router(stash.router, prefix="/stash", tags=["stashes"])
