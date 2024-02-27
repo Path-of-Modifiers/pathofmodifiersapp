d0494f2324efeb900b4c8bc349cc95b3955aef41
Bogadisa <magnus@hoddevik.eu>
2024-02-27 14:40:55 +0100

    77 Fixed bugs introduced in issues 61 and 83

--- src/backend/app/app/tests/utils/model_utils/stash.py
+++ src/backend/app/app/tests/utils/model_utils/stash.py
@@ -1,21 +1,23 @@
 import asyncio
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Optional, Union, List
 from sqlalchemy.orm import Session
 
 from app import crud
-from app.core.models.models import Stash
+from app.core.models.models import Stash, Account
 from app.core.schemas.stash import StashCreate
 from app.tests.utils.utils import random_lower_string, random_bool
 
 from app.tests.utils.model_utils.account import generate_random_account
 
 
-async def create_random_stash_dict(db: Session) -> Dict:
+async def create_random_stash_dict(
+    db: Session, retrieve_dependencies: bool
+) -> Union[Dict, Tuple[Dict, List[Union[Dict, Account]]]]:
     public = random_bool()
     league = random_lower_string()
     stashId = random_lower_string()
 
-    _, account = await generate_random_account(db)
+    account_dict, account = await generate_random_account(db)
     accountName = account.accountName
 
     stash = {
@@ -24,12 +26,22 @@ async def create_random_stash_dict(db: Session) -> Dict:
         "league": league,
         "stashId": stashId,
     }
+    if not retrieve_dependencies:
+        return stash
+    else:
+        return stash, [account_dict, account]
 
-    return stash
 
-
-async def generate_random_stash(db: Session) -> Tuple[Dict, Stash]:
-    stash_dict = await create_random_stash_dict(db)
+async def generate_random_stash(
+    db: Session, retrieve_dependencies: Optional[bool] = False
+) -> Tuple[Dict, Stash]:
+    output = await create_random_stash_dict(
+        db, retrieve_dependencies=retrieve_dependencies
+    )
+    if retrieve_dependencies:
+        stash_dict, account_dependecies = output
+    else:
+        stash_dict = output
     stash_create = StashCreate(**stash_dict)
 
     stash = await crud.CRUD_stash.create(db, obj_in=stash_create)
