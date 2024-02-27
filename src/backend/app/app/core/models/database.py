76f699641db377b29e43400bcc7f990ae7f0a1bc
Bogadisa <magnus@hoddevik.eu>
2024-02-27 18:11:41 +0100

    77 Added an inspector to the database

--- src/backend/app/app/core/models/database.py
+++ src/backend/app/app/core/models/database.py
@@ -2,9 +2,12 @@ import sqlalchemy as _sql
 import sqlalchemy.orm as _orm
 
 from app.core.config import settings
+from sqlalchemy.inspection import inspect
 
 engine = _sql.create_engine(settings.DATABASE_URL.unicode_string())
 
 SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = _orm.declarative_base()
+
+insp = inspect(engine)
