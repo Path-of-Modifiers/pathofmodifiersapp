1c668235baf7808cd1f97965a3fb9c232acae547
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-27 16:12:32 +0100

    86 Created a working react ts app

--- src/frontend/src/pages/Dashboard.tsx
+++ src/frontend/src/pages/Dashboard.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import { Container, Text } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react';
 
 
+
+const logo = './src/assets/images/POM_logo.jpg';
 
 const Dashboard: React.FC = () => {
 
@@ -11,6 +14,7 @@ const Dashboard: React.FC = () => {
             <Container maxW='full' pt={12}>
                 <Text fontSize='2xl'>Hi, Sjukingen! 👋🏼</Text>
                 <Text>Welcome back, nice to see you again!</Text>
+                <Image src="./POM_logo.jpg" alt="POM logo"/>
             </Container>
         </>
 
