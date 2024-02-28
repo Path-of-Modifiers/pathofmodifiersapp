d81185bfe6295e49ab22c9f25650428db9504312
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-28 12:08:47 +0100

    86 Setup client side routing

--- src/frontend/src/routes/root.jsx
+++ src/frontend/src/routes/root.jsx
@@ -1,3 +1,6 @@
+import { Outlet, Link } from "react-router-dom";
+
+
 export default function Root() {
     return (
       <>
@@ -29,15 +32,17 @@ export default function Root() {
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`contacts/1`}>Your name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`contacts/2`}>Your Friend</Link>
               </li>
             </ul>
           </nav>
         </div>
-        <div id="detail"></div>
+        <div id="detail">
+          <Outlet />
+        </div>
       </>
     );
   }
