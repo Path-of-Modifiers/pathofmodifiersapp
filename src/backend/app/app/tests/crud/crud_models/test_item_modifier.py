56d4bdc7037a4e039cac9140289504aa3cef729c
Bogadisa <magnus@hoddevik.eu>
2024-02-28 11:09:36 +0100

    77 Added the rest of the dependency chain for itemModifier

--- src/backend/app/app/tests/crud/crud_models/test_item_modifier.py
+++ src/backend/app/app/tests/crud/crud_models/test_item_modifier.py
@@ -2,8 +2,24 @@ import asyncio
 from typing import Callable, Dict, Tuple, List, Union
 import pytest
 
-from app.crud import CRUD_itemModifier, CRUD_item, CRUD_modifier
-from app.core.models.models import ItemModifier, Item, Modifier
+from app.crud import (
+    CRUD_itemModifier,
+    CRUD_account,
+    CRUD_stash,
+    CRUD_itemBaseType,
+    CRUD_currency,
+    CRUD_item,
+    CRUD_modifier,
+)
+from app.core.models.models import (
+    ItemModifier,
+    Account,
+    Stash,
+    ItemBaseType,
+    Currency,
+    Item,
+    Modifier,
+)
 from app.crud.base import CRUDBase
 import app.tests.crud.cascade_tests as cascade_test
 from app.tests.utils.model_utils.item_modifier import generate_random_item_modifier
@@ -15,12 +31,24 @@ def object_generator_func() -> Callable[[], Dict]:
 
 
 @pytest.fixture(scope="module")
-def object_generator_func_w_deps() -> (
-    Callable[[], Tuple[Dict, ItemModifier, List[Union[Dict, Item, Modifier]]]]
-):
+def object_generator_func_w_deps() -> Callable[
+    [],
+    Tuple[
+        Dict,
+        ItemModifier,
+        List[Union[Dict, Account, Stash, ItemBaseType, Currency, Item, Modifier]],
+    ],
+]:
     def generate_random_item_modifier_w_deps(
         db,
-    ) -> Callable[[], Tuple[Dict, ItemModifier, List[Union[Dict, Item, Modifier]]]]:
+    ) -> Callable[
+        [],
+        Tuple[
+            Dict,
+            ItemModifier,
+            List[Union[Dict, Item, Account, Stash, ItemBaseType, Currency, Modifier]],
+        ],
+    ]:
         return generate_random_item_modifier(db, retrieve_dependencies=True)
 
     return generate_random_item_modifier_w_deps
@@ -33,7 +61,14 @@ def crud_instance() -> CRUDBase:
 
 @pytest.fixture(scope="module")
 def crud_deps_instances() -> List[CRUDBase]:
-    return [CRUD_item, CRUD_modifier]
+    return [
+        CRUD_account,
+        CRUD_stash,
+        CRUD_itemBaseType,
+        CRUD_currency,
+        CRUD_item,
+        CRUD_modifier,
+    ]
 
 
 class TestItemModifierCRUD(cascade_test.TestCascade):
