d8ba118ef4db8a3bf39a580094dfa1d839aaed9c
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-28 15:12:34 +0100

    86 Added delete functionality

--- src/frontend/src/routes/root.jsx
+++ src/frontend/src/routes/root.jsx
@@ -5,6 +5,7 @@ import {
   Form,
   redirect,
   NavLink,
+  useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
@@ -20,6 +21,7 @@ export async function action() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
   return (
     <>
       <div id="sidebar">
@@ -72,7 +74,10 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div
+        id="detail"
+        className={navigation.state === "loading" ? "loading" : ""}
+      >
         <Outlet />
       </div>
     </>
