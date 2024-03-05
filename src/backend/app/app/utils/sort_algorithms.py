78c59d3281b7550b7d6251184dc86b4fbb528ea2
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-03-05 15:57:06 +0100

    77 Resolved return types, parameter type and changed refrences -> references

--- src/backend/app/app/utils/sort_algorithms.py
+++ src/backend/app/app/utils/sort_algorithms.py
@@ -1,4 +1,7 @@
-def quickSort(array):
+from typing import Any, List
+
+
+def quickSort(array: List[Any]) -> List[Any]:
     if len(array) > 1:
         pivot = array.pop()
         grtr_lst, equal_lst, smlr_lst = [], [pivot], []
@@ -14,14 +17,16 @@ def quickSort(array):
         return array
 
 
-def sort_with_refrence(items, refrence, presorted: bool = False):
+def sort_with_refrence(
+    items: List[Any], reference: List[Any], presorted: bool = False
+) -> List[Any]:
     if not presorted:
-        sorted_refrence = quickSort(refrence.copy())
+        sorted_refrence = quickSort(reference.copy())
     else:
-        sorted_refrence = refrence
+        sorted_refrence = reference
 
     sorted_items = ["filler"] * len(items)
-    for obj, column_value in zip(items, refrence):
+    for obj, column_value in zip(items, reference):
         ind = sorted_refrence.index(column_value)
         sorted_items[ind] = obj
 
