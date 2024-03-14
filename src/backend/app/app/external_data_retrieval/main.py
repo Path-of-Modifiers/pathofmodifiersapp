e45c702c63caca1d06cc53b050c73c2c6dc60b17
Bogadisa <magnus@hoddevik.eu>
2024-03-14 16:15:13 +0100

    94 Restructured files with class renaming

---
+++ src/backend/app/app/external_data_retrieval/main.py
@@ -0,0 +1,25 @@
+from app.external_data_retrieval.data_retrieval.poe_api import APIHandler
+
+
+def main():
+    auth_token = "***REMOVED***"
+    url = "https://api.pathofexile.com/public-stash-tabs"
+
+    n_wanted_items = 10000
+    n_unique_wanted_items = 15
+
+    api_handler = APIHandler(
+        url=url,
+        auth_token=auth_token,
+        n_wanted_items=n_wanted_items,
+        n_unique_wanted_items=n_unique_wanted_items,
+    )
+    api_handler.dump_stream(
+        initial_next_change_id="2304265269-2292493816-2218568823-2460180973-2390424272"  # From poe.ninja
+    )  # max_iterations=100)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
