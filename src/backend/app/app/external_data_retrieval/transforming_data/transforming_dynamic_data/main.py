5ab34989d53bef14b76f8556798bd082c630547e
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-03-18 14:10:15 +0100

    101 Added test data file to  2024_01_24 22_33.json

--- src/backend/app/app/external_data_retrieval/transforming_data/transforming_dynamic_data/main.py
+++ src/backend/app/app/external_data_retrieval/transforming_data/transforming_dynamic_data/main.py
@@ -7,7 +7,7 @@ def load_test_data():
     """
     Temporary test data loader
     """
-    with open("testing_data/all_properties_test.json", encoding="utf8") as infile:
+    with open("testing_data/2024_01_24 22_33.json", encoding="utf8") as infile:
         data = json.load(infile)
 
     return data
