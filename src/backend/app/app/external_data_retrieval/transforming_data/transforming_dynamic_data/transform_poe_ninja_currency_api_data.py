83fe5d58b797098d002af3442d0d9961fb610a05
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-03-21 08:51:36 +0100

    113 Created better load data function

--- src/backend/app/app/external_data_retrieval/transforming_data/transforming_dynamic_data/transform_poe_ninja_currency_api_data.py
+++ src/backend/app/app/external_data_retrieval/transforming_data/transforming_dynamic_data/transform_poe_ninja_currency_api_data.py
@@ -2,12 +2,22 @@ import json
 from typing import List
 import pandas as pd
 
+from app.external_data_retrieval.data_retrieval.poe_ninja_currency_retrieval.poe_ninja_currency_api import PoeNinjaCurrencyAPIHandler
 
-def load_test_data():
-    """
-    Temporary test data loader
-    """
-    with open("testing_data/2024_01_24 22_33.json", encoding="utf8") as infile:
-        data = json.load(infile)
 
-    return data
+def load_data():
+    """
+    Loads data from the poe.ninja currency API.
+    """
+    poe_ninja_currency_api_handler = PoeNinjaCurrencyAPIHandler(
+        url="https://poe.ninja/api/data/currencyoverview?league=Affliction&type=Currency"
+    )
+    
+    currencies, currency_details = poe_ninja_currency_api_handler._make_request()
+    
+    currencies_df = poe_ninja_currency_api_handler._json_to_df(currencies)
+    currency_details_df = poe_ninja_currency_api_handler._json_to_df(currency_details)
+    
+    return currencies_df, currency_details_df
+
+
