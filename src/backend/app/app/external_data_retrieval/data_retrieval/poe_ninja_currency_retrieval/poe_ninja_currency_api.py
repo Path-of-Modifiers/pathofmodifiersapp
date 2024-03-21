71a82226e23d9a14785fa6f3bc09c3dbd1877dfe
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-03-21 09:15:01 +0100

    113 Updated with combined currency data in poe_ninja_currency_api.py

--- src/backend/app/app/external_data_retrieval/data_retrieval/poe_ninja_currency_retrieval/poe_ninja_currency_api.py
+++ src/backend/app/app/external_data_retrieval/data_retrieval/poe_ninja_currency_retrieval/poe_ninja_currency_api.py
@@ -1,7 +1,6 @@
 from typing import List, Tuple
 import pandas as pd
 import requests
-from tqdm import tqdm
 
 
 class PoeNinjaCurrencyAPIHandler:
@@ -9,12 +8,22 @@ class PoeNinjaCurrencyAPIHandler:
 
         self.url = url
 
+    def _combine_currency_data(self, currencies: List, currency_details: List) -> List:
+        """
+        Combines the currency data.
+        """
+        combined_currency_data = []
+        for currency, currency_detail in zip(currencies, currency_details):
+            combined_currency_data.append({**currency, **currency_detail})
+
+        return combined_currency_data
+
     def _json_to_df(self, currencies: List) -> pd.DataFrame:
         df = pd.json_normalize(currencies)
 
         return df
 
-    def _make_request(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    def make_request(self) -> pd.DataFrame:
         """
         Makes an initial, synchronous, API call.
         """
@@ -26,7 +35,13 @@ class PoeNinjaCurrencyAPIHandler:
         currencies = response_json["lines"]
         currency_details = response_json["currencyDetails"]
 
-        return currencies, currency_details
+        combined_currency_data = self._combine_currency_data(
+            currencies, currency_details
+        )
+
+        combined_currency_data_df = self._json_to_df(combined_currency_data)
+
+        return combined_currency_data_df
 
     def store_data(self, path: str) -> None:
         """
@@ -37,11 +52,11 @@ class PoeNinjaCurrencyAPIHandler:
         df_currency_details = self._json_to_df(currency_details)
 
         df_currencies.to_csv(path + "/poe_ninja_currencies.csv", index=False)
-        df_currency_details.to_csv(path + "/poe_ninja_currency_details.csv", index=False)
+        df_currency_details.to_csv(
+            path + "/poe_ninja_currency_details.csv", index=False
+        )
 
 
 PoeNinjaCurrencyAPIHandler(
     url="https://poe.ninja/api/data/currencyoverview?league=Standard&type=Currency"
-).store_data(
-    "./app/external_data_retrieval/depricated_data/test_data"
-)
+).store_data("./app/external_data_retrieval/depricated_data/test_data")
