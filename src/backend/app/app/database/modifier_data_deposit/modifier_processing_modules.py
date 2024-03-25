b112176dacca6896d2b22977acd75ba45da483fb
Ivareh <ivareh@ntnu.no>
2024-03-25 16:03:29 +0100

    128 Moved utils to /database/ (root) and renamed processing modules to modifier_processing_modules

--- src/backend/app/app/database/modifier_data_deposit/modifier_processing_modules.py
+++ src/backend/app/app/database/modifier_data_deposit/modifier_processing_modules.py
@@ -2,7 +2,7 @@ import logging
 import pandas as pd
 from typing import Tuple, List, Optional, Dict, Any
 
-from app.database.modifier_data_deposit.utils import df_to_JSON
+from backend.app.app.database.utils import df_to_JSON
 
 logging.basicConfig(
     filename="history.log",
