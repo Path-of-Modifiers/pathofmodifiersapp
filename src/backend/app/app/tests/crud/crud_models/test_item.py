fdd50341f5e042797a49d884bef9ca087f093618
Ivareh <ivareh@ntnu.no>
2024-03-27 10:27:30 +0100

    147 Removed stash traces in test

--- src/backend/app/app/tests/crud/crud_models/test_item.py
+++ src/backend/app/app/tests/crud/crud_models/test_item.py
@@ -6,12 +6,11 @@ import pytest
 from app.crud import (
     CRUD_item,
     CRUD_account,
-    CRUD_stash,
     CRUD_itemBaseType,
     CRUD_currency,
 )
 from app.core.models.database import engine
-from app.core.models.models import Item, Account, Stash, ItemBaseType, Currency
+from app.core.models.models import Item, Account, ItemBaseType, Currency
 from app.crud.base import CRUDBase
 import app.tests.crud.cascade_tests as cascade_test
 from app.tests.utils.model_utils.item import generate_random_item
@@ -25,13 +24,13 @@ def object_generator_func() -> Callable[[], Dict]:
 @pytest.fixture(scope="module")
 def object_generator_func_w_deps() -> (
     Callable[
-        [], Tuple[Dict, Item, List[Union[Dict, Account, Stash, ItemBaseType, Currency]]]
+        [], Tuple[Dict, Item, List[Union[Dict, Account, ItemBaseType, Currency]]]
     ]
 ):
     def generate_random_item_w_deps(
         db,
     ) -> Callable[
-        [], Tuple[Dict, Item, List[Union[Dict, Account, Stash, ItemBaseType, Currency]]]
+        [], Tuple[Dict, Item, List[Union[Dict, Account, ItemBaseType, Currency]]]
     ]:
         return generate_random_item(db, retrieve_dependencies=True)
 
@@ -47,7 +46,6 @@ def crud_instance() -> CRUDBase:
 def crud_deps_instances() -> CRUDBase:
     return [
         CRUD_account,
-        CRUD_stash,
         CRUD_itemBaseType,
         CRUD_currency,
     ]
