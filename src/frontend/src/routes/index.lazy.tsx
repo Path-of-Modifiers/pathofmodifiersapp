7014b3e09888df55f7a3ffc5d3edfa7cc0216fa8
Ivareh <ivareh@ntnu.no>
2024-03-28 19:38:19 +0100

    156 ADded navbar and some routes

--- src/frontend/src/routes/index.lazy.tsx
+++ src/frontend/src/routes/index.lazy.tsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import Navbar from '../components/Common/Navbar'
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
@@ -8,6 +9,8 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+
+      <Navbar/>
     </div>
   )
 }
