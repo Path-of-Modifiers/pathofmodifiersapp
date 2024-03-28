6ffed6cca1e7fdfdd51e93d22f1e96ec7eb6181c
Ivareh <ivareh@ntnu.no>
2024-03-28 09:52:43 +0100

    #86 Added updated frontend example from fullstackapi repo

---
+++ src/frontend/src/components/UserSettings/DeleteAccount.tsx
@@ -0,0 +1,36 @@
+import {
+  Button,
+  Container,
+  Heading,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react"
+import type React from "react"
+
+import DeleteConfirmation from "./DeleteConfirmation"
+
+const DeleteAccount: React.FC = () => {
+  const confirmationModal = useDisclosure()
+
+  return (
+    <>
+      <Container maxW="full">
+        <Heading size="sm" py={4}>
+          Delete Account
+        </Heading>
+        <Text>
+          Permanently delete your data and everything associated with your
+          account.
+        </Text>
+        <Button variant="danger" mt={4} onClick={confirmationModal.onOpen}>
+          Delete
+        </Button>
+        <DeleteConfirmation
+          isOpen={confirmationModal.isOpen}
+          onClose={confirmationModal.onClose}
+        />
+      </Container>
+    </>
+  )
+}
+export default DeleteAccount
---
+++ src/frontend/src/hooks/useAuth.ts
@@ -0,0 +1,42 @@
+import { useNavigate } from "@tanstack/react-router"
+import { useQuery } from "react-query"
+
+import {
+  type Body_login_login_access_token as AccessToken,
+  LoginService,
+  type UserOut,
+  UsersService,
+} from "../client"
+
+const isLoggedIn = () => {
+  return localStorage.getItem("access_token") !== null
+}
+
+const useAuth = () => {
+  const navigate = useNavigate()
+  const { data: user, isLoading } = useQuery<UserOut | null, Error>(
+    "currentUser",
+    UsersService.readUserMe,
+    {
+      enabled: isLoggedIn(),
+    },
+  )
+
+  const login = async (data: AccessToken) => {
+    const response = await LoginService.loginAccessToken({
+      formData: data,
+    })
+    localStorage.setItem("access_token", response.access_token)
+    navigate({ to: "/" })
+  }
+
+  const logout = () => {
+    localStorage.removeItem("access_token")
+    navigate({ to: "/login" })
+  }
+
+  return { login, logout, user, isLoading }
+}
+
+export { isLoggedIn }
+export default useAuth
