2738537af0746e12cf084bf67c11ec30cbcb1af7
Ivareh <ivareh@ntnu.no>
2024-03-29 14:31:35 +0100

    156 Changed some fonts, coloring and created a theme with these colors

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,7 +1,4 @@
-// 1. Import the extendTheme function
-import { extendTheme } from "@chakra-ui/react";
-
-import React, { StrictMode } from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
@@ -9,16 +6,7 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { ChakraProvider } from "@chakra-ui/react";
 
-// 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-const theme = extendTheme({ colors });
+import theme from "./theme"
 
 // Create a new router instance
 const router = createRouter({ routeTree });
