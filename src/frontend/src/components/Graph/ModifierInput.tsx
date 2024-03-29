adc3a4e3f7b9567ee45712db75ed4fd0f7d54e77
Ivareh <ivareh@ntnu.no>
2024-03-29 16:40:29 +0100

    165 Started on modififerinput getModifiers component

--- src/frontend/src/components/Graph/ModifierInput.tsx
+++ src/frontend/src/components/Graph/ModifierInput.tsx
@@ -7,8 +7,6 @@ import {
 } from "@tanstack/react-query";
 import { useState } from "react";
 
-const queryClient = new QueryClient();
-
 const ModiferInput = () => {
   return (
     <QueryClientProvider client={queryClient}>
@@ -18,37 +16,41 @@ const ModiferInput = () => {
 };
 
 interface Modifier {
-    modifierId: number
-    position: number
-    minRoll: number
-    maxRoll: number
-    textRolls: string
-    static: boolean
-    effect: string
-    regex: string
-    implicit: boolean
-    explicit: boolean
-    delve: boolean
-    fractured: boolean
-    synthesized: boolean
-    corrupted: boolean
-    enchanted: boolean
-    veiled: boolean
+  modifierId: number;
+  position: number;
+  minRoll: number;
+  maxRoll: number;
+  textRolls: string;
+  static: boolean;
+  effect: string;
+  regex: string;
+  implicit: boolean;
+  explicit: boolean;
+  delve: boolean;
+  fractured: boolean;
+  synthesized: boolean;
+  corrupted: boolean;
+  enchanted: boolean;
+  veiled: boolean;
 }
 
 function GetModifiers() {
+  const queryClient = new QueryClient();
+
   const [effectValueSearchInpuut, setEffectValueSearchInput] = useState("");
   const [effectValueList, setEffectValueList] = useState("");
 
   const { isPending, error, data } = useQuery({
-    queryKey: ["repoData"],
+    queryKey: ["modifiers"],
     queryFn: () =>
       fetch("http://localhost/api/api_v1/modifier/").then((res) => res.json()),
-  })
+  });
+
+  console.log(data);
 
   const effects: Array<string> = Array.from(
     new Set(data.map((modifier: Modifier) => modifier.effect))
-  )
+  );
 
   if (isPending) return "Loading...";
 
@@ -66,10 +68,7 @@ function GetModifiers() {
         <MenuButton>Filter by Effect</MenuButton>
         <MenuList maxHeight="200px" overflowY="auto">
           {effects.map((effect, index) => (
-            <MenuItem
-              key={index}
-              onClick={() => setEffectValueList(effect)}
-            >
+            <MenuItem key={index} onClick={() => setEffectValueList(effect)}>
               {effect === effectValueList ? "✓" : ""} {effect}
             </MenuItem>
           ))}
