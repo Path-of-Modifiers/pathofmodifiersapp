2b566ca3b85762ae62fe86348f2cfa4e862f1fba
Ivareh <ivareh@ntnu.no>
2024-03-30 08:51:35 +0100

    165 Modified and added more to modifireinput and mainpage in frontend

--- src/frontend/src/routes/index.lazy.tsx
+++ src/frontend/src/routes/index.lazy.tsx
@@ -3,10 +3,14 @@ import SideBar from "../components/Common/Sidebar";
 import Header from "../components/Common/Header";
 import { Flex } from "@chakra-ui/layout";
 import ModifierInput from "../components/Graph/ModifierInput";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
 });
+
+// Create a client
+const queryClient = new QueryClient()
 
 function Index() {
   return (
@@ -15,7 +19,9 @@ function Index() {
       <Flex flex="1" direction="row">
         <SideBar />
         <Flex flex="1" direction="row" p="1rem" bg="ui.main">
-          <ModifierInput/>
+          <QueryClientProvider client={queryClient}>
+            <ModifierInput />
+          </QueryClientProvider>
         </Flex>
       </Flex>
     </Flex>
