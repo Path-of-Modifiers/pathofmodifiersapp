51c529ba83169011498cd35ebdc2cd7d0b7bc0cf
Bogadisa <magnus@hoddevik.eu>
2024-04-17 20:19:06 +0200

    168 Added Schemas for API calls

---
+++ src/backend/app/app/plotting/schemas/input.py
@@ -0,0 +1,45 @@
+from typing import Optional, List
+import pydantic as _pydantic
+
+from app.core.schemas.item import Influences
+
+
+class ItemSpecs(_pydantic.BaseModel):
+    identified: Optional[bool] = None
+    ilvl: Optional[int] = None
+    rarity: Optional[str] = None
+    corrupted: Optional[bool] = None
+    delve: Optional[bool] = None
+    fractured: Optional[bool] = None
+    synthesized: Optional[bool] = None
+    replica: Optional[bool] = None
+    influences: Optional[Influences] = None
+    searing: Optional[bool] = None
+    tangled: Optional[bool] = None
+    isRelic: Optional[bool] = None
+    foilVariation: Optional[int] = None
+
+
+class BaseSpecs(_pydantic.BaseModel):
+    baseType: Optional[str] = None
+    category: Optional[str] = None
+    subCategory: Optional[str] = None
+
+
+class ModifierLimitations(_pydantic.BaseModel):
+    maxRoll: Optional[float] = None
+    minRoll: Optional[float] = None
+    textRoll: Optional[int] = None
+
+
+class WantedModifier(_pydantic.BaseModel):
+    modifierId: int
+    position: int
+    modifierLimitations: Optional[ModifierLimitations] = None
+
+
+class PlotQuery(_pydantic.BaseModel):
+    league: str
+    itemSpecifications: ItemSpecs
+    baseSpecifications: Optional[BaseSpecs] = None
+    wantedModifiers: List[WantedModifier]
