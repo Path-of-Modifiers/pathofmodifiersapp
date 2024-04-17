62fe9f2a876c0ef02061304613c0fc6bf2821153
Bogadisa <magnus@hoddevik.eu>
2024-04-17 20:19:06 +0200

    168 Added Schemas for API calls

---
+++ src/backend/app/app/plotting/schemas/output.py
@@ -0,0 +1,10 @@
+import datetime as _dt
+from typing import Optional, List
+import pydantic as _pydantic
+
+
+class PlotData(_pydantic.BaseModel):
+    valueInChaos: List[float]
+    timeStamp: List[_dt.datetime]
+    mostCommonCurrencyUsed: str
+    conversionValue: float
