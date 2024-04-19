521fe334be641f71a8c7851cfefaf5164ab34fe1
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-04-19 17:24:39 +0200

    165 Updated frontend with new API structure

---
+++ src/frontend/src/client/models/GroupedModifierByEffect.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type GroupedModifierByEffect = {
+    modifierId: Array<number>;
+    position: Array<number>;
+    minRoll?: null;
+    maxRoll?: null;
+    textRolls?: null;
+    effect: string;
+    static?: null;
+};
+
