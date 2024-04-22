5a58ee81b69cf6273df38971e8b28d2f5f4182fa
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-04-22 10:15:14 +0200

    (main) fixed small mistake | null in GroupedModifierEffect

---
+++ src/frontend/src/client/models/GroupedModifierByEffect.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type GroupedModifierByEffect = {
+  modifierId: Array<number | null>;
+  position: Array<number | null>;
+  minRoll?: Array<number | null> | null;
+  maxRoll?: Array<number | null> | null;
+  textRolls?: Array<string | null> | null;
+  effect: string;
+  static?: Array<boolean | null> | null;
+};
