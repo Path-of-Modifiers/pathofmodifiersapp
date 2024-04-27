e24c3ff3f90f32c42204cf88aa3904d156647de5
Magnus Hoddevik <bjornhod@student.matnat.uio.no>
2024-04-27 16:22:25 +0200

    99 Moved files and renamed them

---
+++ src/backend/data_retrieval/prestart.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+python dataretrieval_pre_start.py
+
+# Create initial data in DB
+python database/modifier_data_deposit/deposit_modifier_data.py
+
+python external_data_retrieval/main.py
