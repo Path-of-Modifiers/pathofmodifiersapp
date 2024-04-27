fd1b33d5b13226237f71312c5fd44c251a8983a0
Magnus Hoddevik <bjornhod@student.matnat.uio.no>
2024-04-27 12:29:57 +0200

    99 Prestart data retrieval

---
+++ src/backend/app/data_retrieval_startup.sh
@@ -0,0 +1,6 @@
+python /app/app/dataretrieval_pre_start.py
+
+# Create initial data in DB
+python /app/app/database/modifier_data_deposit/deposit_modifier_data.py
+
+python /app/app/external_data_retrieval/main.py
