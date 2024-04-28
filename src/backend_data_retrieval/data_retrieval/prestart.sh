045f08691709bc6c0ed7a92b137ab2980cd27097
Magnus Hoddevik <bjornhod@student.matnat.uio.no>
2024-04-28 14:12:34 +0200

    99 Fixed docker container

--- src/backend_data_retrieval/data_retrieval/prestart.sh
+++ src/backend_data_retrieval/data_retrieval/prestart.sh
@@ -3,6 +3,6 @@
 python dataretrieval_pre_start.py
 
 # Create initial data in DB
-python database/modifier_data_deposit/deposit_modifier_data.py
+python modifier_data_deposit/deposit_modifier_data.py
 
 python external_data_retrieval/main.py
