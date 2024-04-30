12ba935f190426d70e601f4a5253a98cfb79e51f
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-04-30 11:56:14 +0200

    165 Restructured item input component

--- src/frontend/src/components/Input/ItemInput.tsx
+++ src/frontend/src/components/Input/ItemInput.tsx
@@ -1,5 +1,34 @@
+import { Flex } from "@chakra-ui/layout";
 import { ItemNameInput } from "./ItemInputComp/ItemNameInput";
+import { ItemRarityInput } from "./ItemInputComp/ItemRarityInput";
+import { IsItemInput } from "./ItemInputComp/IsItemProp";
+import { MinMaxInput } from "./ItemInputComp/MinMaxProp";
 
 export const ItemInput = () => {
-  return <ItemNameInput />;
+  return (
+    <Flex direction={"column"}>
+      <ItemNameInput />
+      <ItemRarityInput />
+      <IsItemInput itemSpecKey={"identified"} text={"Identified"} />
+      <IsItemInput itemSpecKey={"corrupted"} text={"Corrupted"} />
+      <IsItemInput itemSpecKey={"delve"} text={"Delve item"} />
+      <IsItemInput itemSpecKey={"fractured"} text={"Fractured item"} />
+      <IsItemInput itemSpecKey={"synthesized"} text={"Synthesized item"} />
+      <IsItemInput itemSpecKey={"replica"} text={"Replica item"} />
+      <IsItemInput itemSpecKey={"searing"} text={"Exarch influence item"} />
+      <IsItemInput itemSpecKey={"tangled"} text={"Eater influence item"} />
+      <IsItemInput itemSpecKey={"elder"} text={"Elder influence item"} />
+      <IsItemInput itemSpecKey={"shaper"} text={"Shaper influence item"} />
+      <IsItemInput itemSpecKey={"crusader"} text={"Crusader influence item"} />
+      <IsItemInput itemSpecKey={"redeemer"} text={"Redeemer influence item"} />
+      <IsItemInput itemSpecKey={"hunter"} text={"Hunter influence item"} />
+      <IsItemInput itemSpecKey={"warlord"} text={"Warlord influence item"} />
+      <IsItemInput itemSpecKey={"isRelic"} text={"Relic item"} />
+      <MinMaxInput
+        itemMinSpecKey="minIlvl"
+        itemMaxSpecKey="maxIlvl"
+        text="Item level"
+      />
+    </Flex>
+  );
 };
