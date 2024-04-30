ef70fd27380616bc565a6defe5cde3b4af87fbcb
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-04-30 11:56:24 +0200

    165 Removed imports

--- src/frontend/src/components/Input/GraphInput.tsx
+++ src/frontend/src/components/Input/GraphInput.tsx
@@ -1,9 +1,6 @@
 import { Box } from "@chakra-ui/react";
-import { ItemRarityInput } from "./ItemInputComp/ItemRarityInput";
 import { ModifierInput } from "./ModifierInput";
 import { ItemInput } from "./ItemInput";
-import { IsItemInput } from "./ItemInputComp/IsItemProp";
-import { MinMaxInput } from "./ItemInputComp/MinMaxProp";
 
 export const GraphInput = () => {
   return (
