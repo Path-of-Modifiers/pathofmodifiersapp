5e231c9f7ca4a67e33d63461fe6a75652ce6ae63
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-01 15:49:02 +0200

    194 Setup vite config with API url

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -4,6 +4,15 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost/api/api_v1/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [
     // ...,
     TanStackRouterVite(),
