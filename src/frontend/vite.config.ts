53edcb801801d23a60005b7f1dfe5c503f58a690
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-01 16:27:17 +0200

    194 Updated with correct redirection to url

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,15 +1,13 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost/api/api_v1/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+      "/": {
+        target: "http://localhost/",
       },
     },
   },
@@ -17,4 +15,4 @@ export default defineConfig({
     // ...,
     TanStackRouterVite(),
   ],
-})
+});
