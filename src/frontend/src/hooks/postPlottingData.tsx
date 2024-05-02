1b0006db4f88f9471025c79ee9b943b1f6aea4b6
Bogadisa <magnus@hoddevik.eu>
2024-05-02 16:21:32 +0200

    191 Now sends post requests without errors

--- src/frontend/src/hooks/postPlottingData.tsx
+++ src/frontend/src/hooks/postPlottingData.tsx
@@ -2,26 +2,18 @@ import { useState } from "react";
 import {PlottingService, PlotQuery, PlotData } from "../client";
 import { useQuery } from "@tanstack/react-query";
 
-const plotData: PlotData = {
-  valueInChaos: [0, 1, 2, 3],
-  timeStamp: ["2024-05-01T11:16:41.690Z", "2024-05-02T11:16:41.690Z", "2024-05-03T11:16:41.690Z", "2024-05-04T11:16:41.690Z"],
-  mostCommonCurrencyUsed: "divine",
-  conversionValue: [1, 1, 1, 1]
-}
-
 export const PostPlottingData = (requestBody: PlotQuery) => {
   const [responseBody, setPlotData] = useState<PlotData>();
   try {
-    // setPlotData(plotData)
     useQuery({
       queryKey: ["allPlotData"],
       queryFn: async () => {
         setPlotData(
           await PlottingService.getPlotDataApiApiV1PlotPost({requestBody})
         );
+        return 1;
       },
     });
-    console.log(responseBody)
     return responseBody
   } catch (error) {
     console.log(error);
