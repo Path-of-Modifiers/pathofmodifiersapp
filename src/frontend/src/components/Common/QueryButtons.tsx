1199549aa3f7619c577b70e2bd7eb5abe5a216f6
Bogadisa <magnus@hoddevik.eu>
2024-05-06 13:50:04 +0200

    199 Added functionality for showing/hiding filters

--- src/frontend/src/components/Common/QueryButtons.tsx
+++ src/frontend/src/components/Common/QueryButtons.tsx
@@ -1,11 +1,17 @@
 import { Box, Button, ButtonGroup} from "@chakra-ui/react";
-import {useState} from "react";
+import { Dispatch } from "react";
 import { MdExpandMore, MdExpandLess } from "react-icons/md"
 
+interface QueryButtonProps {
+    showingFilter: boolean;
+    setShowingFilter: Dispatch<React.SetStateAction<boolean>>
+  }
 
-const QueryButtons = () => {
-    const [showingFilter, setShowingFilter] = useState(true);
 
+const QueryButtons = ({showingFilter, setShowingFilter}: QueryButtonProps) => {
+    const handleShowingFilter = () => {
+        setShowingFilter(!showingFilter);
+    }
     return <Box bg="ui.main">
         <ButtonGroup variant="solid" colorScheme="red">
             <Button>
@@ -14,8 +20,7 @@ const QueryButtons = () => {
             <Button variant="solid" colorScheme="green">
                 Query and Plot
             </Button>
-            {/* <showFilterButton /> */}
-            <Button variant="solid" colorScheme="gray" rightIcon={showingFilter?<MdExpandLess />:<MdExpandMore />}> 
+            <Button variant="solid" colorScheme="gray" rightIcon={showingFilter?<MdExpandLess />:<MdExpandMore />} onClick={handleShowingFilter}> 
                 {showingFilter?"Hide Filters":"Show Filters"}
             </Button>
         </ButtonGroup>
