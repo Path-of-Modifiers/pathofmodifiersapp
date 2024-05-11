bf060b673c86cc5431e4e10b054eaf7ab7b7dc79
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-11 14:08:39 +0200

    213 Changed env vars, and added to auth token in poe public stashes data retrieval

--- src/backend_data_retrieval/data_retrieval/external_data_retrieval/main.py
+++ src/backend_data_retrieval/data_retrieval/external_data_retrieval/main.py
@@ -25,12 +25,13 @@ logging.basicConfig(
 )
 
 BASEURL = os.getenv("DOMAIN")
+POE_PUBLIC_STASHES_AUTH_TOKEN = os.getenv("POE_PUBLIC_STASHES_AUTH_TOKEN")
 
 
 class ContiniousDataRetrieval:
-    auth_token = "***REMOVED***"
+    auth_token = POE_PUBLIC_STASHES_AUTH_TOKEN
     url = "https://api.pathofexile.com/public-stash-tabs"
-    
+
     if "localhost" not in BASEURL:
         modifier_url = f"https://{BASEURL}"
     else:
