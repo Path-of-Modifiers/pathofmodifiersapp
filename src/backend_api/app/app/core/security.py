46325c067b5f893dcd538aa61d5580c5531bc8ca
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-13 15:18:01 +0200

    Created check_current_user in new core/security.py file

---
+++ src/backend_api/app/app/core/security.py
@@ -0,0 +1,36 @@
+import secrets
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+import os
+
+
+PRIVATIZE_API = os.getenv("PRIVATIZE_API", False)
+FIRST_SUPERUSER = os.getenv("FIRST_SUPERUSER")
+FIRST_SUPERUSER_PASSWORD = os.getenv("FIRST_SUPERUSER_PASSWORD")
+
+security = HTTPBasic()
+
+
+def check_current_user(
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)],
+) -> None:
+    if PRIVATIZE_API:
+        current_username_bytes = credentials.username.encode("utf8")
+        correct_username_bytes = FIRST_SUPERUSER
+        is_correct_username = secrets.compare_digest(
+            current_username_bytes, correct_username_bytes
+        )
+        current_password_bytes = credentials.password.encode("utf8")
+        correct_password_bytes = FIRST_SUPERUSER_PASSWORD
+        is_correct_password = secrets.compare_digest(
+            current_password_bytes, correct_password_bytes
+        )
+        if not (is_correct_username and is_correct_password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Basic"},
+            )
+    return None
