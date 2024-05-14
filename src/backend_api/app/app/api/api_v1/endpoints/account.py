a26fff1f9e7aa444d2d9c70c9fa5b10087d9ee71
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-14 09:57:34 +0200

    212 Added verification dependencies for route /account

--- src/backend_api/app/app/api/api_v1/endpoints/account.py
+++ src/backend_api/app/app/api/api_v1/endpoints/account.py
@@ -10,36 +10,54 @@ import app.core.schemas as schemas
 
 from sqlalchemy.orm import Session
 
+from app.core.security import verification
+
+import os
+
 
 router = APIRouter()
+
+PRIVATIZE_API = os.getenv("PRIVATIZE_API", False)
 
 
 @router.get(
     "/{accountName}",
     response_model=Union[schemas.Account, List[schemas.Account]],
 )
-async def get_account(accountName: str, db: Session = Depends(get_db)):
+async def get_account(
+    accountName: str,
+    db: Session = Depends(get_db),
+    verification: bool = Depends(verification),
+):
     """
-    Get the account by mapping with key and value for "accountName" . 
-    
+    Get the account by mapping with key and value for "accountName" .
+
     Always returns one account.
     """
-    account_map = {"accountName": accountName}
-    account = await CRUD_account.get(db=db, filter=account_map)
+    if verification:
+        account_map = {"accountName": accountName}
+        account = await CRUD_account.get(db=db, filter=account_map)
 
-    return account
+        return account
+    else:
+        return f"Unauthorized to access API in {get_account.__name__}"
 
 
 @router.get("/", response_model=Union[schemas.Account, List[schemas.Account]])
-async def get_all_accounts(db: Session = Depends(get_db)):
+async def get_all_accounts(
+    db: Session = Depends(get_db),
+    verification: bool = Depends(verification),
+):
     """
     Get all accounts.
-    
+
     Returns a list of all accounts.
     """
-    all_accounts = await CRUD_account.get(db=db)
-
-    return all_accounts
+    if verification:
+        all_accounts = await CRUD_account.get(db=db)
+        return all_accounts
+    else:
+        return f"Unauthorized to access API in {get_all_accounts.__name__}"
 
 
 @router.post(
@@ -49,13 +67,17 @@ async def get_all_accounts(db: Session = Depends(get_db)):
 async def create_account(
     account: Union[schemas.AccountCreate, List[schemas.AccountCreate]],
     db: Session = Depends(get_db),
+    verification: bool = Depends(verification),
 ):
     """
     Create one or a list of accounts.
-    
+
     Returns the created account or list of accounts.
     """
-    return await CRUD_account.create(db=db, obj_in=account)
+    if verification:
+        return await CRUD_account.create(db=db, obj_in=account)
+    else:
+        return f"Unauthorized to access API in {create_account.__name__}"
 
 
 @router.put("/{accountName}", response_model=schemas.Account)
@@ -63,30 +85,39 @@ async def update_account(
     accountName: str,
     account_update: schemas.AccountUpdate,
     db: Session = Depends(get_db),
+    verification: bool = Depends(verification),
 ):
     """
     Update an account by key and value for "accountName".
-    
+
     Returns the updated account.
     """
-    account_map = {"accountName": accountName}
-    account = await CRUD_account.get(
-        db=db,
-        filter=account_map,
-    )
-
-    return await CRUD_account.update(db_obj=account, obj_in=account_update, db=db)
+    if verification:
+        account_map = {"accountName": accountName}
+        account = await CRUD_account.get(
+            db=db,
+            filter=account_map,
+        )
+        return await CRUD_account.update(db_obj=account, obj_in=account_update, db=db)
+    else:
+        return f"Unauthorized to access API in {update_account.__name__}"
 
 
 @router.delete("/{accountName}", response_model=str)
-async def delete_account(accountName: str, db: Session = Depends(get_db)):
+async def delete_account(
+    accountName: str,
+    db: Session = Depends(get_db),
+    verification: bool = Depends(verification),
+):
     """
     Delete an account by key and value "accountName".
-    
+
     Returns a message indicating the account was deleted.
     Always deletes one account.
     """
-    account_map = {"accountName": accountName}
-    await CRUD_account.remove(db=db, filter=account_map)
-
-    return f"Account with mapping ({account_map}) deleted successfully"
+    if verification:
+        account_map = {"accountName": accountName}
+        await CRUD_account.remove(db=db, filter=account_map)
+        return f"Account with mapping ({account_map}) deleted successfully"
+    else:
+        return f"Unauthorized to access API in {delete_account.__name__}"
