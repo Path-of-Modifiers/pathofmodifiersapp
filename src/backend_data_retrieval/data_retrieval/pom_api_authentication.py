30c9109d56237756b31ff73175deec2b5eb2a31d
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-14 12:06:09 +0200

    229 Moved pom_api_auth py file in root of container

---
+++ src/backend_data_retrieval/data_retrieval/pom_api_authentication.py
@@ -0,0 +1,11 @@
+from requests.auth import HTTPBasicAuth
+import os
+
+
+FIRST_SUPERUSER = os.getenv("FIRST_SUPERUSER")
+FIRST_SUPERUSER_PASSWORD = os.getenv("FIRST_SUPERUSER_PASSWORD")
+
+
+def get_authentication() -> HTTPBasicAuth:
+    authentication = HTTPBasicAuth(FIRST_SUPERUSER, FIRST_SUPERUSER_PASSWORD)
+    return authentication
