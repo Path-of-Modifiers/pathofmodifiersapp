776cc317ada029c96e9fff6d9a250f50044dee29
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-14 09:45:12 +0200

    212 Adjusted verification function parameters

--- src/backend_api/app/app/core/security.py
+++ src/backend_api/app/app/core/security.py
@@ -13,17 +13,17 @@ FIRST_SUPERUSER_PASSWORD = os.getenv("FIRST_SUPERUSER_PASSWORD")
 security = HTTPBasic()
 
 
-def check_current_user(
-    credentials: Annotated[HTTPBasicCredentials, Depends(security)],
+def verification(
+    credentials: HTTPBasicCredentials = Depends(security),
 ) -> None:
     if PRIVATIZE_API:
         current_username_bytes = credentials.username.encode("utf8")
-        correct_username_bytes = FIRST_SUPERUSER
+        correct_username_bytes = FIRST_SUPERUSER.encode("utf8")
         is_correct_username = secrets.compare_digest(
             current_username_bytes, correct_username_bytes
         )
         current_password_bytes = credentials.password.encode("utf8")
-        correct_password_bytes = FIRST_SUPERUSER_PASSWORD
+        correct_password_bytes = FIRST_SUPERUSER_PASSWORD.encode("utf8")
         is_correct_password = secrets.compare_digest(
             current_password_bytes, correct_password_bytes
         )
