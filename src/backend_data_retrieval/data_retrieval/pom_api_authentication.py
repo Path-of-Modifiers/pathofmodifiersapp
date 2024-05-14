dc0a3008ae3ac0b7ec2020fedd7041ba8d5786fa
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-14 13:45:01 +0200

    229 Renamed to get_super_authentication in pom_api_auth and initialized authentication as self.pom_auth in classes

--- src/backend_data_retrieval/data_retrieval/pom_api_authentication.py
+++ src/backend_data_retrieval/data_retrieval/pom_api_authentication.py
@@ -6,6 +6,6 @@ FIRST_SUPERUSER = os.getenv("FIRST_SUPERUSER")
 FIRST_SUPERUSER_PASSWORD = os.getenv("FIRST_SUPERUSER_PASSWORD")
 
 
-def get_authentication() -> HTTPBasicAuth:
+def get_super_authentication() -> HTTPBasicAuth:
     authentication = HTTPBasicAuth(FIRST_SUPERUSER, FIRST_SUPERUSER_PASSWORD)
     return authentication
