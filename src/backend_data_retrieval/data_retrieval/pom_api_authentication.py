7bffbb55ac99813a6b682ef9e68fef383393e1f0
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-14 13:49:35 +0200

    229 Added documentation for get_super_authentication()

--- src/backend_data_retrieval/data_retrieval/pom_api_authentication.py
+++ src/backend_data_retrieval/data_retrieval/pom_api_authentication.py
@@ -7,5 +7,11 @@ FIRST_SUPERUSER_PASSWORD = os.getenv("FIRST_SUPERUSER_PASSWORD")
 
 
 def get_super_authentication() -> HTTPBasicAuth:
+    """
+    Get the super authentication for the Path of Modifiers API.
+
+    Returns:
+        HTTPBasicAuth: POM user and password authentication.
+    """
     authentication = HTTPBasicAuth(FIRST_SUPERUSER, FIRST_SUPERUSER_PASSWORD)
     return authentication
