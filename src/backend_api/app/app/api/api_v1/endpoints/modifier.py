5b9bce4c6288293f3b2eb3a2d5bca7bd527e7ffd
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-14 10:18:04 +0200

    212 Added security verification dependancies function to rest of API routes DML

--- src/backend_api/app/app/api/api_v1/endpoints/modifier.py
+++ src/backend_api/app/app/api/api_v1/endpoints/modifier.py
@@ -10,6 +10,8 @@ import app.core.schemas as schemas
 
 from sqlalchemy.orm import Session
 
+from app.core.security import verification
+
 
 router = APIRouter()
 
@@ -75,12 +77,16 @@ async def get_grouped_modifier_by_effect(db: Session = Depends(get_db)):
 async def create_modifier(
     modifier: Union[schemas.ModifierCreate, List[schemas.ModifierCreate]],
     db: Session = Depends(get_db),
+    verification: bool = Depends(verification),
 ):
     """
     Create one or a list of new modifiers.
 
     Returns the created modifier or list of modifiers.
     """
+    if not verification:
+        return f"Unauthorized to access API in {create_modifier.__name__}"
+
     return await CRUD_modifier.create(db=db, obj_in=modifier)
 
 
@@ -90,6 +96,7 @@ async def update_modifier(
     position: int,
     modifier_update: schemas.ModifierUpdate,
     db: Session = Depends(get_db),
+    verification: bool = Depends(verification),
 ):
     """
     Update a modifier by key and value for "modifierId" and "position".
@@ -98,6 +105,9 @@ async def update_modifier(
 
     Returns the updated modifier.
     """
+    if not verification:
+        return f"Unauthorized to access API in {update_modifier.__name__}"
+
     modifier_map = {"modifierId": modifierId, "position": position}
     modifier = await CRUD_modifier.get(
         db=db,
@@ -109,7 +119,10 @@ async def update_modifier(
 
 @router.delete("/{modifierId}", response_model=str)
 async def delete_modifier(
-    modifierId: int, position: Optional[int] = None, db: Session = Depends(get_db)
+    modifierId: int,
+    position: Optional[int] = None,
+    db: Session = Depends(get_db),
+    verification: bool = Depends(verification),
 ):
     """
     Delete a modifier by key and value for "modifierId"
@@ -120,6 +133,9 @@ async def delete_modifier(
     Returns a message that the modifier was deleted.
     Always deletes one modifier.
     """
+    if not verification:
+        return f"Unauthorized to access API in {delete_modifier.__name__}"
+
     modifier_map = {"modifierId": modifierId}
     if position is not None:
         modifier_map["position"] = position
