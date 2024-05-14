f780e6f3489c594324d27e354ddf14ae28df7652
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-14 17:33:40 +0200

    226 Imported test_stash to test_item and test_item_modifier

--- src/backend_api/app/app/tests/crud/crud_models/test_item.py
+++ src/backend_api/app/app/tests/crud/crud_models/test_item.py
@@ -1,5 +1,3 @@
-import asyncio
-from sqlalchemy.orm import Session
 from typing import Callable, Dict, Tuple, List, Union
 import pytest
 
@@ -8,9 +6,9 @@ from app.crud import (
     CRUD_account,
     CRUD_itemBaseType,
     CRUD_currency,
+    CRUD_stash,
 )
-from app.core.models.database import engine
-from app.core.models.models import Item, Account, ItemBaseType, Currency
+from app.core.models.models import Account, Item, ItemBaseType, Currency, Stash
 from app.crud.base import CRUDBase
 import app.tests.crud.cascade_tests as cascade_test
 from app.tests.utils.model_utils.item import generate_random_item
@@ -23,14 +21,25 @@ def object_generator_func() -> Callable[[], Dict]:
 
 @pytest.fixture(scope="module")
 def object_generator_func_w_deps() -> (
-    Callable[
-        [], Tuple[Dict, Item, List[Union[Dict, Account, ItemBaseType, Currency]]]
-    ]
+    Callable[[], Tuple[Dict, Item, List[Union[Dict, Stash, ItemBaseType, Currency, Account]]]]
 ):
     def generate_random_item_w_deps(
         db,
     ) -> Callable[
-        [], Tuple[Dict, Item, List[Union[Dict, Account, ItemBaseType, Currency]]]
+        [],
+        Tuple[
+            Dict,
+            Item,
+            List[
+                Union[
+                    Dict,
+                    Stash,
+                    ItemBaseType,
+                    Currency,
+                    Account
+                ]
+            ],
+        ],
     ]:
         return generate_random_item(db, retrieve_dependencies=True)
 
@@ -45,9 +54,10 @@ def crud_instance() -> CRUDBase:
 @pytest.fixture(scope="module")
 def crud_deps_instances() -> CRUDBase:
     return [
-        CRUD_account,
+        CRUD_stash,
         CRUD_itemBaseType,
         CRUD_currency,
+        CRUD_account,
     ]
 
 
