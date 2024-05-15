f76ec4e50280ee092f5edd6c6a9c69fb583ee8c0
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-15 15:52:13 +0200

    91 Removed some dead function, adjusted lines and added docs to mock_database.py

--- src/backend_api/app/app/tests/mock_database.py
+++ src/backend_api/app/app/tests/mock_database.py
@@ -2,21 +2,16 @@ from sqlalchemy import MetaData, create_engine, event
 from app.core.models.database import engine as src_db_engine
 
 
-src_db_metadata = MetaData()
-
 test_db_engine = create_engine(
     "postgresql://test-pom-oltp-user:test-pom-oltp-password@test-db/test-pom-oltp-db"
 )
 
+src_db_metadata = MetaData()
 test_db_metadata = MetaData()
 
 
-@event.listens_for(src_db_metadata, "column_reflect")
-def genericize_datatypes(inspector, tablename, column_dict):
-    column_dict["type"] = column_dict["type"].as_generic(allow_nulltype=True)
-
-
 def mock_src_database_for_test_db():
+    """Mock the source database to create the test database schema."""
     src_conn = src_db_engine.connect()
     tgt_conn = test_db_engine.connect()
     test_db_metadata.reflect(bind=test_db_engine)
@@ -40,4 +35,3 @@ def mock_src_database_for_test_db():
     tgt_conn.commit()
     src_conn.close()
     tgt_conn.close()
-
