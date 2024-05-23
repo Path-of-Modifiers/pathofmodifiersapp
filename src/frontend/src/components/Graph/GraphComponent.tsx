60b07497209a6c43d506d7ed3d3fe8b52372842b
Bogadisa <magnus@hoddevik.eu>
2024-05-23 14:10:08 +0200

    191 Formatting

--- src/frontend/src/components/Graph/GraphComponent.tsx
+++ src/frontend/src/components/Graph/GraphComponent.tsx
@@ -1,37 +1,46 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import getPlotData from '../../hooks/graphing/processPlottingData';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import getPlotData from "../../hooks/graphing/processPlottingData";
 
 interface Datum {
-    date: Date | string,
-    valueInChaos: number,
-    yaxis2?: number
+  date: Date | string;
+  valueInChaos: number;
+  yaxis2?: number;
 }
 
-function GraphComponent () {
-  const data: Datum[] = getPlotData()
+function GraphComponent() {
+  const data: Datum[] = getPlotData();
   return (
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        > 
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Legend verticalAlign="top" height={36}/>
-          <Line type="monotone" dataKey="valueInChaos" stroke="#8884d8" />
-          {/* {data[0].yaxis2 !== undefined && <Line type="monotone" dataKey="yaxis2" stroke="#82ca9d" />} */}
-        </LineChart>
-      </ResponsiveContainer>
-    );
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend verticalAlign="top" height={36} />
+        <Line type="monotone" dataKey="valueInChaos" stroke="#8884d8" />
+        {/* {data[0].yaxis2 !== undefined && <Line type="monotone" dataKey="yaxis2" stroke="#82ca9d" />} */}
+      </LineChart>
+    </ResponsiveContainer>
+  );
 }
 
 export default GraphComponent;
