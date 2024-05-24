6ce89062e35349b7f1990cd370fc5a91ec5656dc
Bogadisa <magnus@hoddevik.eu>
2024-05-24 16:41:58 +0200

    207 Working Query and plot button with loading spinner

--- src/frontend/src/hooks/graphing/processPlottingData.tsx
+++ src/frontend/src/hooks/graphing/processPlottingData.tsx
@@ -1,35 +1,24 @@
-import { PostPlottingData } from "./postPlottingData";
+import PostPlottingData from "./postPlottingData";
 import { PlotQuery } from "../../client";
 import { groupByAndMeanTopN } from "./utils";
 import Datum from "../../schemas/Datum";
-// import { useGraphInputStore } from "../../store/GraphInputStore";
-
-// Will remove when the we hook up the user input
-const plotQuery: PlotQuery = {
-  league: "Necropolis",
-  itemSpecifications: {
-    identified: true,
-  },
-  wantedModifiers: [
-    {
-      modifierId: 61,
-      position: 1,
-      modifierLimitations: {
-        textRoll: 0,
-      },
-    },
-  ],
-};
 /**
  * A function that takes the current plot query and returns
  * data that is ready to be plotted.
  *
  * The data retrieved from the database is grouped by the hour
  * and only the cheapest 100 items that hour are considered.
- * @returns result
+ * @returns The processed data or undefined and the current fetch status.
  */
-const getPlotData = () => {
-  const plotData = PostPlottingData(plotQuery);
+function GetPlotData(plotQuery: PlotQuery): {
+  result: Datum[] | undefined;
+  fetchStatus: string;
+} {
+  const { plotData, fetchStatus } = PostPlottingData(plotQuery);
+  let result: Datum[] | undefined = undefined;
+  if (plotData === undefined) {
+    return { result, fetchStatus };
+  }
 
   const data: Datum[] = [];
   if (plotData?.timeStamp !== undefined) {
@@ -40,8 +29,8 @@ const getPlotData = () => {
       });
     }
   }
-  const result = groupByAndMeanTopN(data, 100);
-  return result;
-};
+  result = groupByAndMeanTopN(data, 100);
+  return { result, fetchStatus };
+}
 
-export default getPlotData;
+export default GetPlotData;
