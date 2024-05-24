c741e55510087d6fe05c0b92ea130055914047c9
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-24 14:53:11 +0200

    259 Updated init to import model_utils for tests

---
+++ src/backend_api/app/app/tests/utils/model_utils/__init__.py
@@ -0,0 +1,13 @@
+from account import create_random_account_dict, generate_random_account
+from currency import create_random_currency_dict, generate_random_currency
+from item_base_type import (
+    create_random_item_base_type_dict,
+    generate_random_item_base_type,
+)
+from item_modifier import (
+    create_random_item_modifier_dict,
+    generate_random_item_modifier,
+)
+from item import create_random_item_dict, generate_random_item
+from modifier import create_random_modifier_dict, generate_random_modifier
+from stash import create_random_stash_dict, generate_random_stash
