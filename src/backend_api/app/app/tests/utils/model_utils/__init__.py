134ff133da3fb78d320f3c0bf8cd38623e5e9f09
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-28 15:54:19 +0200

    259 Adjusted imports for init in model_utils

--- src/backend_api/app/app/tests/utils/model_utils/__init__.py
+++ src/backend_api/app/app/tests/utils/model_utils/__init__.py
@@ -1,13 +1,28 @@
-from account import create_random_account_dict, generate_random_account
-from currency import create_random_currency_dict, generate_random_currency
-from item_base_type import (
+from app.tests.utils.model_utils.account import (
+    create_random_account_dict,
+    generate_random_account,
+)
+from app.tests.utils.model_utils.currency import (
+    create_random_currency_dict,
+    generate_random_currency,
+)
+from app.tests.utils.model_utils.item_base_type import (
     create_random_item_base_type_dict,
     generate_random_item_base_type,
 )
-from item_modifier import (
+from app.tests.utils.model_utils.item_modifier import (
     create_random_item_modifier_dict,
     generate_random_item_modifier,
 )
-from item import create_random_item_dict, generate_random_item
-from modifier import create_random_modifier_dict, generate_random_modifier
-from stash import create_random_stash_dict, generate_random_stash
+from app.tests.utils.model_utils.item import (
+    create_random_item_dict,
+    generate_random_item,
+)
+from app.tests.utils.model_utils.modifier import (
+    create_random_modifier_dict,
+    generate_random_modifier,
+)
+from app.tests.utils.model_utils.stash import (
+    create_random_stash_dict,
+    generate_random_stash,
+)
