7c70069f4e17d0db936a1519c39f9cb44f61982e
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-28 15:54:04 +0200

    259 Created utils for superuser headers, configured fixture for API in conftest

--- src/backend_api/app/app/tests/conftest.py
+++ src/backend_api/app/app/tests/conftest.py
@@ -1,14 +1,25 @@
 from typing import Generator
+from fastapi.testclient import TestClient
 import pytest
 
 from sqlalchemy.orm import Session
 
+from app.main import app
 from app.tests.mock_database import mock_src_database_for_test_db, test_db_engine
 
-@pytest.fixture(scope="session")
+
+
+@pytest.fixture(scope="session", autouse=True)
 def db() -> Generator:
     mock_src_database_for_test_db()
     with Session(test_db_engine) as session:
         yield session
     session.rollback()
     session.close()
+
+
+@pytest.fixture(scope="module")
+def client() -> Generator[TestClient, None, None]:
+    with TestClient(app) as c:
+        yield c
+        
