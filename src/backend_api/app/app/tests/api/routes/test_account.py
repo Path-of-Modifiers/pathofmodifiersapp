27697416105fd7b02530bf241d18d32187cb77d2
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-28 15:53:09 +0200

    259 Configured test_account for await generate db object, adjusted authorization headers (not working)

--- src/backend_api/app/app/tests/api/routes/test_account.py
+++ src/backend_api/app/app/tests/api/routes/test_account.py
@@ -3,22 +3,21 @@ from sqlalchemy.orm import Session
 
 from app.core.models.models import Account as model
 
-import app.app.api.api_v1.endpoints.account as account_api
+import app.api.api_v1.endpoints.account as account_api
 
 from app.core.config import settings
 from app.tests.utils.model_utils import (
     create_random_account_dict,
     generate_random_account,
 )
+from app.tests.api.utils import get_superuser_headers
 
 
-def test_create_account(
-    client: TestClient, superuser_token_headers: dict[str, str]
-) -> None:
+def test_create_account(client: TestClient) -> None:
     account = create_random_account_dict()
     response = client.post(
-        f"{settings.API_V1_STR}/accounts/",
-        headers=superuser_token_headers,
+        f"{settings.API_V1_STR}/account/",
+        headers=get_superuser_headers(),
         json=account,
     )
     assert response.status_code == 200
@@ -29,13 +28,11 @@ def test_create_account(
     assert content["isBanned"] == account["isBanned"]
 
 
-def test_get_account(
-    client: TestClient, superuser_token_headers: dict[str, str], db: Session
-) -> None:
-    account = generate_random_account(db)
+async def test_get_account(client: TestClient, db: Session) -> None:
+    account = await generate_random_account(db)
     response = client.get(
-        f"{settings.API_V1_STR}/accounts/{account.accountName}",
-        headers=superuser_token_headers,
+        f"{settings.API_V1_STR}/account/{account.accountName}",
+        headers=get_superuser_headers(),
     )
     assert response.status_code == 200
     content = response.json()
@@ -44,55 +41,53 @@ def test_get_account(
 
 
 def test_get_account_not_found(
-    client: TestClient, superuser_token_headers: dict[str, str]
+    client: TestClient,
 ) -> None:
+    accountNameTest = "999"
     response = client.get(
-        f"{settings.API_V1_STR}/accounts/999",
-        headers=superuser_token_headers,
+        f"{settings.API_V1_STR}/account/{accountNameTest}",
+        headers=get_superuser_headers(),
     )
     assert response.status_code == 404
     content = response.json()
     assert (
         content["detail"]
-        == f"No object matching the query ({model.accountName.__str__} : {content["accountName"]}) in the table {model.__tablename__} was found."
+        == f"Not Found"
     )
 
 
-def test_get_account_not_enough_permissions(
-    client: TestClient, normal_user_token_headers: dict[str, str], db: Session
-) -> None:
-    account = generate_random_account(db)
+async def test_get_account_not_enough_permissions(client: TestClient, db: Session) -> None:
+    account = await generate_random_account(db)
     response = client.get(
-        f"{settings.API_V1_STR}/accounts/{account.accountName}",
-        headers=normal_user_token_headers,
+        f"{settings.API_V1_STR}/account/{account.accountName}",
+        headers=get_superuser_headers(),
     )
     assert response.status_code == 400
     content = response.json()
-    assert content["detail"] == f"Unauthorize API access for {account_api.get_account.__name__}"
+    assert (
+        content["detail"]
+        == f"Unauthorize API access for {account_api.get_account.__name__}"
+    )
 
 
-def test_get_accounts(
-    client: TestClient, superuser_token_headers: dict[str, str], db: Session
-) -> None:
-    generate_random_account(db)
-    generate_random_account(db)
+async def test_get_account(client: TestClient, db: Session) -> None:
+    await generate_random_account(db)
+    await generate_random_account(db)
     response = client.get(
-        f"{settings.API_V1_STR}/accounts/",
-        headers=superuser_token_headers,
+        f"{settings.API_V1_STR}/account/",
+        headers=get_superuser_headers(),
     )
     assert response.status_code == 200
     content = response.json()
     assert len(content["data"]) >= 2
 
 
-def test_update_account(
-    client: TestClient, superuser_token_headers: dict[str, str], db: Session
-) -> None:
-    account = generate_random_account(db)
+async def test_update_account(client: TestClient, db: Session) -> None:
+    account = await generate_random_account(db)
     updated_account = create_random_account_dict()
     response = client.put(
-        f"{settings.API_V1_STR}/accounts/{account.accountName}",
-        headers=superuser_token_headers,
+        f"{settings.API_V1_STR}/account/{account.accountName}",
+        headers=get_superuser_headers(),
         json=updated_account,
     )
     assert response.status_code == 200
@@ -101,68 +96,74 @@ def test_update_account(
     assert content["isBanned"] == updated_account["isBanned"]
 
 
-def test_update_account_not_found(
-    client: TestClient, superuser_token_headers: dict[str, str]
+async def test_update_account_not_found(
+    client: TestClient,
 ) -> None:
     updated_account = create_random_account_dict()
     response = client.put(
-        f"{settings.API_V1_STR}/accounts/999",
-        headers=superuser_token_headers,
+        f"{settings.API_V1_STR}/account/999",
+        headers=get_superuser_headers(),
         json=updated_account,
     )
     assert response.status_code == 404
     content = response.json()
-    assert content["detail"] == f"No object matching the query ({model.accountName.__str__} : {content["accountName"]}) in the table {model.__tablename__} was found."
+    assert (
+        content["detail"]
+        == f"No object matching the query ({model.accountName.__str__} : {content['accountName']}) in the table {model.__tablename__} was found."
+    )
 
 
-def test_update_account_not_enough_permissions(
-    client: TestClient, normal_user_token_headers: dict[str, str], db: Session
-) -> None:
-    account = generate_random_account(db)
+async def test_update_account_not_enough_permissions(client: TestClient, db: Session) -> None:
+    account = await generate_random_account(db)
     updated_account = create_random_account_dict()
     response = client.put(
-        f"{settings.API_V1_STR}/accounts/{account.accountName}",
-        headers=normal_user_token_headers,
+        f"{settings.API_V1_STR}/account/{account.accountName}",
+        headers=get_superuser_headers(),
         json=updated_account,
     )
     assert response.status_code == 400
     content = response.json()
-    assert content["detail"] == f"Unauthorized API access for {account_api.update_account.__name__}"
+    assert (
+        content["detail"]
+        == f"Unauthorized API access for {account_api.update_account.__name__}"
+    )
 
 
-def test_delete_account(
-    client: TestClient, superuser_token_headers: dict[str, str], db: Session
-) -> None:
-    account = generate_random_account(db)
+async def test_delete_account(client: TestClient, db: Session) -> None:
+    account = await generate_random_account(db)
     response = client.delete(
-        f"{settings.API_V1_STR}/accounts/{account.accountName}",
-        headers=superuser_token_headers,
+        f"{settings.API_V1_STR}/account/{account.accountName}",
+        headers=get_superuser_headers(),
     )
     assert response.status_code == 200
     content = response.json()
-    assert content["message"] == f"Account with mapping ({model.accountName.__str__}: {content["accountName"]}) deleted successfully"
+    assert (
+        content["message"]
+        == f"Account with mapping ({model.accountName.__str__}: {content['accountName']}) deleted successfully"
+    )
 
 
-def test_delete_account_not_found(
-    client: TestClient, superuser_token_headers: dict[str, str]
-) -> None:
-    response = client.delete(
-        f"{settings.API_V1_STR}/accounts/999",
-        headers=superuser_token_headers,
+async def test_delete_account_not_found(client: TestClient) -> None:
+    response = await client.delete(
+        f"{settings.API_V1_STR}/account/999",
+        headers=get_superuser_headers(),
     )
     assert response.status_code == 404
     content = response.json()
-    assert content["detail"] == f"No object matching the query ({model.accountName.__str__}: {content["accountName"]}) in the table {model.__tablename__} was found."
+    assert (
+        content["detail"]
+        == f"No object matching the query ({model.accountName.__str__}: {content['accountName']}) in the table {model.__tablename__} was found."
+    )
 
 
-def test_delete_account_not_enough_permissions(
-    client: TestClient, normal_user_token_headers: dict[str, str], db: Session
-) -> None:
-    account = generate_random_account(db)
+async def test_delete_account_not_enough_permissions(client: TestClient, db: Session) -> None:
+    account = await generate_random_account(db)
     response = client.delete(
-        f"{settings.API_V1_STR}/accounts/{account.accountName}",
-        headers=normal_user_token_headers,
+        f"{settings.API_V1_STR}/account/{account.accountName}",
     )
     assert response.status_code == 400
     content = response.json()
-    assert content["detail"] == f"Unauthorized API access for {account_api.delete_account.__name__}"
+    assert (
+        content["detail"]
+        == f"Unauthorized API access for {account_api.delete_account.__name__}"
+    )
