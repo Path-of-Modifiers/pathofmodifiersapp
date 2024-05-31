f6cccd7538a0025b1468c73a3fa6b48272ba409d
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-31 13:33:59 +0200

    259 Revert back to account how it was originally delete_account feedback

--- src/backend_api/app/app/api/api_v1/endpoints/account.py
+++ src/backend_api/app/app/api/api_v1/endpoints/account.py
@@ -133,7 +133,5 @@ async def delete_account(
 
     account_map = {"accountName": accountName}
     await CRUD_account.remove(db=db, filter=account_map)
-    
-    account_key = list(account_map.keys())[0]
-    account_value = list(account_map.values())[0]
-    return f"Account with mapping ({account_key} : {account_value}) deleted successfully"
+
+    return f"Account with mapping ({account_map}) deleted successfully"
