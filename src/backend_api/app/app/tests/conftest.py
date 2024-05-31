19b2c1b252b8ce7f43a0f6fdf632f3f5c5e16880
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-05-31 08:17:43 +0200

    259 Created superuser headers fixture with its utils, Created clear_db() fixture with its database utils, created database utils and renamed from mock_database.py file

--- src/backend_api/app/app/tests/conftest.py
+++ src/backend_api/app/app/tests/conftest.py
@@ -1,12 +1,19 @@
 from typing import Generator
 from fastapi.testclient import TestClient
 import pytest
+from requests.auth import HTTPBasicAuth
 
 from sqlalchemy.orm import Session
 
 from app.main import app
-from app.tests.mock_database import mock_src_database_for_test_db, test_db_engine
-
+from app.tests.test_database import override_get_db, test_db_engine
+from app.tests.utils.database_utils import (
+    clear_all_tables,
+    mock_src_database_for_test_db,
+)
+from app.core.models.database import Base
+from app.api.deps import get_db
+from app.tests.utils.utils import get_super_authentication
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -19,7 +26,19 @@ def db() -> Generator:
 
 
 @pytest.fixture(scope="module")
+def superuser_headers() -> HTTPBasicAuth:
+    return get_super_authentication()
+
+
+@pytest.fixture(scope="module")
 def client() -> Generator[TestClient, None, None]:
+    app.dependency_overrides[get_db] = override_get_db
     with TestClient(app) as c:
         yield c
-        
+
+
+@pytest.fixture(scope="module")
+def clear_db() -> Generator:
+    yield
+    # Remove any data from database (even data not created by this session)
+    clear_all_tables()
