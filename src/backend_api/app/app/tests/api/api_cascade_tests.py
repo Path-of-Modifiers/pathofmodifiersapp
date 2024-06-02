68103f11f045e8fc425744ffc21f6fd626ee527b
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-02 10:06:11 +0200

    259 Created cascade update tests for api, fixed some function return parameters

--- src/backend_api/app/app/tests/api/api_cascade_tests.py
+++ src/backend_api/app/app/tests/api/api_cascade_tests.py
@@ -1,12 +1,15 @@
-from typing import Dict, List, Optional, Tuple, Union
+from copy import deepcopy
+from typing import Callable, Dict, List, Optional, Tuple, Union
 from fastapi.testclient import TestClient
 import pytest
 from typing import Dict, List, Optional, Tuple, Union
+from sqlalchemy.orm import Session
 
 from app.core.config import settings
 from app.tests.api.api_test_base import TestAPI
 from app.tests.crud.cascade_tests import TestCascade as UtilTestCascade
 from app.crud.base import CRUDBase, ModelType
+from app.tests.utils.utils import random_based_on_type
 
 
 @pytest.mark.usefixtures("clear_db", autouse=True)
@@ -19,7 +22,7 @@ class TestCascadeAPI(TestAPI):
         ],
         get_crud_test_cascade_model: UtilTestCascade,
         retrieve_dependencies: Optional[bool] = False,
-    ) -> Tuple[Dict, ModelType, Optional[List[Union[Dict, ModelType]]]]:
+    ) -> Tuple[Dict, ModelType, Dict, Optional[List[Union[Dict, ModelType]]]]:
         """A private method used to create objects, with the option to retrieve dependencies.
 
         Args:
@@ -78,13 +81,12 @@ class TestCascadeAPI(TestAPI):
         model: ModelType,
         deps: List[Union[Dict, ModelType]],
         get_crud_test_cascade_model: UtilTestCascade,
-    ) -> List[str]:
+    ) -> Dict[str, str]:
         """
         Retrieves all tables related to model, which delete all dependent entries
         """
         return get_crud_test_cascade_model._find_cascading_update(model, deps)
 
-    
     @pytest.mark.asyncio
     async def test_cascade_delete(
         self,
@@ -97,7 +99,7 @@ class TestCascadeAPI(TestAPI):
         unique_identifier: str,
         get_crud_test_cascade_model: UtilTestCascade,
         superuser_headers: Dict,
-        api_deps_instances: List[Dict],
+        api_deps_instances: List[List[str]],
     ) -> None:
         """
         A method for testing cascade delete
@@ -139,9 +141,10 @@ class TestCascadeAPI(TestAPI):
 
             # Get key name of dict api_deps_instances[i]
             print("YIDA", api_deps_instances, i)
-            dep_route_name = list(api_deps_instances[i].keys())[0]
+            dep_route_name = api_deps_instances[i][0]
             print("DEP_ROUTE_NAME", dep_route_name)
-            dep_unique_identifier = api_deps_instances[i].get(dep_route_name)
+            dep_unique_identifier = api_deps_instances[i][1]
+            print("DEP_UNIQUE_IDENTIFIER", dep_unique_identifier)
 
             response_dep = client.delete(
                 f"{settings.API_V1_STR}/{dep_route_name}/{object_out_dict[dep_unique_identifier]}",
@@ -166,3 +169,130 @@ class TestCascadeAPI(TestAPI):
             )
             print("HAGLEBU", response)
             assert response.status_code == 404
+
+    @pytest.mark.asyncio
+    async def test_cascade_update(
+        self,
+        db: pytest.Session,
+        object_generator_func_w_deps: Tuple[
+            Dict, ModelType, Optional[List[Union[Dict, ModelType]]]
+        ],
+        client: TestClient,
+        route_name: str,
+        unique_identifier: str,
+        get_crud_test_cascade_model: UtilTestCascade,
+        superuser_headers: Dict,
+        api_deps_instances: List[List[str]],
+    ) -> None:
+        """
+        A test function.
+
+        1. Creates the object
+        2. Uses the object to determine which tables cascade
+        3. Counts how many dependencies there are
+        4. Loops through all dependencies, ignoring non cascading tables
+        5. Updates a dependency
+        6. Checks if the update affects the dependent
+        """
+        _, obj, _, deps = await self._create_object_cascade(
+            db,
+            object_generator_func_w_deps,
+            get_crud_test_cascade_model,
+            retrieve_dependencies=True,
+        )
+
+        cascading_tables = self._find_cascading_update(
+            obj, deps, get_crud_test_cascade_model
+        )
+
+        n_deps = len(deps) // 2
+        for i in range(n_deps):
+            # object_dict, object_out, _, deps = await self._create_object_cascade(
+            #     db,
+            #     object_generator_func_w_deps,
+            #     get_crud_test_cascade_model,
+            #     retrieve_dependencies=True,
+            # )
+            object_dict, object_out, object_out_dict, deps = (
+                await self._create_object_cascade(
+                    db,
+                    object_generator_func_w_deps,
+                    get_crud_test_cascade_model,
+                    retrieve_dependencies=True,
+                )
+            )  # New objects have to be created for every test, since they are constantly being deleted
+            self._test_object(get_crud_test_cascade_model, object_out, object_dict)
+            obj_map = self._create_primary_key_map(
+                object_out, get_crud_test_cascade_model
+            )  # Needs to be a global utility function
+
+            dep_dict, dep_model = deps[2 * i], deps[2 * i + 1]
+
+            self._test_object(get_crud_test_cascade_model, dep_model, dep_dict)
+            if dep_model.__tablename__ not in cascading_tables:
+                continue
+
+            cascading_tables_keys = cascading_tables[dep_model.__tablename__]
+            new_dep_dict = deepcopy(dep_dict)
+
+            for key in cascading_tables_keys:
+                if key not in object_dict or key not in dep_dict:
+                    continue
+                new_key_value = random_based_on_type(dep_dict[key])
+                new_dep_dict[key] = new_key_value
+
+                if key in obj_map:
+                    obj_map[key] = new_key_value
+
+                assert object_dict[key] != new_dep_dict[key]
+
+            # updated_dep_model = await crud_deps_instances[i].update(
+            #     db, db_obj=dep_model, obj_in=new_dep_dict
+            # )
+            dep_route_name = api_deps_instances[i][0]
+            dep_unique_identifier = api_deps_instances[i][1]
+            print(
+                "ABOBA",
+                dep_route_name,
+                dep_unique_identifier,
+                "!!!",
+                new_dep_dict[dep_unique_identifier],
+            )
+
+            # Update the dependency
+            response_update_dep = client.put(
+                f"{settings.API_V1_STR}/{dep_route_name}/{dep_dict[dep_unique_identifier]}",
+                auth=superuser_headers,
+                json=new_dep_dict,
+            )
+
+            updated_dep_model_content = response_update_dep.json()
+            print("UPDATED_DEP_MODEL_DARK_VADER", updated_dep_model_content)
+            print("RESPONSEVADER_STATUS", response_update_dep.status_code)
+            assert response_update_dep.status_code == 200
+            self._compare_dicts(
+                updated_dep_model_content,
+                new_dep_dict,
+                ignore=["updatedAt", "createdAt"],
+            )
+
+            # Get the original model
+            response_get_model = client.get(
+                f"{settings.API_V1_STR}/{route_name}/{object_out_dict[unique_identifier]}",
+                auth=superuser_headers,
+            )
+
+            assert response_get_model.status_code == 200
+
+            get_model_content = response_get_model.json()
+
+            print("GRAVERN", updated_dep_model_content)
+            print("SJUKERN", object_dict)
+
+            # Check if the updated dependency affects the dependent original model
+            for key in cascading_tables_keys:
+                assert get_model_content[key] == updated_dep_model_content[key]
+            # assert updated_dep_model[dep_unique_identifier] == object_dict[dep_unique_identifier]
+            # new_updated_object = await crud_instance.get(db, filter=obj_map)
+
+            # self._test_object(new_updated_object, object_dict)
