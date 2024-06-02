5be97ee960fa9cf715d573c0955359214fe22313
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-02 10:05:36 +0200

    259 Added prefix route name as variables for global use

--- src/backend_api/app/app/api/api_v1/api.py
+++ src/backend_api/app/app/api/api_v1/api.py
@@ -13,15 +13,40 @@ from app.api.api_v1.endpoints import (
 
 api_router = APIRouter()
 
-api_router.include_router(account.router, prefix="/account", tags=["accounts"])
-api_router.include_router(currency.router, prefix="/currency", tags=["currencies"])
+account_prefix = "account"
+currency_prefix = "currency"
+item_base_type_prefix = "itemBaseType"
+item_modifier_prefix = "itemModifier"
+item_prefix = "item"
+modifier_prefix = "modifier"
+stash_prefix = "stash"
+plot_prefix = "plot"
+
 api_router.include_router(
-    item_base_type.router, prefix="/itemBaseType", tags=["itemBaseTypes"]
+    account.router, prefix=f"/{account_prefix}", tags=[f"{account_prefix}s"]
 )
 api_router.include_router(
-    item_modifier.router, prefix="/itemModifier", tags=["itemModifiers"]
+    currency.router, prefix=f"/{currency_prefix}", tags=[f"{currency_prefix}s"]
 )
-api_router.include_router(item.router, prefix="/item", tags=["items"])
-api_router.include_router(modifier.router, prefix="/modifier", tags=["modifiers"])
-api_router.include_router(stash.router, prefix="/stash", tags=["stashes"])
-api_router.include_router(plot.router, prefix="/plot", tags=["plotting"])
+api_router.include_router(
+    item_base_type.router,
+    prefix=f"/{item_base_type_prefix}",
+    tags=[f"{item_base_type_prefix}s"],
+)
+api_router.include_router(
+    item_modifier.router,
+    prefix=f"/{item_modifier_prefix}",
+    tags=[f"{item_modifier_prefix}s"],
+)
+api_router.include_router(
+    item.router, prefix=f"/{item_prefix}", tags=[f"{item_prefix}s"]
+)
+api_router.include_router(
+    modifier.router, prefix=f"/{modifier_prefix}", tags=[f"{modifier_prefix}s"]
+)
+api_router.include_router(
+    stash.router, prefix=f"/{stash_prefix}", tags=[f"{stash_prefix}s"]
+)
+api_router.include_router(
+    plot.router, prefix=f"/{plot_prefix}", tags=[f"{plot_prefix}s"]
+)
