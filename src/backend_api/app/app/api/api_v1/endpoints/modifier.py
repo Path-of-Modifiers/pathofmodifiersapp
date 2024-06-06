834fdb2e8ff28399d0fe340100fac81400bf179e
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-06 15:35:13 +0200

    259 Updated some params in modifier api endpoint

--- src/backend_api/app/app/api/api_v1/endpoints/modifier.py
+++ src/backend_api/app/app/api/api_v1/endpoints/modifier.py
@@ -25,19 +25,17 @@ modifier_prefix = "modifier"
     response_model=Union[schemas.Modifier, List[schemas.Modifier]],
 )
 async def get_modifier(
-    modifierId: str, position: Optional[int] = None, db: Session = Depends(get_db)
+    modifierId: str, db: Session = Depends(get_db)
 ):
     """
     Get modifier or list of modifiers by key and
-    value for "modifierId" and optional "position"
+    value for "modifierId"
 
     Dominant key is "modifierId".
 
     Returns one or a list of modifiers.
     """
     modifier_map = {"modifierId": modifierId}
-    if position is not None:
-        modifier_map["position"] = position
     modifier = await CRUD_modifier.get(db=db, filter=modifier_map)
 
     return modifier
@@ -101,12 +99,11 @@ async def create_modifier(
 async def update_modifier(
     modifierId: int,
     modifier_update: schemas.ModifierUpdate,
-    position: Optional[int] = None,
     db: Session = Depends(get_db),
     verification: bool = Depends(verification),
 ):
     """
-    Update a modifier by key and value for "modifierId" and optional "position".
+    Update a modifier by key and value for "modifierId"
 
     Dominant key is "modifierId".
 
@@ -119,8 +116,6 @@ async def update_modifier(
         )
 
     modifier_map = {"modifierId": modifierId}
-    if position is not None:
-        modifier_map["position"] = position
 
     modifier = await CRUD_modifier.get(
         db=db,
@@ -133,15 +128,11 @@ async def update_modifier(
 @router.delete("/{modifierId}", response_model=str)
 async def delete_modifier(
     modifierId: int,
-    position: Optional[int] = None,
     db: Session = Depends(get_db),
     verification: bool = Depends(verification),
 ):
     """
     Delete a modifier by key and value for "modifierId"
-    and optional "position".
-
-    Dominant key is "modifierId".
 
     Returns a message that the modifier was deleted.
     Always deletes one modifier.
@@ -153,8 +144,6 @@ async def delete_modifier(
         )
 
     modifier_map = {"modifierId": modifierId}
-    if position is not None:
-        modifier_map["position"] = position
     await CRUD_modifier.remove(db=db, filter=modifier_map)
 
     return get_delete_return_message(modifier_prefix, modifier_map)
