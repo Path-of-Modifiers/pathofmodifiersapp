1fa29b8831b0f8a6035b5be1b75b01a162223702
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-06 09:14:36 +0200

    259 Cleaned some import issues, function types and ignore columns

--- src/backend_api/app/app/tests/api/routes/test_currency.py
+++ src/backend_api/app/app/tests/api/routes/test_currency.py
@@ -1,7 +1,7 @@
 from typing import Callable, Dict, List, Tuple
-
 import pytest
-import backend_api.app.app.tests.api.api_routes_test_base as test_api
+
+import app.tests.api.api_routes_test_base as test_api
 from app.tests.utils.model_utils.currency import (
     create_random_currency_dict,
     generate_random_currency,
