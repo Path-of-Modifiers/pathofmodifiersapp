0e6a58423810ac27442a4ddcd6aac1d45793f4ed
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-08 10:34:15 +0200

    259 Removed unused themes from theme.tsx and added inputSizes theme attributes

--- src/frontend/src/theme.tsx
+++ src/frontend/src/theme.tsx
@@ -1,6 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
 
-// example theme
 const theme = extendTheme({
   colors: {
     ui: {
@@ -20,21 +19,6 @@ const theme = extendTheme({
     heading: "Josefin-Sans, Georgia, serif",
     sidebar: "Inter, serif",
   },
-  fontSizes: {
-    xs: "0.75rem",
-    sm: "0.875rem",
-    md: "1rem",
-    lg: "1.125rem",
-    xl: "1.25rem",
-    "2xl": "1.5rem",
-    "3xl": "1.875rem",
-    "4xl": "2.25rem",
-    "5xl": "3rem",
-    "6xl": "3.75rem",
-    "7xl": "4.5rem",
-    "8xl": "6rem",
-    "9xl": "8rem",
-  },
   fontWeights: {
     hairline: 100,
     thin: 200,
@@ -45,6 +29,11 @@ const theme = extendTheme({
     bold: 700,
     extrabold: 800,
     black: 900,
+  },
+  fontSizes: {
+    defaultRead: "1rem",
+    input: "1.2rem",
+    menu: "1.325rem",
   },
   lineHeights: {
     normal: "normal",
@@ -71,40 +60,11 @@ const theme = extendTheme({
     wider: "0.05em",
     widest: "0.1em",
   },
-  space: {
-    px: '1px',
-    0.5: '0.125rem',
-    1: '0.25rem',
-    1.5: '0.375rem',
-    2: '0.5rem',
-    2.5: '0.625rem',
-    3: '0.75rem',
-    3.5: '0.875rem',
-    4: '1rem',
-    5: '1.25rem',
-    6: '1.5rem',
-    7: '1.75rem',
-    8: '2rem',
-    9: '2.25rem',
-    10: '2.5rem',
-    12: '3rem',
-    14: '3.5rem',
-    16: '4rem',
-    20: '5rem',
-    24: '6rem',
-    28: '7rem',
-    32: '8rem',
-    36: '9rem',
-    40: '10rem',
-    44: '11rem',
-    48: '12rem',
-    52: '13rem',
-    56: '14rem',
-    60: '15rem',
-    64: '16rem',
-    72: '18rem',
-    80: '20rem',
-    96: '24rem',
+  sizes: {
+    inputSizes: {
+      defaultSelect: "11rem",
+      defaultDescriptionText: "11rem",
+    },
   },
 });
 
