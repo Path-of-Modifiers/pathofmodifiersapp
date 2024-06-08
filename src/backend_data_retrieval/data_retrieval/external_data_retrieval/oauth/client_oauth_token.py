bf6682aeb300feaade10f7446c609c5a3c3cafc6
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-08 13:52:18 +0200

    280 Formatted client_oath_token.py and added contact_email to get_access_token function

--- src/backend_data_retrieval/data_retrieval/external_data_retrieval/oauth/client_oauth_token.py
+++ src/backend_data_retrieval/data_retrieval/external_data_retrieval/oauth/client_oauth_token.py
@@ -2,9 +2,10 @@ import requests
 import os
 
 
-OATH_ACC_TOKEN_CONTACT_EMAIL = os.getenv("OATH_ACC_TOKEN_CONTACT_EMAIL")
 OAUTH_CLIENT_ID = os.getenv("OAUTH_CLIENT_ID")
 OAUTH_CLIENT_SECRET = os.getenv("OAUTH_CLIENT_SECRET")
+OATH_ACC_TOKEN_CONTACT_EMAIL = os.getenv("OATH_ACC_TOKEN_CONTACT_EMAIL")
+
 
 def error_reason(response):
     print(response.status_code)
@@ -13,12 +14,16 @@ def error_reason(response):
     quit()
 
 
-def get_access_token(url: str, client_id: str, client_secret: str, contact_email: str) -> str:
+def get_access_token(
+    url: str, client_id: str, client_secret: str, contact_email: str
+) -> str:
     """
     https://stackoverflow.com/questions/36719540/how-can-i-get-an-oauth2-access-token-using-python
     """
     url = "https://www.pathofexile.com/oauth/token"
-    head_user_agent = f"OAuth pathofmodifiers/0.1.0 (contact: {contact_email}) StrictMode"
+    head_user_agent = (
+        f"OAuth pathofmodifiers/0.1.0 (contact: {contact_email}) StrictMode"
+    )
     response = requests.post(
         url,
         data={
@@ -44,7 +49,13 @@ def main():
     url = "https://www.pathofexile.com/oauth/token"
     client_id = OAUTH_CLIENT_ID
     client_secret = OAUTH_CLIENT_SECRET  # Generated at: https://www.pathofexile.com/my-account/applications/pathofmodifiers/manage
-    token = get_access_token(url=url, client_id=client_id, client_secret=client_secret)
+    contact_email = OATH_ACC_TOKEN_CONTACT_EMAIL
+    token = get_access_token(
+        url=url,
+        client_id=client_id,
+        client_secret=client_secret,
+        contact_email=contact_email,
+    )
     print(f"Generated a new token:\n {token}")
     return 0
 
