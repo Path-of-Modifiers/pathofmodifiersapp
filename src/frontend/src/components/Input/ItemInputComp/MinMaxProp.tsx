579bee8456c1aa6aedd7bb8986769ce0d5115905
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-08 11:03:48 +0200

    198 Created generalized MinMaxNumberInput component with MinMaxProp to inherit it

--- src/frontend/src/components/Input/ItemInputComp/MinMaxProp.tsx
+++ src/frontend/src/components/Input/ItemInputComp/MinMaxProp.tsx
@@ -1,13 +1,5 @@
-import {
-  Flex,
-  Text,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-} from "@chakra-ui/react";
 import { useGraphInputStore } from "../../../store/GraphInputStore";
+import { MinMaxNumberInput } from "../StandardLayoutInput/MinMaxNumberInput";
 
 interface MinMaxInputProps {
   itemMinSpecKey: string;
@@ -16,7 +8,11 @@ interface MinMaxInputProps {
 }
 
 // Min Max Item Lvl Input Component  -  This component is used to input the min and max ilvl of an item.
-export const MinMaxInput = ({ text }: MinMaxInputProps) => {
+export const MinMaxInput = ({
+  itemMinSpecKey,
+  itemMaxSpecKey,
+  text,
+}: MinMaxInputProps) => {
   const { setItemSpecMinIlvl, setItemSpecMaxIlvl } = useGraphInputStore();
 
   const [minValue, maxValue] = useGraphInputStore((state) => [
@@ -52,61 +48,15 @@ export const MinMaxInput = ({ text }: MinMaxInputProps) => {
     setItemSpecMaxIlvl(eventValueInt);
   };
 
-  // useEffect(() => {}, [clearClicked]);
-
   return (
-    <Flex
-      color={"ui.white"}
-      m={2}
-      ml={1}
-      bgColor={"ui.secondary"}
-      alignItems={"center"}
-    >
-      <Text ml={1} width={150}>
-        {text}
-      </Text>
-      <NumberInput
-        value={getMinValue()}
-        step={1}
-        key={"itemSpecKey" + "min_number_input"}
-        bgColor={"ui.input"}
-        precision={0}
-        focusBorderColor={"ui.white"}
-        borderColor={"ui.grey"}
-        onChange={(e) => handleMinChange(e)}
-        width={150}
-        mr={1}
-        ml={1}
-        _placeholder={{ color: "ui.white" }}
-        textAlign={"center"}
-      >
-        <NumberInputField placeholder={"Min"} />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
-
-      <NumberInput
-        value={getMaxValue()}
-        step={1}
-        key={"MinMaxInput" + "max_number_item_input"}
-        bgColor={"ui.input"}
-        focusBorderColor={"ui.white"}
-        borderColor={"ui.grey"}
-        onChange={(e) => handleMaxChange(e)}
-        width={150}
-        mr={1}
-        ml={1}
-        _placeholder={{ color: "ui.white" }}
-        textAlign={"center"}
-      >
-        <NumberInputField placeholder={"Max"} />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
-    </Flex>
+    <MinMaxNumberInput
+      text={text}
+      minSpecKey={itemMinSpecKey}
+      maxSpecKey={itemMaxSpecKey}
+      getMinValue={getMinValue}
+      getMaxValue={getMaxValue}
+      handleMinChange={handleMinChange}
+      handleMaxChange={handleMaxChange}
+    />
   );
 };
