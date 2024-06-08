ab50da50146153ce908b2b0c7589a21ae43c7672
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-08 13:40:34 +0200

    280 Made contact_email, client_id and client_secret a secret in .env file

--- src/backend_data_retrieval/data_retrieval/external_data_retrieval/oauth/client_oauth_token.py
+++ src/backend_data_retrieval/data_retrieval/external_data_retrieval/oauth/client_oauth_token.py
@@ -1,5 +1,10 @@
 import requests
+import os
 
+
+OATH_ACC_TOKEN_CONTACT_EMAIL = os.getenv("OATH_ACC_TOKEN_CONTACT_EMAIL")
+OAUTH_CLIENT_ID = os.getenv("OAUTH_CLIENT_ID")
+OAUTH_CLIENT_SECRET = os.getenv("OAUTH_CLIENT_SECRET")
 
 def error_reason(response):
     print(response.status_code)
@@ -8,11 +13,12 @@ def error_reason(response):
     quit()
 
 
-def get_access_token(url: str, client_id: str, client_secret: str) -> str:
+def get_access_token(url: str, client_id: str, client_secret: str, contact_email: str) -> str:
     """
     https://stackoverflow.com/questions/36719540/how-can-i-get-an-oauth2-access-token-using-python
     """
     url = "https://www.pathofexile.com/oauth/token"
+    head_user_agent = f"OAuth pathofmodifiers/0.1.0 (contact: {contact_email}) StrictMode"
     response = requests.post(
         url,
         data={
@@ -24,7 +30,7 @@ def get_access_token(url: str, client_id: str, client_secret: str) -> str:
         headers={
             "Content-Type": "application/x-www-form-urlencoded",
             "accept": "application/json",
-            "User-Agent": "OAuth pathofmodifiers/0.1.0 (contact: magnus.hoddevik@gmail.com) StrictMode",
+            f"User-Agent": head_user_agent,
         },
     )  # Sends necessary information required to generate a new OAuth2 Access Token
     if (
@@ -36,8 +42,8 @@ def get_access_token(url: str, client_id: str, client_secret: str) -> str:
 
 def main():
     url = "https://www.pathofexile.com/oauth/token"
-    client_id = "pathofmodifiers"
-    client_secret = "iF1xVCcdhFu9"  # Generated at: https://www.pathofexile.com/my-account/applications/pathofmodifiers/manage
+    client_id = OAUTH_CLIENT_ID
+    client_secret = OAUTH_CLIENT_SECRET  # Generated at: https://www.pathofexile.com/my-account/applications/pathofmodifiers/manage
     token = get_access_token(url=url, client_id=client_id, client_secret=client_secret)
     print(f"Generated a new token:\n {token}")
     return 0
