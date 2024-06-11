677c98e00b24042b5ee07ae3f138d5956cc11b45
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-11 15:42:40 +0200

    286 Debugging the scripts and gitignore

--- src/frontend/hidden_content/replaceGraphing_clean_script.sh
+++ src/frontend/hidden_content/replaceGraphing_clean_script.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Define relative paths or use environment variables
-source_dir="../src/hooks/graphing"
-destination_dir="./graphing_clean"
+source_dir="./graphing_smudged"
+smudged_graphing="../src/hooks/graphing"
 
 # Get the absolute path of the current directory
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Construct absolute paths
 source_path="$current_dir/$source_dir"
-destination_path="$current_dir/$destination_dir"
+destination_path="$current_dir/$smudged_graphing"
 
 # Copy files from source to destination
-cp -r "$source_path"/* "$destination_path"/
+cp -r "$source_path"/utils.tsx "$destination_path"/utils.tsx
+cp -r "$source_path"/processPlottingData.tsx "$destination_path"/processPlottingData.tsx
