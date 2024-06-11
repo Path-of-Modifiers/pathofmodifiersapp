713cb5ce889a235ed49d2d1f79beeb51e538034f
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-11 13:49:12 +0200

    286 Final adjustments to the gitattributes and replacement files

--- src/frontend/hidden_content/replaceGraphing_smudge_script.sh
+++ src/frontend/hidden_content/replaceGraphing_smudge_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define relative paths or use environment variables
-source_dir="./graphing_smudged"
-destination_dir="../src/hooks/graphing"
+source_dir="../src/hooks/graphing"
+destination_dir="./graphing"
 
 # Get the absolute path of the current directory
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -13,5 +13,3 @@ destination_path="$current_dir/$destination_dir"
 
 # Copy files from source to destination
 cp -r "$source_path"/* "$destination_path"/
-
-echo "Frontend graphing hooks replaced with smudged version."
