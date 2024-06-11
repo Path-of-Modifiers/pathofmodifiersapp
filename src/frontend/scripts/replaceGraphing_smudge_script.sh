054a0373304e888c24fc63b71f17d286a0bce377
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-11 11:22:46 +0200

    286 Adjusted scripts to clean and smudge, and adjusted .gitignore to hidden module

--- src/frontend/scripts/replaceGraphing_smudge_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Define paths
-source_dir="D:/Proggeprosjekter/POE/hidden_content/graphing"
-destination_dir="D:/Proggeprosjekter/POE/pathofmodifiersapp/src/frontend/src/hooks/graphing"
-
-# Copy files from source to destination
-cp -r "$source_dir"/* "$destination_dir"/
