38b8b7dd61f8fdd25423b82dd3de171dd811d5d3
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-11 14:23:41 +0200

    286 Testing with these configs for script files

--- src/frontend/hidden_content/replaceGraphing_clean_script.sh
+++ src/frontend/hidden_content/replaceGraphing_clean_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define relative paths or use environment variables
-source_dir="./graphing"
-destination_dir="../src/hooks/graphing"
+source_dir="../src/hooks/graphing"
+destination_dir="./graphing"
 
 # Get the absolute path of the current directory
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
