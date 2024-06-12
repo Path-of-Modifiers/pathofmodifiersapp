a04b6eb96fb8d5fd8bbb951eb8f12c2f9be0afdf
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-12 15:14:42 +0200

    286 Fixed pathing in smudge script

--- src/frontend/hidden_content/replaceGraphingSmudgeScript.sh
+++ src/frontend/hidden_content/replaceGraphingSmudgeScript.sh
@@ -6,7 +6,9 @@ destination_dir="../src/hooks/graphing"
 
 # Get the absolute path of the current directory
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-	@@ -12,6 +12,4 @@ source_path="$current_dir/$source_dir"
+
+# Construct absolute paths
+source_path="$current_dir/$source_dir"
 destination_path="$current_dir/$destination_dir"
 
 # Copy files from source to destination
