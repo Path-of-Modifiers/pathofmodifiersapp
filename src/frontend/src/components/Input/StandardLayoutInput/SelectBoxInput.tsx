8274b363eaeb75307cb84de2f6c75b3c75576e27
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-06-14 09:19:10 +0200

    279 Adjusted select box input component to be searchable

--- src/frontend/src/components/Input/StandardLayoutInput/SelectBoxInput.tsx
+++ src/frontend/src/components/Input/StandardLayoutInput/SelectBoxInput.tsx
@@ -1,7 +1,16 @@
-import { Flex, Text } from "@chakra-ui/layout";
-import { Select } from "@chakra-ui/react";
-import { GetValueFunction, HandleChangeEventFunction } from "../../../schemas/function/InputFunction";
-
+import { Flex } from "@chakra-ui/layout";
+import {
+  GetValueFunction,
+  HandleChangeEventFunction,
+} from "../../../schemas/function/InputFunction";
+import {
+  AutoComplete,
+  AutoCompleteInput,
+  AutoCompleteItem,
+  AutoCompleteList,
+} from "@choc-ui/chakra-autocomplete";
+import { FormControl, FormLabel } from "@chakra-ui/react";
+import { useState } from "react";
 
 export interface SelectBoxProps {
   descriptionText: string;
@@ -15,58 +24,74 @@ export interface SelectBoxProps {
 
 export type SelectBoxOptionValue = { value: string; text: string };
 
-export const SelectBox = ({
+export const SelectBoxInput = ({
   descriptionText,
   optionsList,
   itemKeyId,
-  defaultValue,
   defaultText,
   getSelectValue,
   handleChange,
 }: SelectBoxProps) => {
+  const [inputText, setInputText] = useState<string>(defaultText);
+
+  const selectValue = getSelectValue();
+
+  const handeSetInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target_value = e.currentTarget.value;
+    setInputText(target_value);
+    handleChange(e);
+  };
+
+  const handleChangeWithText = (
+    e: React.FormEvent<HTMLElement>,
+    actual_value?: string
+  ) => {
+    setInputText(e.currentTarget.textContent || "");
+    handleChange(e, actual_value);
+  };
+
   return (
-    <Flex alignItems="center" bgColor={"ui.secondary"} color={"ui.white"} m={1}>
-      <Text
-        ml={1}
-        width={"inputSizes.defaultDescriptionText"}
-        fontSize="defaultRead"
-      >
-        {descriptionText}
-      </Text>
-      <Select
-        value={getSelectValue()}
-        bgColor={"ui.input"}
-        onChange={(e) => handleChange(e)}
-        width={"inputSizes.defaultBox"}
-        color={"ui.white"}
-        focusBorderColor={"ui.white"}
-        borderColor={"ui.grey"}
-        mr={1}
-        ml={1}
-        key={{ descriptionText } + itemKeyId}
-      >
-        {
-          <option
-            value={defaultValue}
-            key={descriptionText + itemKeyId + "_undefined"}
-            style={{ color: "white", backgroundColor: "#2d3333" }}
+    <Flex m={1}>
+      <FormControl width={"inputSizes.defaultBox"} color={"ui.white"}>
+        <FormLabel>{descriptionText}</FormLabel>
+        <AutoComplete openOnFocus listAllValuesOnFocus>
+          <AutoCompleteInput
+            value={inputText}
+            onChange={(e) => handeSetInputText(e)}
+            onFocus={() => setInputText("")}
+            onBlur={() => setInputText(selectValue?.toString() || defaultText)}
+            placeholder={inputText}
+            autoComplete="off"
+          />
+          <AutoCompleteList
+            borderColor={"ui.grey"}
+            margin={0}
+            p={0}
+            marginBottom={0}
+            borderRadius={0}
+            maxH={"150px"}
+            overflowY={"auto"}
           >
-            {defaultText}
-          </option>
-        }
-        ,
-        {optionsList.map((option) => {
-          return (
-            <option
-              value={option["value"]}
-              key={descriptionText + itemKeyId + option["value"]}
-              style={{ color: "white", backgroundColor: "#2d3333" }}
-            >
-              {option["text"]}
-            </option>
-          );
-        })}
-      </Select>
+            {optionsList.map((option) => (
+              <AutoCompleteItem
+                value={option["text"]}
+                key={descriptionText + itemKeyId + option["value"]}
+                style={{
+                  color: "white",
+                  backgroundColor: "#2d3333",
+                  margin: 0,
+                  // marginBottom: -25,
+                  // padding: 10,
+                  borderRadius: 0,
+                }}
+                onClick={(e) => handleChangeWithText(e, option["value"])}
+              >
+                {option["text"]}
+              </AutoCompleteItem>
+            ))}
+          </AutoCompleteList>
+        </AutoComplete>
+      </FormControl>
     </Flex>
   );
 };
