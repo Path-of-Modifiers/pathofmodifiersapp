86810191a904c05bba5845d8ac810ffda834d285
Ivareh <ivareh@ntnu.no>
2024-06-19 12:00:43 +0200

    279 Refactor TextInput component to use SelectBoxInput

--- src/frontend/src/components/Input/StandardLayoutInput/TextInput.tsx
+++ src/frontend/src/components/Input/StandardLayoutInput/TextInput.tsx
@@ -1,12 +1,12 @@
-import { Input, Flex, Text } from "@chakra-ui/react";
 import {
   GetValueFunction,
-  HandleChangeStringFunction,
+  HandleChangeEventFunction,
 } from "../../../schemas/function/InputFunction";
+import { SelectBoxInput, SelectBoxOptionValue } from "./SelectBoxInput";
 
 interface TextInputProps {
   getTextValue: GetValueFunction;
-  handleTextChange: HandleChangeStringFunction;
+  handleTextChange: HandleChangeEventFunction;
 }
 
 // Item Name Input Component  -  This component is used to input the name of an item.
@@ -14,29 +14,35 @@ export const TextInput = ({
   getTextValue,
   handleTextChange,
 }: TextInputProps) => {
+  const optionsList: Array<SelectBoxOptionValue> = [
+    { value: "", text: "Any" },
+    { value: "Grand Spectrum", text: "Grand Spectrum" },
+    { value: "Forbidden Flesh", text: "Forbidden Flesh" },
+    { value: "The Balance of Terror", text: "The Balance of Terror" },
+    { value: "That Which Was Taken", text: "That Which Was Taken" },
+    { value: "Forbidden Flame", text: "Forbidden Flame" },
+    { value: "Split Personality", text: "Split Personality" },
+    { value: "Thread of Hope", text: "Thread of Hope" },
+    { value: "Impossible Escape", text: "Impossible Escape" },
+    { value: "Watcher's Eye", text: "Watcher's Eye" },
+    { value: "Sublime Vision", text: "Sublime Vision" },
+    { value: "Glorious Vanity", text: "Glorious Vanity" },
+    { value: "Lethal Pride", text: "Lethal Pride" },
+    { value: "Brutal Restraint", text: "Brutal Restraint" },
+    { value: "Militant Faith", text: "Militant Faith" },
+    { value: "Elegant Hubris", text: "Elegant Hubris" },
+    { value: "Voices", text: "Voices" },
+  ];
+
   return (
-    <Flex
-      color={"ui.white"}
-      bgColor={"ui.secondary"}
-      alignItems={"center"}
-      m={1}
-    >
-      <Text ml={1} width={"inputSizes.defaultDescriptionText"}>
-        Item name
-      </Text>
-      <Input
-        value={getTextValue()}
-        bgColor={"ui.input"}
-        color={"ui.white"}
-        onChange={(e) => handleTextChange(e.target.value)}
-        width={"inputSizes.lgBox"}
-        focusBorderColor={"ui.white"}
-        placeholder="Item Name"
-        borderColor={"ui.grey"}
-        mr={1}
-        ml={1}
-        key={"ItemRarityInput"}
-      />
-    </Flex>
+    <SelectBoxInput
+      descriptionText="Item Name"
+      optionsList={optionsList}
+      itemKeyId="itemName"
+      defaultText="Any"
+      defaultValue={undefined}
+      getSelectValue={getTextValue}
+      handleChange={handleTextChange}
+    />
   );
 };
