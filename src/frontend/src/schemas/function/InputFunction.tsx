7f775d5856d83eafad240de64b573de95d7a0562
bogadisa <89651095+bogadisa@users.noreply.github.com>
2024-07-16 15:17:58 +0200

    Revert "198 Created schemas for global use InputFunction and changed SelectBox to import from it"
    This reverts commit 2858ca11e8d3008b79b4a4d90380c00ec47e5bb8.

--- src/frontend/src/schemas/function/InputFunction.tsx
+++
@@ -1,7 +0,0 @@
-export type HandleChangeEventFunction = (
-  event: React.ChangeEvent<HTMLSelectElement>
-) => void;
-
-export type HandleChangeStringFunction = (s: string) => void;
-
-export type GetValueFunction = () => string | number | undefined;
