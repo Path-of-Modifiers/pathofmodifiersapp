917412ced8bb76307614377eda7f22701930dbf0
Bogadisa <magnus@hoddevik.eu>
2024-07-24 17:26:18 +0200

    269 Removed unused version, v9->v1, v2->v1

--- src/backend_data_retrieval/data_retrieval/external_data_retrieval/data_retrieval/poe_api_retrieval/poe_api.py
+++ src/backend_data_retrieval/data_retrieval/external_data_retrieval/data_retrieval/poe_api_retrieval/poe_api.py
@@ -4,9 +4,12 @@ import logging
 import asyncio
 import aiohttp
 import os
+import threading
 import pandas as pd
 from tqdm import tqdm
+from copy import deepcopy
 from typing import List, Union, Tuple, Dict, Coroutine, Iterator, Optional
+from concurrent.futures import ThreadPoolExecutor, Future
 
 from external_data_retrieval.detectors.unique_detector import (
     UniqueJewelDetector,
@@ -15,6 +18,8 @@ from external_data_retrieval.detectors.unique_detector import (
     UniqueWeaponDetector,
     UniqueDetector,
 )
+
+from external_data_retrieval.utils import sync_timing_tracker
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -103,19 +108,6 @@ class APIHandler:
 
         return df
 
-    def _df_to_json(self, df: pd.DataFrame, stashes: List) -> List:
-        """
-        DEPRICATED
-        """
-        wanted_stashes = []
-        for stash_index in df["stash_index"].unique():
-            wanted_stash = stashes[stash_index]
-            wanted_items = df.loc[df["stash_index"] == stash_index]
-            wanted_stash["items"] = wanted_items.to_dict()
-            wanted_stashes.append(wanted_stash)
-
-        return wanted_stashes
-
     def _check_stashes(self, stashes: List) -> pd.DataFrame:
         """
         Parameters:
@@ -151,15 +143,10 @@ class APIHandler:
 
         # Updates progress bars
         self.n_found_items += n_new_items
-        self.item_count_pbar.update(n_new_items)
 
-        self.unique_items_count_pbar.update(
-            n_total_unique_items
-            - self.n_unique_items_found  # Updates with the difference of current unique items found and previously found
-        )
         self.n_unique_items_found = n_total_unique_items
 
-        return df_wanted
+        return df_wanted.reset_index()
 
     def _get_latest_change_id(self) -> str:
         response = requests.get(
@@ -175,134 +162,217 @@ class APIHandler:
 
         return next_change_id
 
-    def _initialize_stream(self) -> Tuple[str, List]:
-        """
-        Makes an initial, synchronous, API call.
-        """
-        next_change_id = self._get_latest_change_id()
+    async def _send_n_recursion_requests(
+        self,
+        n: int,
+        session: aiohttp.ClientSession,
+        waiting_for_next_id_lock: threading.Lock,
+    ) -> List:
+        stashes = []  # For exeption handling
+        if n == 0:
+            # End of recursion
+            return []
 
-        response = requests.get(
-            self.url, headers=self.headers, params={"id": next_change_id}
-        )
-        if response.status_code >= 300:
-            if response.status_code == 429:
-                # https://www.pathofexile.com/developer/docs/index#ratelimits
-                # Rate limits are dynamic
+        if self.requests_since_last_checkpoint == 30:
+            # End of expedition
+            return []
+        waiting_for_next_id_lock.acquire()
+        try:
+            async with session.get(
+                self.url, params={"id": self.next_change_id}
+            ) as response:
                 headers = response.headers
-                retry_after = int(headers["Retry-After"])
-                time.sleep(retry_after + 1)
-                return self._initialize_stream(next_change_id=next_change_id)
-
-            response.raise_for_status()
-        response_json = response.json()
-
-        stashes = response_json["stashes"]
-        if stashes:
-            next_change_id = response_json["next_change_id"]
-            self.iteration_pbar.update()
-
-        return next_change_id, stashes
-
-    async def _start_next_request(
-        self, session: aiohttp.ClientSession, next_change_id: str
-    ) -> Coroutine:
-
-        async with session.get(self.url, params={"id": next_change_id}) as response:
-            if response.status >= 300:
-                if response.status == 429:
-                    # https://www.pathofexile.com/developer/docs/index#ratelimits
-                    # Rate limits are dynamic
-                    headers = response.headers
-                    retry_after = int(headers["Retry-After"])
-                    self.time_for_last_ratelimit = time.perf_counter()
-                    await asyncio.sleep(retry_after + 1)
-                    return await self._start_next_request(session, next_change_id)
-
-                else:
+                if response.status >= 300:
+                    if response.status == 502:
+                        waiting_for_next_id_lock.release()
+                        time.sleep(2)
+                        stashes = await self._send_n_recursion_requests(
+                            n, session, waiting_for_next_id_lock
+                        )
+                        return stashes
                     response.raise_for_status()
-            response_json = await response.json()
-            next_change_id = response_json["next_change_id"]
-            stashes = response_json["stashes"]
-            return next_change_id, stashes
 
-    async def _follow_stream(self) -> pd.DataFrame:
+                new_next_change_id = headers["X-Next-Change-Id"]
+                print(
+                    f"Thread {threading.get_ident()} acquired lock. Current id={self.next_change_id}, next id={new_next_change_id}"
+                )
+                self.next_change_id = new_next_change_id
+
+                self.requests_since_last_checkpoint += 1
+                n -= 1
+                # print(
+                #     f"New id ready, releasing lock. Current request count = {self.requests_since_last_checkpoint}"
+                # )
+                if (
+                    headers["X-Rate-Limit-Ip"].split(":")[0]
+                    == headers["X-Rate-Limit-Ip-State"].split(":")[0]
+                ):
+                    print("Hit ratelimit, cooling down for one test period.")
+                    time.sleep(int(headers["X-Rate-Limit-Ip"].split(":")[1]))
+                waiting_for_next_id_lock.release()
+
+                stashes = await self._send_n_recursion_requests(
+                    n, session, waiting_for_next_id_lock
+                )
+
+                response_json = await response.json()
+                stashes += response_json["stashes"]
+                del response_json
+                return stashes
+        except Exception as e:
+            print("Exiting '_send_n_recursion_requests' gracefully")
+            self.logger.critical(e)
+            self.logger.info("Exiting '_send_n_recursion_requests' gracefully")
+            if waiting_for_next_id_lock.locked():
+                print("Released lock after crash")
+                waiting_for_next_id_lock.release()
+            raise e
+
+    async def _follow_stream(
+        self,
+        stashes_ready_event: threading.Event,
+        waiting_for_next_id_lock: threading.Lock,
+        stash_lock: threading.Lock,
+    ):
         """
         Follows the API stream until conditions are met
         """
-        next_change_id, new_stashes = self._initialize_stream()
-        df = pd.DataFrame()
+        stashes = []  # For exeption handling
 
-        iteration = 2
         session = aiohttp.ClientSession(headers=self.headers)
-        while (
-            self.n_found_items < self.n_wanted_items
-            or self.n_unique_items_found < self.n_unique_wanted_items
-        ):
-            if self.recently_ratelimited:
-                # Adds artificial delay if we have recently been ratelimited
+        expedition_depth = 30
+        try:
+            while True:
+                while self.requests_since_last_checkpoint < expedition_depth:
+
+                    stashes = await self._send_n_recursion_requests(
+                        5, session, waiting_for_next_id_lock
+                    )
+                    print(f"Thread {threading.get_ident()} finished 5 requests")
+                    stash_lock.acquire()
+                    self.stashes += stashes
+                    stash_lock.release()
+                    del stashes
+
+                stashes = []
+                stashes_ready_event.set()
                 time.sleep(1)
+        except Exception as e:
+            self.logger.critical(e)
+            raise e
+        finally:
+            self.logger.info("Exiting '_follow_stream' gracefully.")
+            print("Exiting '_follow_stream' gracefully.")
 
-            future = asyncio.ensure_future(
-                self._start_next_request(session, next_change_id=next_change_id)
+            await session.close()
+
+    def _run_async_follow_stream(
+        self,
+        stashes_ready_event: threading.Event,
+        waiting_for_next_id_lock: threading.Lock,
+        stash_lock: threading.Lock,
+    ):
+        asyncio.run(
+            self._follow_stream(
+                stashes_ready_event, waiting_for_next_id_lock, stash_lock
             )
+        )
 
-            if not new_stashes:
-                time.sleep(
-                    120
-                )  # Waits 120 seconds before continuing to pursue the stream
-            else:
-                df_wanted = self._check_stashes(stashes=new_stashes)
-                df = pd.concat((df, df_wanted))
+    @sync_timing_tracker
+    def _process_stream(
+        self,
+        stashes_ready_event: threading.Event,
+        stash_lock: threading.Lock,
+        df: pd.DataFrame,
+    ) -> pd.DataFrame:
 
-                self.iteration_pbar.update()
-                iteration += 1
+        stashes_ready_event.wait()
+        stash_lock.acquire()
 
-            task_response = await asyncio.gather(future)
-            next_change_id, new_stashes = task_response[0]
+        print("Stashes are ready for processing.")
+        stashes_local = self.stashes
+        del self.stashes
+        self.stashes = []
 
-        df_wanted = self._check_stashes(stashes=new_stashes)
-        df = pd.concat((df, df_wanted))
+        self.requests_since_last_checkpoint = 0
+        stash_lock.release()
+        stashes_ready_event.clear()
 
-        self.iteration_pbar.update()
-        await session.close()
+        print("Copied stashes locally and reset the event.")
+        wandted_df = self._check_stashes(stashes_local)
+        df = pd.concat((df, wandted_df))
+        print("Finished processing the data, waiting for more.")
+        return df
+
+    def _gather_n_checkpoints(
+        self,
+        stashes_ready_event: threading.Event,
+        stash_lock: threading.Lock,
+        n: int = 10,
+    ) -> pd.DataFrame:
+        df = pd.DataFrame()
+        for i in range(n):
+            df = self._process_stream(stashes_ready_event, stash_lock, df)
 
         return df
 
-    def dump_stream(self, track_progress: bool = True) -> Iterator[pd.DataFrame]:
+    def start_data_stream(
+        self, executor: ThreadPoolExecutor, listeners: int, has_crashed: bool
+    ) -> Dict[Future, str] | Future:
+        if not has_crashed:
+            stashes_ready_event = threading.Event()
+            waiting_for_next_id_lock = threading.Lock()
+            stash_lock = threading.Lock()
+
+            self.stashes_ready_event = stashes_ready_event
+            self.waiting_for_next_id_lock = waiting_for_next_id_lock
+            self.stash_lock = stash_lock
+
+            self.next_change_id = self._get_latest_change_id()
+            self.requests_since_last_checkpoint = 0
+            self.stashes = []
+
+        else:
+            stashes_ready_event = self.stashes_ready_event
+            waiting_for_next_id_lock = self.waiting_for_next_id_lock
+            stash_lock = self.stash_lock
+
+        print("Initializing follow stream threads")
+        futures = {}
+        for i in range(listeners):
+            future = executor.submit(
+                self._run_async_follow_stream,
+                stashes_ready_event,
+                waiting_for_next_id_lock,
+                stash_lock,
+            )
+            futures[future] = "listener"
+
+        if has_crashed:
+            return future
+        else:
+            return futures
+
+    def dump_stream(
+        self, track_progress: bool = True, listeners: int = 2
+    ) -> Iterator[pd.DataFrame]:
         """
         The method which begins making API calls and fetching data.
 
         Parameters:
             :track_progress: (bool) Defaults to True. Currently has no function
         """
-        # Intializes progressbar context managers
-        with (
-            tqdm(desc="Iterations", position=0) as self.iteration_pbar,
-            tqdm(
-                total=self.n_wanted_items,
-                desc="    Items found",
-                unit="item",
-                position=1,
-            ) as self.item_count_pbar,
-            tqdm(
-                total=self.n_unique_wanted_items,
-                desc="    Unique items found",
-                unit="item",
-                position=2,
-            ) as self.unique_items_count_pbar,
-        ):
-            try:
-                while True:
-                    df = asyncio.run(self._follow_stream())
 
-                    # Ready for next iteration
-                    self.n_found_items = 0
-                    self.n_unique_items_found = 0
-                    yield df.reset_index()
-                    self.item_count_pbar.reset()
-            except Exception as e:
-                raise e
-            finally:
-                self.iteration_pbar.close()
-                self.item_count_pbar.close()
-                self.unique_items_count_pbar.close()
+        try:
+            stashes_ready_event = self.stashes_ready_event
+            stash_lock = self.stash_lock
+        except AttributeError:
+            raise Exception("The method 'start_data_stream' must be called prior")
+        else:
+            time.sleep(5)
+            while True:
+                print("Begining processing the stream")
+                df = self._gather_n_checkpoints(stashes_ready_event, stash_lock)
+                print("Finished processing the stream, entering transformation phase")
+                yield df.reset_index()
+                del df
