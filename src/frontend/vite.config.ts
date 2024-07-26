53695d2ee5554248f0daa3e87381124cc1e31f07
Ivareh <ivareh@ntnu.no>
2024-07-26 14:52:12 +0200

    320 Initial add backend origins and middlewares to api application

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,20 +1,9 @@
 // vite.config.ts
-import { defineConfig } from "vite";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // It removes the /api from the request address, so it will truly be used only for differentiation
-      },
-    },
-  },
-  plugins: [
-    // ...,
-    TanStackRouterVite(),
-  ],
+  plugins: [react(), TanStackRouterVite()],
 });
