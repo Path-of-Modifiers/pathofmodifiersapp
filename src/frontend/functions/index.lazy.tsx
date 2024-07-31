fa2ae9d2079fad5ddd642b8a4b25104a450e44ff
Ivareh <ivareh@ntnu.no>
2024-07-31 17:11:03 +0200

    328 Removed env-vars.tsx and replaced with .env vars instead with secrets

---
+++ src/frontend/functions/index.lazy.tsx
@@ -0,0 +1,17 @@
+// import { PagesFunction } from "@cloudflare/workers-types";
+// import hCaptchaPlugin from "@cloudflare/pages-plugin-hcaptcha";
+
+
+// export const onRequestPost: PagesFunction[] = [
+//   hCaptchaPlugin({
+//     secret:   ,
+//     sitekey: ,
+//   }),
+//   async () => {
+//     // Request has been validated as coming from a human
+
+//     // Store user credentials
+
+//     return new Response("Successfully registered!");
+//   },
+// ];
