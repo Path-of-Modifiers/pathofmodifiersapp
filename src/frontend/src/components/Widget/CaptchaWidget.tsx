bd27298cd29e41bdafb76d484b2a068f8b9f919b
Ivareh <ivareh@ntnu.no>
2024-08-03 12:43:41 +0200

    328 Create CaptchaWidget and validation for turnstyle, add script to index.html

--- src/frontend/src/components/Widget/CaptchaWidget.tsx
+++ src/frontend/src/components/Widget/CaptchaWidget.tsx
@@ -1,23 +1,44 @@
 import Turnstile, { useTurnstile } from "react-turnstile";
+import useTurnstyleValidation from "../../hooks/turnstyle/turnstyleValidation";
 
 const siteKey = import.meta.env.VITE_APP_TURNSTILE_SITE_KEY || "";
 
+const Verify = (token: string, ip: string) => {
+  const { turnstyleResponse, fetchStatus, isFetched, isError } =
+    useTurnstyleValidation({
+      token: token,
+      ip: ip,
+    });
+
+  if (isError) {
+    return false;
+  }
+  return { turnstyleResponse, fetchStatus, isFetched, isError };
+};
+
 // Cloudfare turnstile widget for captcha
-function CaptchaWidget() {
+const CaptchaWidget = () => {
+  let ip = "";
+  fetch("https://api.ipify.org?format=json")
+    .then((response) => response.json())
+    .then((data) => {
+      ip = data.ip;
+    })
+    .catch((error) => {
+      console.log("Error fetching IP:", error);
+    });
+
   const turnstile = useTurnstile();
   return (
     <Turnstile
       sitekey={siteKey}
       onVerify={(token) => {
-        fetch("/", {
-          method: "POST",
-          body: JSON.stringify({ token }),
-        }).then((response) => {
-          if (!response.ok) turnstile.reset();
-        });
+        if (!Verify(token, ip)) {
+          turnstile.reset();
+        }
       }}
     />
   );
-}
+};
 
 export default CaptchaWidget;
