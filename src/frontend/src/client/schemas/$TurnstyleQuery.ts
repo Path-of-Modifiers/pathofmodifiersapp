0fd4c666d44372d1c03a18219bbfd46e184143d8
Ivareh <ivareh@ntnu.no>
2024-08-03 12:43:10 +0200

    328 Migrate turnstyle backend endpoints and schemas to frontend client

---
+++ src/frontend/src/client/schemas/$TurnstyleQuery.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export const $TurnstyleQuery = {
+    properties: {
+        token: {
+            type: 'string',
+            isRequired: true,
+        },
+        ip: {
+            type: 'string',
+            isRequired: true,
+        },
+    },
+} as const;
