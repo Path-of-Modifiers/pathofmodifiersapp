9624e26e717b376750044232dae21dec35ca0b64
Ivareh <ivareh@ntnu.no>
2024-08-10 11:25:06 +0200

    333 Created schemas for temporary hashed user ip model

---
+++ src/backend_api/app/core/schemas/temporary_hashed_user_ip.py
@@ -0,0 +1,29 @@
+import datetime as _dt
+import pydantic as _pydantic
+
+
+# Shared hashed user ip props
+class _BaseTemporaryHashedUserIP(_pydantic.BaseModel):
+    model_config = _pydantic.ConfigDict(from_attributes=True)
+
+    hashedIP: str
+
+
+# Properties to receive on hashed user ip creation
+class TemporaryHashedUserIpCreate(_BaseTemporaryHashedUserIP):
+    pass
+
+
+# Properties shared by models stored in DB
+class TemporaryHashedUserIpInDBBase(_BaseTemporaryHashedUserIP):
+    createdAt: _dt.datetime
+
+
+# Properties to return to client
+class TemporaryHashedUserIp(_BaseTemporaryHashedUserIP):
+    pass
+
+
+# Properties stored in DB
+class TemporaryHashedUserIpInDB(_BaseTemporaryHashedUserIP):
+    pass
