28e3e29395b2cffab4edccaa867af6299ddb187f
Ivareh <ivareh@ntnu.no>
2024-08-10 13:27:55 +0200

    333 Created mocks for testing validation module

---
+++ src/backend_api/app/tests/validation/turnstile_validation_tests.py
@@ -0,0 +1,21 @@
+from validation.turnstile_validation import ValidateTurnstileRequest
+
+
+class TestTurnstileValidation:
+    def test_create_hashed_ip(
+        self,
+        # validate_turnstile_instance: ValidateTurnstileRequest,
+    ):
+        pass
+
+    def test_get_hashed_ip_statement(
+        self,
+        # validate_turnstile_instance: ValidateTurnstileRequest,
+    ):
+        pass
+
+    def test_add_hashed_ip_to_db(self):
+        pass
+
+    def test_validate_turnstile_request(self):
+        pass
