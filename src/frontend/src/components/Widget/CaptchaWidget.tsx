4b3056bebe59ef789ed212b7108e45536ab52011
Ivareh <ivareh@ntnu.no>
2024-08-11 17:46:09 +0200

    372 Fixed small bug in captcha rendering page vars

--- src/frontend/src/components/Widget/CaptchaWidget.tsx
+++ src/frontend/src/components/Widget/CaptchaWidget.tsx
@@ -1,25 +1,18 @@
 import { Turnstile } from "@marsidev/react-turnstile";
 import useTurnstileValidation from "../../hooks/validation/turnstileValidation";
-import { useEffect, useState } from "react";
-import { useTurnstileStore } from "../../store/TurnstileStore";
+import { useState } from "react";
 
 const siteKey = import.meta.env.VITE_APP_TURNSTILE_SITE_KEY || "";
 
 // Cloudfare turnstile widget for captcha
 const CaptchaWidget = () => {
   const [token, setToken] = useState<string>("");
-  const { setTurnstileResponse } = useTurnstileStore();
   const security_ip = localStorage.getItem("security_ip");
-  const { turnstileResponse } = useTurnstileValidation({
+
+  useTurnstileValidation({
     token: token,
     ip: security_ip ?? "",
   });
-
-  useEffect(() => {
-    if (turnstileResponse) {
-      setTurnstileResponse(turnstileResponse);
-    }
-  }, [turnstileResponse, setTurnstileResponse]);
 
   return <Turnstile siteKey={siteKey} onSuccess={setToken} />;
 };
