a99a24582be7f46ab0750a73f009cba2e08496e2
Ivareh <ivareh@ntnu.no>
2024-08-12 08:01:08 +0200

    329 Adjusted width  and padding in index and QueryButtons.tsx

--- src/frontend/src/components/Common/QueryButtons.tsx
+++ src/frontend/src/components/Common/QueryButtons.tsx
@@ -54,7 +54,7 @@ const QueryButtons = (props: FlexProps) => {
       width="100%"
       flexWrap="wrap"
     >
-      <Box flex={["none", "1"]} mb={[4, 0]}></Box>{" "}
+      <Box width="10vw" flex={["none", "1"]} mb={[4, 20]} alignContent={"center"}></Box>{" "}
       {/* Empty space for centering the middle item */}
       <Box textAlign="center" mb={[4, 0]}>
         <Button
@@ -64,8 +64,8 @@ const QueryButtons = (props: FlexProps) => {
           _hover={{ bg: "ui.queryMainInput" }}
           borderWidth={1}
           borderColor="ui.grey"
-          width="inputSizes.lgBox"
-          maxW="100vw"
+          width={["inputSizes.defaultBox", "inputSizes.lgBox"]}
+          maxW="98vw"
           onClick={handlePlotQuery}
         >
           Query and Plot
