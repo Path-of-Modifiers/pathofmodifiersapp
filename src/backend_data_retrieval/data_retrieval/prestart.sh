2ccdd4eb32284c742350d100e53210aac412ab18
Ivareh <ivareh@ntnu.no>
2024-08-12 15:38:38 +0200

    379 Updated prestart and docker with improved restart script for backend_data_retrieval

--- src/backend_data_retrieval/data_retrieval/prestart.sh
+++ src/backend_data_retrieval/data_retrieval/prestart.sh
@@ -1,10 +1,14 @@
-#! /usr/bin/env bash
+#!/bin/bash
 
 python dataretrieval_pre_start.py
 
 # Create initial data in DB
 python modifier_data_deposit/deposit_modifier_data.py
 
-while true; do
-    python external_data_retrieval/main.py
+
+
+while true
+do
+ python external_data_retrieval/main.py || echo "App crashed... restarting..." >&2
+ echo "Press Ctrl-C to quit." && sleep 1
 done
