3388cf63cd2fcd2e0c9c39e946a445d680221a79
Ivareh <ivareh@ntnu.no>
2024-08-14 13:15:30 +0200

    387 Added NewPassword basemodel to token.py

--- src/backend_api/app/core/schemas/token.py
+++ src/backend_api/app/core/schemas/token.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 # JSON payload containing access token
@@ -10,3 +10,8 @@ class Token(BaseModel):
 # Contents of JWT token
 class TokenPayload(BaseModel):
     sub: str | None = None
+
+
+class NewPassword(BaseModel):
+    token: str
+    new_password: str = Field(min_length=8, max_length=40)
