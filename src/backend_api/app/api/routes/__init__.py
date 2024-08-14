6fd071c36c2f06d7cf0ad76621359f55970554f1
Ivareh <ivareh@ntnu.no>
2024-08-14 19:35:55 +0200

    386 Created routes for login and user for backend api

--- src/backend_api/app/api/routes/__init__.py
+++ src/backend_api/app/api/routes/__init__.py
@@ -3,7 +3,9 @@ from app.api.routes.currency import currency_prefix
 from app.api.routes.item_base_type import item_base_type_prefix
 from app.api.routes.item_modifier import item_modifier_prefix
 from app.api.routes.item import item_prefix
+from app.api.routes.login import login_prefix
 from app.api.routes.modifier import modifier_prefix
 from app.api.routes.stash import stash_prefix
 from app.api.routes.plot import plot_prefix
 from app.api.routes.turnstile import turnstile_prefix
+from app.api.routes.user import user_prefix
