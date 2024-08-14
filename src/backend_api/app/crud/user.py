95036a73f7c09866b3cdf18a1b0479e3472baf07
Ivareh <ivareh@ntnu.no>
2024-08-14 19:40:05 +0200

    389 Debugging crud user and added HTTP exceptions to functions

--- src/backend_api/app/crud/user.py
+++ src/backend_api/app/crud/user.py
@@ -1,7 +1,9 @@
 from typing import Any, List, Union
 
+from fastapi import HTTPException
 from pydantic import TypeAdapter
-from sqlmodel import Session, select
+from sqlalchemy.sql import select
+from sqlalchemy.orm import Session
 
 from app.core.security import get_password_hash, verify_password
 from app.core.models.models import User as model_User
@@ -27,18 +29,16 @@ class CRUDUser:
             User: Created user
         """
 
-        db_obj = self.validate(
-            user_create,
-            update={"hashed_password": get_password_hash(user_create.password)},
-        )
+        db_obj = model_User(**user_create.model_dump())
+        print("CREATING USER", db_obj)
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
-        return db_obj
+        return self.validate(db_obj)
 
     def update_user(
         self, db: Session, *, db_user: model_User, user_in: UserUpdate
-    ) -> Any:
+    ) -> model_User:
         """Update user
 
         Args:
@@ -62,7 +62,7 @@ class CRUDUser:
         db.add(db_user)
         db.commit()
         db.refresh(db_user)
-        return db_user
+        return self.validate(db_user)
 
     def get_user_by_email(self, db: Session, *, email: str) -> model_User | None:
         """Get user by email
@@ -76,8 +76,14 @@ class CRUDUser:
         """
 
         statement = select(model_User).where(model_User.email == email)
-        session_user = db.exec(statement).first()
-        return session_user
+        session_user = db.execute(statement).first()
+        if not session_user:
+            raise HTTPException(
+                status_code=404,
+                detail="The user with this email does not exist in the system.",
+            )
+        print("session_user", session_user)
+        return self.validate(session_user)
 
     def authenticate(
         self, db: Session, *, email: str, password: str
@@ -95,7 +101,13 @@ class CRUDUser:
 
         db_user = self.get_user_by_email(db=db, email=email)
         if not db_user:
-            return None
+            raise HTTPException(
+                status_code=404,
+                detail="Could not authenticate. The user with this email does not exist in the system.",
+            )
         if not verify_password(password, db_user.hashedPassword):
-            return None
-        return db_user
+            raise HTTPException(
+                status_code=400,
+                detail="Incorrect password",
+            )
+        return self.validate(db_user)
