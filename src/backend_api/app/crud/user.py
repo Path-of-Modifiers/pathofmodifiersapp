f3cc2bdc96a1c2a2d0e03576c3e612bc04df61d6
Ivareh <ivareh@ntnu.no>
2024-08-14 14:04:58 +0200

    389 Improved user CRUD model and created model in init

--- src/backend_api/app/crud/user.py
+++ src/backend_api/app/crud/user.py
@@ -16,11 +16,11 @@ class CRUDUser:
     def __init__(self):
         self.validate = TypeAdapter(Union[User, List[User]]).validate_python
 
-    def create_user(self, *, session: Session, user_create: UserCreate) -> model_User:
+    def create_user(self, db: Session, *, user_create: UserCreate) -> model_User:
         """Create a new user
 
         Args:
-            session (Session): DB session
+            db (Session): DB session
             user_create (UserCreate): User data
 
         Returns:
@@ -31,18 +31,18 @@ class CRUDUser:
             user_create,
             update={"hashed_password": get_password_hash(user_create.password)},
         )
-        session.add(db_obj)
-        session.commit()
-        session.refresh(db_obj)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
         return db_obj
 
     def update_user(
-        self, *, session: Session, db_user: model_User, user_in: UserUpdate
+        self, db: Session, *, db_user: model_User, user_in: UserUpdate
     ) -> Any:
         """Update user
 
         Args:
-            session (Session): DB session
+            db (Session): DB session
             db_user (model_User): DB user object
             user_in (UserUpdate): User data to update
 
@@ -59,16 +59,16 @@ class CRUDUser:
         for field in db_user:
             if field in user_in:
                 setattr(db_user, field, user_in[field])
-        session.add(db_user)
-        session.commit()
-        session.refresh(db_user)
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
         return db_user
 
-    def get_user_by_email(self, *, session: Session, email: str) -> model_User | None:
+    def get_user_by_email(self, db: Session, *, email: str) -> model_User | None:
         """Get user by email
 
         Args:
-            session (Session): DB session
+            db (Session): DB session
             email (str): Email
 
         Returns:
@@ -76,16 +76,16 @@ class CRUDUser:
         """
 
         statement = select(model_User).where(model_User.email == email)
-        session_user = session.exec(statement).first()
+        session_user = db.exec(statement).first()
         return session_user
 
     def authenticate(
-        self, *, session: Session, email: str, password: str
+        self, db: Session, *, email: str, password: str
     ) -> model_User | None:
         """Authenticate user
 
         Args:
-            session (Session): DB session
+            db (Session): DB session
             email (str): Email
             password (str): Password
 
@@ -93,7 +93,7 @@ class CRUDUser:
             model_User | None: model_User object or None
         """
 
-        db_user = self.get_user_by_email(session=session, email=email)
+        db_user = self.get_user_by_email(db=db, email=email)
         if not db_user:
             return None
         if not verify_password(password, db_user.hashedPassword):
