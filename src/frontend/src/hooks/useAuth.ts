30edcd2ab3907d5d37151fe03bdb68667649d322
Ivareh <ivareh@ntnu.no>
2024-08-20 12:24:42 +0200

    406 Added routes and components for User login, registration etc. Utils and hooks for user login

---
+++ src/frontend/src/hooks/useAuth.ts
@@ -0,0 +1,103 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "@tanstack/react-router";
+import { useState } from "react";
+
+import { AxiosError } from "axios";
+import {
+  type Body_logins_login_access_token as AccessToken,
+  type ApiError,
+  LoginsService,
+  type UserPublic,
+  type UserRegister,
+  UsersService,
+} from "../client";
+import useCustomToast from "./useCustomToast";
+
+const isLoggedIn = () => {
+  return localStorage.getItem("access_token") !== null;
+};
+
+const useAuth = () => {
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const showToast = useCustomToast();
+  const queryClient = useQueryClient();
+  const { data: user, isLoading } = useQuery<UserPublic | null, Error>({
+    queryKey: ["currentUser"],
+    queryFn: UsersService.getUserMe,
+    enabled: isLoggedIn(),
+  });
+
+  const signUpMutation = useMutation({
+    mutationFn: (data: UserRegister) =>
+      UsersService.registerUser({ requestBody: data }),
+
+    onSuccess: () => {
+      navigate({ to: "/login" });
+      showToast(
+        "Account created.",
+        "Your account has been created successfully.",
+        "success"
+      );
+    },
+    onError: (err: ApiError) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      let errDetail = (err.body as any)?.detail;
+
+      if (err instanceof AxiosError) {
+        errDetail = err.message;
+      }
+
+      showToast("Something went wrong.", errDetail, "error");
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+    },
+  });
+
+  const login = async (data: AccessToken) => {
+    const response = await LoginsService.loginAccessToken({
+      formData: data,
+    });
+    localStorage.setItem("access_token", response.access_token);
+  };
+
+  const loginMutation = useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+      navigate({ to: "/" });
+    },
+    onError: (err: ApiError) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      let errDetail = (err.body as any)?.detail;
+
+      if (err instanceof AxiosError) {
+        errDetail = err.message;
+      }
+
+      if (Array.isArray(errDetail)) {
+        errDetail = "Something went wrong";
+      }
+
+      setError(errDetail);
+    },
+  });
+
+  const logout = () => {
+    localStorage.removeItem("access_token");
+    navigate({ to: "/login" });
+  };
+
+  return {
+    signUpMutation,
+    loginMutation,
+    logout,
+    user,
+    isLoading,
+    error,
+    resetError: () => setError(null),
+  };
+};
+
+export { isLoggedIn };
+export default useAuth;
