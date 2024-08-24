0d14f29a9694e3222636cc8e0dbd2bbfd7658ef7
Ivareh <ivareh@ntnu.no>
2024-08-24 16:42:47 +0200

    388 Create message for get_password_rec_email_sent_success for login and adjust login route for tests

--- src/backend_api/app/core/schemas/token.py
+++ src/backend_api/app/core/schemas/token.py
@@ -15,3 +15,8 @@ class TokenPayload(BaseModel):
 class NewPassword(BaseModel):
     token: str
     new_password: str = Field(min_length=8, max_length=40)
+
+
+class RecoverPassword(BaseModel):
+    username: str | None = None
+    email: str | None = None
