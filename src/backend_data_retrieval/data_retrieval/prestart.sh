a32dd41fd0ca9a0e40e5dc9b0ab87cdbf91ef471
Ivareh <ivareh@ntnu.no>
2024-08-28 22:15:15 +0200

    273 Upgraded message feedback for prestart modifier data deposit script

--- src/backend_data_retrieval/data_retrieval/prestart.sh
+++ src/backend_data_retrieval/data_retrieval/prestart.sh
@@ -4,9 +4,9 @@ python dataretrieval_pre_start.py
 
 # Create initial data in DB
 if python modifier_data_deposit/deposit_modifier_data.py; then
-    echo "Data modifier deposit successful"
+    echo "Data modifier deposit successful. Continuing..."
 else
-    echo "Data modifier deposit failed"
+    echo "Data modifier deposit failed. Exiting..."
     exit 1
 fi
 
