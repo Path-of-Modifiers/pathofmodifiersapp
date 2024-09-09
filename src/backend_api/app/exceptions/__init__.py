23bc488025f6869aa6bca5a7ec0cefcb088cf7b5
Ivareh <ivareh@ntnu.no>
2024-09-09 18:35:42 +0200

    435 Created exceptions for all types of errors in the API

---
+++ src/backend_api/app/exceptions/__init__.py
@@ -0,0 +1,4 @@
+from .model_exceptions.db_exception import *
+from .model_exceptions.util_exception import *
+from .model_exceptions.request_exception import *
+from .model_exceptions.user_login_exceptions import *
