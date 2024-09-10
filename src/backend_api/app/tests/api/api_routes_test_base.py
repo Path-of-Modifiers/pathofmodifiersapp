3f4e6a564194442fcc61411b2f871926f781c495
Ivareh <ivareh@ntnu.no>
2024-09-10 10:23:59 +0200

    440 Created tests for duplicate error in api_route_test_base

--- src/backend_api/app/tests/api/api_routes_test_base.py
+++ src/backend_api/app/tests/api/api_routes_test_base.py
@@ -15,6 +15,7 @@ from app.crud.base import CRUDBase, ModelType
 from app.exceptions import (
     DbObjectDoesNotExistError,
 )
+from app.exceptions.model_exceptions.db_exception import DbObjectAlreadyExistsError
 from app.tests.base_test import BaseTest
 from app.tests.utils.utils import is_courotine_function
 
@@ -23,13 +24,41 @@ from app.tests.utils.utils import is_courotine_function
 class TestAPI(BaseTest):
     async def _create_object_api(
         self,
+        client: TestClient,
+        create_object: dict,
+        route_prefix: str,
+        superuser_token_headers: dict[str, str],
+    ) -> Response:
+        """Try to create an object using the API
+
+        Args:
+            client (TestClient): FastAPI test client
+            create_object (Dict): Create object dictionary
+            route_prefix (str): Route name
+            superuser_token_headers: (dict[str, str]): Superuser headers
+
+        Returns:
+            Response: Response from the API
+
+        """
+
+        response = client.post(
+            f"{settings.API_V1_STR}/{route_prefix}/",
+            headers=superuser_token_headers,
+            json=create_object,
+        )
+
+        return response
+
+    async def _create_random_object_api(
+        self,
         db: Session,
         create_random_object_func: Callable[[], tuple[dict, ModelType, dict]] | Any,
         client: TestClient,
         route_prefix: str,
         superuser_token_headers: dict[str, str],
     ) -> tuple[dict, Response]:
-        """Create an object using the API
+        """Create a random object using the API
 
         Also tests correct response codes and if the object created is
         the same as the object returned by the API
@@ -49,7 +78,6 @@ class TestAPI(BaseTest):
             Dict: Object dictionary created
             Response: Response from the API
         """
-
         if is_courotine_function(create_random_object_func):
             create_obj = await create_random_object_func(db)
         else:
@@ -118,8 +146,52 @@ class TestAPI(BaseTest):
             (Union[ Callable[[Session], Awaitable[Dict]], Callable[[], Dict] ]):
             Function to create a random object
         """
-        await self._create_object_api(
+        await self._create_random_object_api(
             db, create_random_object_func, client, route_prefix, superuser_token_headers
+        )
+
+    @pytest.mark.asyncio
+    async def test_create_found_duplicate(
+        self,
+        client: TestClient,
+        crud_instance: CRUDBase,
+        superuser_token_headers: dict[str, str],
+        route_prefix: str,
+        db: Session,
+        create_random_object_func: Callable[[], tuple[dict, ModelType]],
+    ) -> None:
+        """Test create found duplicate instance"""
+        if crud_instance.ignore_duplicates:
+            pytest.skip(
+                f"API test for test_create_found_duplicate in route {route_prefix} does not support ignore_duplicates"
+            )
+
+        create_obj, response = await self._create_random_object_api(
+            db,
+            create_random_object_func,
+            client,
+            route_prefix,
+            superuser_token_headers,
+        )
+        create_obj_create_schema = crud_instance.create_schema(**create_obj)
+        obj_pks_value_filter = crud_instance._map_obj_pks_to_value(
+            create_obj_create_schema
+        )[0]
+        response = await self._create_object_api(
+            client,
+            create_obj,
+            route_prefix,
+            superuser_token_headers,
+        )
+        assert response.status_code == 409
+        assert (
+            response.json()["detail"]
+            == DbObjectAlreadyExistsError(
+                model_table_name=crud_instance.model.__tablename__,
+                filter=obj_pks_value_filter,
+                function_name=crud_instance.create.__name__,
+                class_name=crud_instance.__class__.__name__,
+            ).detail
         )
 
     @pytest.mark.asyncio
@@ -148,7 +220,7 @@ class TestAPI(BaseTest):
 
             route_prefix (str): Route name
         """
-        _, object_out = await self._create_object_crud(db, object_generator_func)
+        _, object_out = await self._create_random_object_crud(db, object_generator_func)
         obj_out_pk_map = self._create_primary_key_map(object_out)
         response = client.get(
             f"{settings.API_V1_STR}/{route_prefix}/{obj_out_pk_map[unique_identifier]}",
@@ -165,7 +237,7 @@ class TestAPI(BaseTest):
     def test_get_instance_not_found(
         self,
         client: TestClient,
-        crud: CRUDBase,
+        crud_instance: CRUDBase,
         superuser_token_headers: dict[str, str],
         model_table_name: str,
         route_prefix: str,
@@ -192,8 +264,8 @@ class TestAPI(BaseTest):
             == DbObjectDoesNotExistError(
                 model_table_name=model_table_name,
                 filter={unique_identifier: not_found_object},
-                function_name=crud.get.__name__,
-                class_name=crud.__class__.__name__,
+                function_name=crud_instance.get.__name__,
+                class_name=crud_instance.__class__.__name__,
             ).detail
         )
 
@@ -202,7 +274,7 @@ class TestAPI(BaseTest):
         self,
         client: TestClient,
         db: Session,
-        crud: CRUDBase,
+        crud_instance: CRUDBase,
         object_generator_func: Callable[[], tuple[dict, ModelType]],
         get_high_permissions: bool,
         route_prefix: str,
@@ -226,7 +298,7 @@ class TestAPI(BaseTest):
         if not get_high_permissions:
             return 0
 
-        _, object_out = await self._create_object_crud(db, object_generator_func)
+        _, object_out = await self._create_random_object_crud(db, object_generator_func)
         obj_out_pk_map = self._create_primary_key_map(object_out)
         response = client.get(
             f"{settings.API_V1_STR}/{route_prefix}/{obj_out_pk_map[unique_identifier]}",
@@ -292,10 +364,10 @@ class TestAPI(BaseTest):
             update_request_params (bool): Whether the update request requires params
             ignore_test_columns (List[str]): Columns to ignore
         """
-        _, object_out = await self._create_object_crud(db, object_generator_func)
+        _, object_out = await self._create_random_object_crud(db, object_generator_func)
         obj_out_pk_map = self._create_primary_key_map(object_out)
 
-        update_object_dict, update_object_out = await self._create_object_crud(
+        update_object_dict, update_object_out = await self._create_random_object_crud(
             db, object_generator_func
         )
         self._test_object(update_object_out, update_object_dict)
@@ -346,7 +418,7 @@ class TestAPI(BaseTest):
         self,
         client: TestClient,
         db: Session,
-        crud: CRUDBase,
+        crud_instance: CRUDBase,
         superuser_token_headers: dict[str, str],
         object_generator_func: Callable[[], tuple[dict, ModelType]],
         route_prefix: str,
@@ -370,7 +442,7 @@ class TestAPI(BaseTest):
             update_request_params (bool): Whether the update request requires params
             unique_identifier (str): Unique identifier
         """
-        update_object_dict, update_object_out = await self._create_object_crud(
+        update_object_dict, update_object_out = await self._create_random_object_crud(
             db, object_generator_func
         )  # create the object to update and add to the db
         self._test_object(update_object_out, update_object_dict)
@@ -420,8 +492,8 @@ class TestAPI(BaseTest):
             == DbObjectDoesNotExistError(
                 model_table_name=model_table_name,
                 filter=update_obj_out_pk_map,
-                function_name=crud.get.__name__,
-                class_name=crud.__class__.__name__,
+                function_name=crud_instance.get.__name__,
+                class_name=crud_instance.__class__.__name__,
             ).detail
         )
 
@@ -451,11 +523,11 @@ class TestAPI(BaseTest):
             unique_identifier (str): Unique identifier
             update_request_params (bool): Whether the update request requires params
         """
-        _, object_out = await self._create_object_crud(db, object_generator_func)
+        _, object_out = await self._create_random_object_crud(db, object_generator_func)
 
         obj_pk_map = self._create_primary_key_map(object_out)
 
-        update_object_dict, update_object_out = await self._create_object_crud(
+        update_object_dict, update_object_out = await self._create_random_object_crud(
             db, object_generator_func
         )
         self._test_object(update_object_out, update_object_dict)
@@ -518,7 +590,9 @@ class TestAPI(BaseTest):
             route_prefix (str): Route name
             unique_identifier (str): Unique identifier
         """
-        _, update_object_out = await self._create_object_crud(db, object_generator_func)
+        _, update_object_out = await self._create_random_object_crud(
+            db, object_generator_func
+        )
         update_obj_pk_map = self._create_primary_key_map(update_object_out)
 
         response = client.delete(
@@ -544,7 +618,7 @@ class TestAPI(BaseTest):
         route_prefix: str,
         model_table_name: str,
         unique_identifier: str,
-        crud: CRUDBase,
+        crud_instance: CRUDBase,
     ) -> None:
         """Test delete instance not found
 
@@ -567,8 +641,8 @@ class TestAPI(BaseTest):
             == DbObjectDoesNotExistError(
                 model_table_name=model_table_name,
                 filter={unique_identifier: not_found_object},
-                function_name=crud.remove.__name__,
-                class_name=crud.__class__.__name__,
+                function_name=crud_instance.remove.__name__,
+                class_name=crud_instance.__class__.__name__,
             ).detail
         )
 
@@ -593,7 +667,7 @@ class TestAPI(BaseTest):
             route_prefix (str): Route name
             unique_identifier (str): Unique identifier
         """
-        _, object_out = await self._create_object_crud(db, object_generator_func)
+        _, object_out = await self._create_random_object_crud(db, object_generator_func)
         obj_out_pk_map = self._create_primary_key_map(object_out)
         response = client.delete(
             f"{settings.API_V1_STR}/{route_prefix}/{obj_out_pk_map[unique_identifier]}",
