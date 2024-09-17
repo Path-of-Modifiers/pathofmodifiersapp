ba9cf6e27758d7deab52c28f5b25d147c5e6566f
Ivareh <ivareh@ntnu.no>
2024-09-17 19:23:59 +0200

    459 Removed the rate limiters and types with bad structure

--- src/backend_api/app/tests/types.py
+++
@@ -1,8 +0,0 @@
-from collections.abc import Awaitable, Callable
-from typing import Any
-
-from app.crud.base import ModelType
-
-AsyncFunction = Callable[[Any, Any], Awaitable[Any]]
-
-ObjectGeneratorFunc = Callable[[], tuple[dict, ModelType]]
