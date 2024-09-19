6d7a7072a8284375a04c57dd84cd7919ac132dfa
Ivareh <ivareh@ntnu.no>
2024-09-19 17:34:41 +0200

    #494 Upgraded logging logic for modules in the API :cool:

--- src/backend_api/app/exceptions/model_exceptions/plot_exception.py
+++ src/backend_api/app/exceptions/model_exceptions/plot_exception.py
@@ -2,6 +2,26 @@ import starlette.status as status
 
 from app.core.config import settings
 from app.exceptions.exception_base import PathOfModifiersAPIError
+from app.logger import logger
+
+
+class PlotQueryToDBError(PathOfModifiersAPIError):
+    def __init__(
+        self,
+        *,
+        exception: Exception,
+        function_name: str | None = "Unknown function",
+        class_name: str | None = None,
+    ):
+        detail = f"Failed to perform plot query to the database. Exception: {exception}"
+        logger.error(detail)
+
+        super().__init__(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            function_name=function_name,
+            class_name=class_name,
+            detail=detail,
+        )
 
 
 class PlotQueryDataNotFoundError(PathOfModifiersAPIError):
@@ -44,14 +64,15 @@ class PlotNoModifiersProvidedError(PathOfModifiersAPIError):
         )
 
 
+# Not part of PathOfModifiersAPIError since it has its own error handler
 class PlotRateLimitExceededError(PathOfModifiersAPIError):
     def __init__(
         self,
         *,
         retry_after_seconds: int,
-        max_amount_of_tries_per_time_period: int,
         function_name: str | None = "Unknown function",
         class_name: str | None = None,
+        max_amount_of_tries_per_time_period: int,
         status_code: int | None = status.HTTP_429_TOO_MANY_REQUESTS,
     ):
         detail = (
@@ -62,8 +83,8 @@ class PlotRateLimitExceededError(PathOfModifiersAPIError):
         # Call the parent constructor with a specific status code
         super().__init__(
             status_code=status_code,
+            detail=detail,
             function_name=function_name,
             class_name=class_name,
-            detail=detail,
             headers={"Retry-After-Seconds": str(retry_after_seconds)},
         )
