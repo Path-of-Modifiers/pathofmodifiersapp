3ebcb2a69e559dea207d99aab5c7ae70f5a61f34
Ivareh <ivareh@ntnu.no>
2024-09-19 17:27:53 +0200

    #494 Updated streaming and format config for logger in api

--- src/backend_api/app/logger.py
+++ src/backend_api/app/logger.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 """
 Taken from https://medium.com/@roy-pstr/fastapi-server-errors-and-logs-take-back-control-696405437983
@@ -9,4 +10,16 @@ uvicorn_access = logging.getLogger("uvicorn.access")
 uvicorn_access.disabled = True
 
 logger = logging.getLogger("uvicorn")
-logging.basicConfig(filename="backend_api.log", encoding="utf-8", level=logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+
+stream_handler = logging.StreamHandler(sys.stdout)
+file_handler = logging.FileHandler("backend_api.log")
+
+stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.handlers = [stream_handler, file_handler]
+
+logger.setLevel(logging.INFO)
