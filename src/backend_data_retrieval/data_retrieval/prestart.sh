a92fce45fb284f36b5d064ba4deb9325e28e3bab
Bogadisa <magnus@hoddevik.eu>
2024-09-27 18:51:44 +0200

    #527 Restructure + added depositer base to easily extend it to specific data

--- src/backend_data_retrieval/data_retrieval/prestart.sh
+++ src/backend_data_retrieval/data_retrieval/prestart.sh
@@ -3,12 +3,13 @@
 python dataretrieval_pre_start.py
 
 # Create initial data in DB
-if python modifier_data_deposit/deposit_modifier_data.py; then
-    echo "Data modifier deposit successful. Continuing..."
+if python data_deposit/main.py; then
+    echo "Data deposit successful. Continuing..."
 else
-    echo "Data modifier deposit failed. Exiting..."
+    echo "Data deposit failed. Exiting..."
     exit 1
 fi
+exit 0
 
 # Variables that need to be set before running container.
 # Check if exists or value = changethis
