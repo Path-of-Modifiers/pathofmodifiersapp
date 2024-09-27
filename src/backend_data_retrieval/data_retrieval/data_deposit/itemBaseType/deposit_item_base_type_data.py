7a9ea3f541a9b41602ea5587ec2cd0499f47e089
Bogadisa <magnus@hoddevik.eu>
2024-09-27 19:04:23 +0200

    #527 Added "on_duplicate_pkey_do_nothing=true" to item base type deposit for when the program restarts

--- src/backend_data_retrieval/data_retrieval/data_deposit/itemBaseType/deposit_item_base_type_data.py
+++ src/backend_data_retrieval/data_retrieval/data_deposit/itemBaseType/deposit_item_base_type_data.py
@@ -15,5 +15,7 @@ class ItemBaseTypeDataDepositer(DataDepositerBase):
     def __init__(self, logger: logging.Logger) -> None:
         super().__init__(data_type="itemBaseType", logger=logger)
 
+        self.data_url += "?on_duplicate_pkey_do_nothing=true"
+
     def _process_data(self, df: DataFrame) -> DataFrame:
         return df
