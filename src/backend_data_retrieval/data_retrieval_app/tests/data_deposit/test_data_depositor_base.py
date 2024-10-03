ff1d6b83190dac168cb768e5676384012146920f
Ivareh <ivareh@ntnu.no>
2024-10-03 19:53:06 +0200

    #534 Created layout for tests in backend data retreival

---
+++ src/backend_data_retrieval/data_retrieval_app/tests/data_deposit/test_data_depositor_base.py
@@ -0,0 +1,6 @@
+class TestDataDepositorBase:
+    def test_helper_load_data(self):
+        pass
+
+    def test_helper_insert_data(self):
+        pass
---
+++ src/backend_data_retrieval/data_retrieval_app/tests/external_data_retrieval/detectors/test_detectors.py
@@ -0,0 +1,11 @@
+class TestDetectors:
+    """Tests all detectors in the app."""
+
+    def test_helper_general_filter(self):
+        pass
+
+    def test_helper_specialized_filter(self):
+        pass
+
+    def test_iterate_stashes(self):
+        pass
