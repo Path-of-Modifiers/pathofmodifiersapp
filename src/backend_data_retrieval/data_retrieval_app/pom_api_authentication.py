494e4cf7faa5bcadda20ad81f22a3a8de1e1b2b1
Ivareh <ivareh@ntnu.no>
2024-10-12 15:46:15 +0200

    #548 Added log for request in pom_api_authentication

--- src/backend_data_retrieval/data_retrieval_app/pom_api_authentication.py
+++ src/backend_data_retrieval/data_retrieval_app/pom_api_authentication.py
@@ -2,6 +2,7 @@ import requests
 from pydantic import HttpUrl
 
 from data_retrieval_app.external_data_retrieval.config import settings
+from data_retrieval_app.logs.logger import main_logger
 
 
 def get_superuser_token_headers(url: HttpUrl) -> dict[str, str]:
@@ -9,7 +10,12 @@ def get_superuser_token_headers(url: HttpUrl) -> dict[str, str]:
         "username": settings.FIRST_SUPERUSER,
         "password": settings.FIRST_SUPERUSER_PASSWORD,
     }
-    r = requests.post(f"{url}/login/access-token", data=login_data)
+    try:
+        r = requests.post(f"{url}/login/access-token", data=login_data)
+        r.raise_for_status()
+    except Exception as e:
+        main_logger.exception(f"The following error occurred during the request: {e}")
+        raise e
     tokens = r.json()
     a_token = tokens["access_token"]
     headers = {"Authorization": f"Bearer {a_token}"}
