e4459837dc8957dde9954b10d8b54b2820b437ac
Ivareh <ivareh@ntnu.no>
2024-10-15 10:13:08 +0200

    #577 Updated login pages with better height and minheight config

--- src/frontend/src/routes/reset-password.tsx
+++ src/frontend/src/routes/reset-password.tsx
@@ -1,17 +1,20 @@
 import {
   Button,
   Container,
+  Flex,
   FormControl,
   FormErrorMessage,
   FormLabel,
   Heading,
   Input,
   Text,
+  Image,
 } from "@chakra-ui/react";
 import { useMutation } from "@tanstack/react-query";
 import { createFileRoute, redirect, useNavigate } from "@tanstack/react-router";
 import { type SubmitHandler, useForm } from "react-hook-form";
 
+import Logo from "/assets/images/POM_logo_rec.svg";
 import { type ApiError, LoginsService, type NewPassword } from "../client";
 import { isLoggedIn } from "../hooks/validation/useAuth";
 import useCustomToast from "../hooks/useCustomToast";
@@ -80,57 +83,73 @@ function ResetPassword() {
   };
 
   return (
-    <Container
+    <Flex
       bgColor="ui.main"
       color="ui.white"
-      as="form"
-      onSubmit={handleSubmit(onSubmit)}
-      h="100vh"
-      maxW="sm"
-      alignItems="stretch"
-      mt={"25vh"}
-      gap={4}
-      centerContent
+      h="loginPages.standardHeight"
+      minH="loginPages.standardMinHeight"
     >
-      <Heading size="xl" textAlign="center" mb={2}>
-        Reset Password
-      </Heading>
-      <Text textAlign="center">
-        Please enter your new password and confirm it to reset your password.
-      </Text>
-      <FormControl mt={4} isInvalid={!!errors.new_password}>
-        <FormLabel htmlFor="password">Set Password</FormLabel>
-        <Input
-          id="password"
-          {...register("new_password", passwordRules())}
-          placeholder="Password"
-          type="password"
-        />
-        {errors.new_password && (
-          <FormErrorMessage>{errors.new_password.message}</FormErrorMessage>
-        )}
-      </FormControl>
-      <FormControl mt={4} isInvalid={!!errors.confirm_password}>
-        <FormLabel htmlFor="confirm_password">Confirm Password</FormLabel>
-        <Input
-          id="confirm_password"
-          {...register("confirm_password", confirmPasswordRules(getValues))}
-          placeholder="Password"
-          type="password"
-        />
-        {errors.confirm_password && (
-          <FormErrorMessage>{errors.confirm_password.message}</FormErrorMessage>
-        )}
-      </FormControl>
-      <Button
-        bg="ui.queryBaseInput"
+      <Container
+        bgColor="ui.main"
         color="ui.white"
-        _hover={{ bg: "ui.queryMainInput" }}
-        variant="primary"
-        type="submit"
+        as="form"
+        onSubmit={handleSubmit(onSubmit)}
+        maxW="sm"
+        alignItems="stretch"
+        mt={"15vh"}
+        gap={4}
+        centerContent
       >
-        Reset Password
-      </Button>
-    </Container>
+        <Image
+          src={Logo}
+          alt="FastAPI logo"
+          height="auto"
+          maxW="2xs"
+          alignSelf="center"
+          mb={4}
+        />
+        <Heading size="xl" textAlign="center" mb={2}>
+          Reset Password
+        </Heading>
+        <Text textAlign="center">
+          Please enter your new password and confirm it to reset your password.
+        </Text>
+        <FormControl mt={4} isInvalid={!!errors.new_password}>
+          <FormLabel htmlFor="password">Set Password</FormLabel>
+          <Input
+            id="password"
+            {...register("new_password", passwordRules())}
+            placeholder="Password"
+            type="password"
+          />
+          {errors.new_password && (
+            <FormErrorMessage>{errors.new_password.message}</FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl mt={4} isInvalid={!!errors.confirm_password}>
+          <FormLabel htmlFor="confirm_password">Confirm Password</FormLabel>
+          <Input
+            id="confirm_password"
+            {...register("confirm_password", confirmPasswordRules(getValues))}
+            placeholder="Password"
+            type="password"
+          />
+          {errors.confirm_password && (
+            <FormErrorMessage>
+              {errors.confirm_password.message}
+            </FormErrorMessage>
+          )}
+        </FormControl>
+        <Button
+          bg="ui.queryBaseInput"
+          color="ui.white"
+          _hover={{ bg: "ui.queryMainInput" }}
+          variant="primary"
+          type="submit"
+        >
+          Reset Password
+        </Button>
+      </Container>
+    </Flex>
   );
 }
