9534e87bf59e71c91be741aef2fbde767b2b86c5
Ivareh <ivareh@ntnu.no>
2024-10-17 19:32:53 +0200

    #470 Added rate limit test to turnstile and made request with turnstile async

--- src/backend_api/app/tests/test_simulating_env/api/routes/test_turnstile.py
+++ src/backend_api/app/tests/test_simulating_env/api/routes/test_turnstile.py
@@ -2,19 +2,24 @@ from datetime import datetime, timedelta
 from unittest.mock import patch
 
 import pytest
-from fastapi.testclient import TestClient
+from httpx import AsyncClient
 
 from app.api.api_message_util import get_failed_send_challenge_request_error_msg
 from app.api.routes import turnstile_prefix
 from app.core.config import settings
+from app.core.rate_limit.rate_limit_config import rate_limit_settings
+from app.core.schemas.turnstile import TurnstileQuery
+from app.tests.test_simulating_env.api.api_test_rate_limit_base import TestRateLimitBase
 from app.tests.test_simulating_env.base_test import BaseTest
+from app.tests.utils.rate_limit import RateLimitPerTimeInterval
 from app.tests.utils.utils import create_random_ip
 
 
 @pytest.mark.usefixtures("clear_db", autouse=True)
 class TestTurnstileAPI(BaseTest):
-    def test_success_turnstile_validation_always_passes(
-        self, client: TestClient
+    @pytest.mark.anyio
+    async def test_success_turnstile_validation_always_passes(
+        self, async_client: AsyncClient
     ) -> None:
         with patch(
             "app.core.config.settings.TURNSTILE_SECRET_KEY",
@@ -24,7 +29,7 @@ class TestTurnstileAPI(BaseTest):
                 "token": "test_token",
                 "ip": create_random_ip(),
             }
-            response = client.post(
+            response = await async_client.post(
                 f"{settings.API_V1_STR}/{turnstile_prefix}/", json=data
             )
             response_data = response.json()
@@ -40,7 +45,10 @@ class TestTurnstileAPI(BaseTest):
                 now - challenge_ts < timedelta(seconds=300)
             )  # https://developers.cloudflare.com/turnstile/get-started/server-side-validation/
 
-    def test_error_turnstile_validation_always_blocks(self, client: TestClient) -> None:
+    @pytest.mark.anyio
+    async def test_error_turnstile_validation_always_blocks(
+        self, async_client: AsyncClient
+    ) -> None:
         with patch(
             "app.core.config.settings.TURNSTILE_SECRET_KEY",
             "2x0000000000000000000000000000000AA",  # Always blocks challenge
@@ -49,7 +57,7 @@ class TestTurnstileAPI(BaseTest):
                 "token": "test_token",
                 "ip": create_random_ip(),
             }
-            response = client.post(
+            response = await async_client.post(
                 f"{settings.API_V1_STR}/{turnstile_prefix}/", json=data
             )
 
@@ -61,8 +69,9 @@ class TestTurnstileAPI(BaseTest):
                 ).message
             )
 
-    def test_error_turnstile_validation_token_already_spent(
-        self, client: TestClient
+    @pytest.mark.anyio
+    async def test_error_turnstile_validation_token_already_spent(
+        self, async_client: AsyncClient
     ) -> None:
         with patch(
             "app.core.config.settings.TURNSTILE_SECRET_KEY",
@@ -72,7 +81,7 @@ class TestTurnstileAPI(BaseTest):
                 "token": "test_token",
                 "ip": create_random_ip(),
             }
-            response = client.post(
+            response = await async_client.post(
                 f"{settings.API_V1_STR}/{turnstile_prefix}/", json=data
             )
             response_data = response.json()
@@ -83,7 +92,10 @@ class TestTurnstileAPI(BaseTest):
                 ).message
             )
 
-    def test_error_turnstile_validation_invalid_token(self, client: TestClient) -> None:
+    @pytest.mark.anyio
+    async def test_error_turnstile_validation_invalid_token(
+        self, async_client: AsyncClient
+    ) -> None:
         with patch(
             "app.core.config.settings.TURNSTILE_SECRET_KEY",
             "IOIJIUSIUdfnnajdfnadnfkjnadnæhlk",  # Invalid token
@@ -92,7 +104,7 @@ class TestTurnstileAPI(BaseTest):
                 "token": "test_token",
                 "ip": create_random_ip(),
             }
-            response = client.post(
+            response = await async_client.post(
                 f"{settings.API_V1_STR}/{turnstile_prefix}/", json=data
             )
             response_data = response.json()
@@ -103,3 +115,44 @@ class TestTurnstileAPI(BaseTest):
                     e=["invalid-input-secret"],
                 ).message
             )
+
+    @pytest.mark.usefixtures("clear_db", autouse=True)
+    @pytest.mark.usefixtures("clear_cache", autouse=True)
+    @pytest.mark.skipif(
+        settings.SKIP_RATE_LIMIT_TEST is True
+        or settings.SKIP_RATE_LIMIT_TEST == "True",
+        reason="Rate limit test is disabled",
+    )
+    class TestTurnstileRateLimitAPI(TestRateLimitBase):
+        @pytest.mark.anyio
+        async def test_post_plot_rate_limit(
+            self,
+            async_client: AsyncClient,
+        ) -> None:
+            """
+            Perform rate limit test for POST plot endpoint.
+            """
+            with patch(
+                "app.core.config.settings.TURNSTILE_SECRET_KEY",
+                "1x0000000000000000000000000000000AA",  # Always passes challenge
+            ):
+                turnstile_query = {
+                    "token": "test_token",
+                    "ip": create_random_ip(),
+                }
+
+                async def post_tursntile_query_from_api_user(query: TurnstileQuery):
+                    return await async_client.post(
+                        f"{settings.API_V1_STR}/{turnstile_prefix}/", json=query
+                    )
+
+                # Get rate limit per time interval for POST plot request
+                rate_limits_per_interval_format = RateLimitPerTimeInterval(
+                    rate_per_interval=f"{rate_limit_settings.TURNSTILE_RATE_LIMIT_MAX_TRIES_PER_TIME_PERIOD}/second"
+                )
+
+                await self.perform_time_interval_requests_with_api_function(
+                    api_function=post_tursntile_query_from_api_user,
+                    all_rate_limits_per_interval=rate_limits_per_interval_format,
+                    query=turnstile_query,
+                )
