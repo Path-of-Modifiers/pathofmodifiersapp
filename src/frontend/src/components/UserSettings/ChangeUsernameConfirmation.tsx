91cc602a36104fdae541ab1a6d5ba010a39db058
Ivareh <ivareh@ntnu.no>
2024-10-17 10:19:54 +0200

    #470 Updated the user settings with better coloring

--- src/frontend/src/components/UserSettings/ChangeUsernameConfirmation.tsx
+++ src/frontend/src/components/UserSettings/ChangeUsernameConfirmation.tsx
@@ -121,13 +121,22 @@ const ChangeUsernameConfirmation = ({ isOpen, onClose }: DeleteProps) => {
             </AlertDialogBody>
 
             <AlertDialogFooter gap={3}>
-              <Button variant="danger" type="submit" isLoading={isSubmitting}>
+              <Button
+                bg="ui.queryBaseInput"
+                _hover={{ bg: "ui.queryMainInput" }}
+                variant="danger"
+                type="submit"
+                isLoading={isSubmitting}
+              >
                 Update
               </Button>
               <Button
                 ref={cancelRef}
                 onClick={handleClose}
                 isDisabled={isSubmitting}
+                bg="ui.lighterSecondary.100"
+                color="ui.white"
+                _hover={{ bg: "ui.lightInput" }}
               >
                 Cancel
               </Button>
