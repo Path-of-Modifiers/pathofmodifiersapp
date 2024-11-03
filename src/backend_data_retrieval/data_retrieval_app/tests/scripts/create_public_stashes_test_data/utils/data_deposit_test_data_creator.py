7444b4e75ef56772b88afba8d9923f55127a6e86
Bogadisa <magnus@hoddevik.eu>
2024-11-03 20:14:41 +0100

    #613 More test data creation optimization

--- src/backend_data_retrieval/data_retrieval_app/tests/scripts/create_public_stashes_test_data/utils/data_deposit_test_data_creator.py
+++ src/backend_data_retrieval/data_retrieval_app/tests/scripts/create_public_stashes_test_data/utils/data_deposit_test_data_creator.py
@@ -15,7 +15,7 @@ from data_retrieval_app.pom_api_authentication import (
 from data_retrieval_app.tests.scripts.create_public_stashes_test_data.config import (
     script_settings,
 )
-from data_retrieval_app.tests.utils import random_int
+from data_retrieval_app.tests.utils import random_float, random_int
 
 
 class DataDepositTestDataCreator:
@@ -44,7 +44,10 @@ class DataDepositTestDataCreator:
         self.grouped_modifier_df = self._get_df_from_url(
             "modifier/grouped_modifiers_by_effect/"
         )
-        self.db_modifier_df = self._get_df_from_url("modifier/")
+        self.db_modifier_df = self._get_df_from_url("modifier/").sort_values(
+            by=["modifierId", "position"]
+        )
+        self.modifier_ids_to_effect_map: dict[list[int], str] = {}
 
         if script_settings.MODIFIER_CSV_FILES_TO_ITERATE:
             test_logger.info(
@@ -117,7 +120,7 @@ class DataDepositTestDataCreator:
         dict[int | str, list[list[int]]], dict[int | str, list[tuple[int, int] | str]]
     ]:
         """
-        Example output connected_modifier_ids_dict dict:
+        Example output connected_modifier_ids_dict:
         {
             3: [
                 [1],
@@ -127,15 +130,30 @@ class DataDepositTestDataCreator:
                 [1]
             ]
         }
+        Example output connected_rolls_dict:
+        {
+            3: [
+                [1, 20],
+                "Balbala|Xibaqua"
+            ],
+            "rest": [
+                [1, 20]
+            ]
+        }
+        Where a list indicates numerical roll and
+            minRoll, maxRoll = list[0], list[1]
+        And a string indicates text roll, where each
+        roll is seperated by `|`
         """
 
-        def get_modifier_ids_and_rolls(
+        def get_modifier_ids_rolls_effect(
             row: pd.Series,
         ) -> pd.Series:
             choosable_modifier_ids = row["groupedModifierProperties"]["modifierId"]
+            effect = row["effect"]
 
             df: pd.DataFrame = self.db_modifier_df.loc[
-                self.db_modifier_df["effect"] == row["effect"]
+                self.db_modifier_df["effect"] == effect
             ]
             not_static = all(df["static"].isna())
             rolls = []
@@ -156,7 +174,13 @@ class DataDepositTestDataCreator:
                     .to_list()
                 )
 
-            return pd.Series({"modifier_ids": choosable_modifier_ids, "rolls": rolls})
+            return pd.Series(
+                {
+                    "modifier_ids": choosable_modifier_ids,
+                    "rolls": rolls,
+                    "effect": effect,
+                }
+            )
 
         grouped_modifier_df = self.grouped_modifier_df
         prev_key = 0
@@ -170,9 +194,6 @@ class DataDepositTestDataCreator:
             choosable_modifiers_slice = slice(prev_key, upper_bound - 1)
             choosable_modifier_df = modifier_df.loc[choosable_modifiers_slice]
 
-            # connected_modifier_df.apply(get_modifier_ids_and_rolls, axis=1)
-
-            # ---- modifier id ----
             # Convert to one long regex string
             effects_to_choose_from = "|".join(
                 choosable_modifier_df["effect"]
@@ -187,15 +208,22 @@ class DataDepositTestDataCreator:
                 choosable_effects_mask
             ]
 
-            modifier_ids_and_rolls = connected_grouped_modifier_df.apply(
-                get_modifier_ids_and_rolls, axis=1
+            modifier_ids_rolls_effect_df = connected_grouped_modifier_df.apply(
+                get_modifier_ids_rolls_effect, axis=1
             )
 
-            modifier_ids = modifier_ids_and_rolls["modifier_ids"].to_list()
+            modifier_ids: list[list[int]] = modifier_ids_rolls_effect_df[
+                "modifier_ids"
+            ].to_list()
             connected_modifier_ids_dict[key] = modifier_ids
 
-            rolls = modifier_ids_and_rolls["rolls"].to_list()
+            rolls = modifier_ids_rolls_effect_df["rolls"].to_list()
             connected_rolls_dict[key] = rolls
+
+            effects: list[list[str]] = modifier_ids_rolls_effect_df["effect"].to_list()
+            for modifier_id, effect in zip(modifier_ids, effects, strict=True):
+                if tuple(modifier_id) not in self.modifier_ids_to_effect_map:
+                    self.modifier_ids_to_effect_map[tuple(modifier_id)] = effect
 
             prev_key = upper_bound
 
@@ -209,8 +237,6 @@ class DataDepositTestDataCreator:
             modifier_df,
         ) in self._get_info_from_modifier_file():
             modifier_template = {}
-            if filename != "MilitantFaith.csv":
-                continue
             if "Unique Name" in modifier_comments:
                 modifier_template["item_name"] = self._parse_comment(
                     modifier_comments["Unique Name"]
@@ -245,6 +271,21 @@ class DataDepositTestDataCreator:
             ) = self._find_connected_modifier_ids_and_rolls(
                 modifier_df, modifier_template["distrubution"]
             )
+            # Check for equal lengths taken from:
+            # https://stackoverflow.com/questions/35791051/better-way-to-check-if-all-lists-in-a-list-are-the-same-length
+            it = iter(
+                [
+                    modifier_template["can_duplicate"],
+                    modifier_template["distrubution"].keys(),
+                    modifier_template["modifier_ids_to_choose"].keys(),
+                    modifier_template["roll_ranges"].keys(),
+                ]
+            )
+            the_len = len(next(it))
+            if not all(len(list_to_compare) == the_len for list_to_compare in it):
+                raise AttributeError(
+                    f"Something went wrong while creating the template, its not correct: \n {modifier_template}"
+                )
 
             self.templates[filename] = modifier_template
 
@@ -265,8 +306,8 @@ class DataDepositTestDataCreator:
             if isinstance(roll, str):
                 new_effect += random.choice(roll.split("|"))
             else:
-                new_effect += f"{random_int(roll[0], roll[1])}"
-
+                new_effect += f"{random_float(roll[0], roll[1])}"
+                new_effect = new_effect.replace("+-", "-")
         return new_effect + split_effect[-1]
 
     def _choose_and_make_modifiers(
@@ -290,26 +331,10 @@ class DataDepositTestDataCreator:
         modifier_ids_to_choose = template["modifier_ids_to_choose"]
         roll_ranges = template["roll_ranges"]
 
-        # Check for equal lengths taken from:
-        # https://stackoverflow.com/questions/35791051/better-way-to-check-if-all-lists-in-a-list-are-the-same-length
-        it = iter(
-            [
-                can_duplicate,
-                distrubution.keys(),
-                modifier_ids_to_choose.keys(),
-                roll_ranges.keys(),
-            ]
-        )
-        the_len = len(next(it))
-        if not all(len(list_to_compare) == the_len for list_to_compare in it):
-            raise ValueError(
-                "can_duplicate, distrubution.keys(), modifier_ids_to_choose.keys() "
-                / ", roll_ranges.keys() do not have the same length!"
-            )
-
         modifiers = []
         for i, key in enumerate(distrubution):
             n_modifiers_to_create: int = random.choice(distrubution[key])
+
             remove_chosen_modifiers_from_pool: bool = not can_duplicate[i]
 
             modifier_ids_to_choose_from: list = modifier_ids_to_choose[key].copy()
@@ -321,10 +346,7 @@ class DataDepositTestDataCreator:
                 chosen_modifier_ids = modifier_ids_to_choose_from[choice_made]
                 chosen_rolls = complementing_roll_ranges[choice_made]
 
-                effect: str = self.db_modifier_df.loc[
-                    self.db_modifier_df["modifierId"] == chosen_modifier_ids[0],
-                    "effect",
-                ].tolist()[0]
+                effect = self.modifier_ids_to_effect_map[tuple(chosen_modifier_ids)]
 
                 if chosen_rolls:
                     # equivalent to modifier not being static
@@ -351,6 +373,7 @@ class DataDepositTestDataCreator:
         )
         base_type = random.choice(template["base_types"])
         modifiers = self._choose_and_make_modifiers(template)
+
         item_dict = {
             "verified": True,
             "name": name,
