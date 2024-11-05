d939b8a99fff211a2d7d7d165d3c70ea80559db4
Bogadisa <magnus@hoddevik.eu>
2024-11-05 12:50:55 +0100

    Requested changes #619

--- src/frontend/src/hooks/getData/prefetchGroupedModifiers.tsx
+++ src/frontend/src/hooks/getData/prefetchGroupedModifiers.tsx
@@ -4,21 +4,20 @@ import { ModifiersService } from "../../client";
 export const useGetGroupedModifiers = async () => {
     const queryClient = useQueryClient();
 
-        try {
-            // Fetch and return the grouped modifiers directly
-            const data = await queryClient.fetchQuery({
-                queryKey: ["prefetchedGroupedModifiers"],
-                queryFn: async () => {
-                    const fetchedData = await ModifiersService.getGroupedModifierByEffect();
-                    return Array.isArray(fetchedData) ? fetchedData : [fetchedData];
-                },
-            });
+    try {
+        // Fetch and return the grouped modifiers directly
+        const groupedModifiers = await queryClient.fetchQuery({
+            queryKey: ["prefetchedGroupedModifiers"],
+            queryFn: async () => {
+                const fetchedData =
+                    await ModifiersService.getGroupedModifierByEffect();
+                return Array.isArray(fetchedData) ? fetchedData : [fetchedData];
+            },
+        });
 
-            return { groupedModifiers: data };
-
-        } catch (error) {
-            console.log(error);
-            return { groupedModifiers: [] }; // Return an empty array on error
-        }
+        return { groupedModifiers };
+    } catch (error) {
+        console.log(error);
+        return { groupedModifiers: [] }; // Return an empty array on error
+    }
 };
-
