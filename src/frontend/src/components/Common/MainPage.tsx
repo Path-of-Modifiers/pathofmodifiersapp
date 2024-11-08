42297f7015b65a98815e0c4d03efbc7e35e9f72f
Bogadisa <magnus@hoddevik.eu>
2024-11-08 15:24:45 +0100

    #626 Moved main page to its own component

---
+++ src/frontend/src/components/Common/MainPage.tsx
@@ -0,0 +1,65 @@
+import Header from "../../components/Common/Header";
+import QueryButtons from "../../components/Common/QueryButtons";
+import { GraphInput } from "../../components/Input/GraphInput";
+import GraphComponent from "../../components/Graph/GraphComponent";
+import Footer from "../../components/Common/Footer";
+
+import { Flex, Box, VStack } from "@chakra-ui/layout";
+
+interface MainPageProps {
+    isReady: boolean;
+}
+
+export const MainPage = (props: MainPageProps) => {
+    return (
+        <Flex
+            direction="column"
+            bg="ui.main"
+            width="99vw"
+            minWidth="bgBoxes.miniPBox"
+        >
+            <>
+                <Box mb={"7rem"}>
+                    <Header />
+                </Box>
+
+                <Flex
+                    direction="row"
+                    bg="ui.secondary"
+                    justifyContent="center"
+                    maxWidth={"100%"}
+                    flexWrap="wrap"
+                    minHeight="100vh"
+                    p={3}
+                    pl={10}
+                    pr={10}
+                    borderTopRadius={10}
+                    borderTopColor={"ui.darkBrown"}
+                    borderTopWidth={1}
+                    alignSelf="center"
+                >
+                    {props.isReady && (
+                        <VStack
+                            width={"bgBoxes.defaultBox"}
+                            height={"100%"}
+                            maxW={"100%"}
+                        >
+                            <GraphInput />
+                            <QueryButtons />
+
+                            <GraphComponent
+                                width={"bgBoxes.mediumBox"}
+                                minH={"bgBoxes.smallBox"}
+                                height={"bgBoxes.smallBox"}
+                                maxW="98vw"
+                                mb="10rem"
+                                justifyItems={"center"}
+                            />
+                            <Footer />
+                        </VStack>
+                    )}
+                </Flex>
+            </>
+        </Flex>
+    );
+};
