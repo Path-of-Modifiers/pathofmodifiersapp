57c9b8702b40928eec7f452ffaff796b49e2058b
Ivareh <ivareh@ntnu.no>
2024-11-12 18:28:34 +0100

    #604 Update models with new layout for hypertables

--- src/backend_api/app/core/models/models.py
+++ src/backend_api/app/core/models/models.py
@@ -2,16 +2,18 @@ import uuid
 from datetime import datetime
 
 from sqlalchemy import (
+    String,
+    Index,
     BigInteger,
     Boolean,
     CheckConstraint,
     DateTime,
     Float,
     ForeignKey,
-    ForeignKeyConstraint,
     Identity,
     SmallInteger,
-    String,
+    Integer,
+    Text,
     UniqueConstraint,
     func,
 )
@@ -24,72 +26,87 @@ from app.core.models.database import Base
 class Currency(Base):
     __tablename__ = "currency"
 
-    currencyId: Mapped[int] = mapped_column(
-        BigInteger, Identity(), primary_key=True, index=True, nullable=False
-    )
-    tradeName: Mapped[str] = mapped_column(String, index=True, nullable=False)
+    currencyId: Mapped[int] = mapped_column(Integer, Identity(), primary_key=True)
+    createdHoursSinceLaunch: Mapped[int] = mapped_column(SmallInteger, nullable=False)
     valueInChaos: Mapped[float] = mapped_column(Float(4), nullable=False)
-    iconUrl: Mapped[str] = mapped_column(String, nullable=False)
-    createdAt: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=func.now(), nullable=False
-    )
+    tradeName: Mapped[str] = mapped_column(Text, nullable=False)
 
 
 class ItemBaseType(Base):
     __tablename__ = "item_base_type"
 
-    baseType: Mapped[str] = mapped_column(
-        String, primary_key=True, nullable=False, index=True
+    itemBaseTypeId: Mapped[int] = mapped_column(
+        SmallInteger,
+        Identity(start=1, increment=1, cycle=True),
+        primary_key=True,
     )
-    category: Mapped[str] = mapped_column(String, nullable=False)
-    subCategory: Mapped[str | None] = mapped_column(String)
-    relatedUniques: Mapped[str | None] = mapped_column(String)
+    baseType: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+        unique=True,
+    )
+    category: Mapped[str] = mapped_column(Text, nullable=False)
+    subCategory: Mapped[str | None] = mapped_column(Text)
+    relatedUniques: Mapped[str | None] = mapped_column(Text)
 
 
 class Item(Base):
+    # Hypertable
+    # For hypertable specs, see alembic revision `cc29b89156db'
     __tablename__ = "item"
 
+    name: Mapped[str | None] = mapped_column(Text, nullable=False)
+    itemBaseTypeId: Mapped[int] = mapped_column(
+        SmallInteger,
+        ForeignKey(
+            "item_base_type.itemBaseTypeId", ondelete="RESTRICT", onupdate="CASCADE"
+        ),
+        nullable=False,
+    )
+    createdHoursSinceLaunch: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    league: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
     itemId: Mapped[int] = mapped_column(
         BigInteger,
         Identity(start=1, increment=1, cycle=True),
-        primary_key=True,
+        primary_key=True,  # Dummy pk, will be removed during hypertable creation
+    )
+    currencyId: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("currency.currencyId", ondelete="RESTRICT"),
         index=True,
         nullable=False,
     )
-    name: Mapped[str | None] = mapped_column(String, index=True)
-    league: Mapped[str] = mapped_column(
-        String, nullable=False
-    )  # TODO: Add index when we add more leagues
-    baseType: Mapped[str] = mapped_column(
-        String,
-        ForeignKey("item_base_type.baseType", ondelete="RESTRICT", onupdate="CASCADE"),
-        nullable=False,
-        index=True,
-    )
-    typeLine: Mapped[str] = mapped_column(String, nullable=False)
     ilvl: Mapped[int] = mapped_column(SmallInteger, nullable=False)
-    rarity: Mapped[str] = mapped_column(String, nullable=False)
+    prefixes: Mapped[int | None] = mapped_column(SmallInteger)
+    suffixes: Mapped[int | None] = mapped_column(SmallInteger)
+    foilVariation: Mapped[int | None] = mapped_column(SmallInteger)
+    currencyAmount: Mapped[float] = mapped_column(Float(4), nullable=False)
     identified: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    currencyAmount: Mapped[float | None] = mapped_column(Float(4))
-    currencyId: Mapped[int | None] = mapped_column(
-        BigInteger, ForeignKey("currency.currencyId", ondelete="RESTRICT")
-    )
     corrupted: Mapped[bool | None] = mapped_column(Boolean)
     delve: Mapped[bool | None] = mapped_column(Boolean)
     fractured: Mapped[bool | None] = mapped_column(Boolean)
     synthesised: Mapped[bool | None] = mapped_column(Boolean)
     replica: Mapped[bool | None] = mapped_column(Boolean)
-    influences: Mapped[dict[str, str] | None] = mapped_column(
-        JSONB
-    )  # elder, shaper, warlord etc
     searing: Mapped[bool | None] = mapped_column(Boolean)
     tangled: Mapped[bool | None] = mapped_column(Boolean)
     isRelic: Mapped[bool | None] = mapped_column(Boolean)
-    prefixes: Mapped[int | None] = mapped_column(SmallInteger)
-    suffixes: Mapped[int | None] = mapped_column(SmallInteger)
-    foilVariation: Mapped[int | None] = mapped_column(SmallInteger)
-    createdAt: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=func.now(), nullable=False
+    typeLine: Mapped[str] = mapped_column(Text, nullable=False)
+    rarity: Mapped[str] = mapped_column(Text, nullable=False)
+    influences: Mapped[dict[str, str] | None] = mapped_column(
+        JSONB
+    )  # elder, shaper, warlord etc
+
+    __table_args__ = (
+        Index(
+            "ix_item_name_itemBaseTypeId_createdHoursSinceLaunch_league",
+            "name",
+            "itemBaseTypeId",
+            "createdHoursSinceLaunch",
+            "league",
+        ),
     )
 
 
@@ -101,20 +118,13 @@ class Modifier(Base):
     __tablename__ = "modifier"
 
     modifierId: Mapped[int] = mapped_column(
-        BigInteger,
+        SmallInteger,
         Identity(start=1, increment=1, cycle=True),
-        nullable=False,
-        index=True,
         primary_key=True,
     )
-    position: Mapped[int] = mapped_column(SmallInteger, nullable=False, index=True)
-    relatedUniques: Mapped[str | None] = mapped_column(String)
+    position: Mapped[int] = mapped_column(SmallInteger, nullable=False)
     minRoll: Mapped[float | None] = mapped_column(Float(4))
     maxRoll: Mapped[float | None] = mapped_column(Float(4))
-    textRolls: Mapped[str | None] = mapped_column(String)
-    static: Mapped[bool | None] = mapped_column(Boolean)
-    effect: Mapped[str] = mapped_column(String, nullable=False)
-    regex: Mapped[str | None] = mapped_column(String)
     implicit: Mapped[bool | None] = mapped_column(Boolean)
     explicit: Mapped[bool | None] = mapped_column(Boolean)
     delve: Mapped[bool | None] = mapped_column(Boolean)
@@ -124,6 +134,11 @@ class Modifier(Base):
     corrupted: Mapped[bool | None] = mapped_column(Boolean)
     enchanted: Mapped[bool | None] = mapped_column(Boolean)
     veiled: Mapped[bool | None] = mapped_column(Boolean)
+    static: Mapped[bool | None] = mapped_column(Boolean)
+    effect: Mapped[str] = mapped_column(Text, nullable=False)
+    relatedUniques: Mapped[str | None] = mapped_column(Text)
+    textRolls: Mapped[str | None] = mapped_column(Text)
+    regex: Mapped[str | None] = mapped_column(Text)
     createdAt: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), default=func.now(), nullable=False
     )
@@ -184,28 +199,34 @@ class Modifier(Base):
 
 class ItemModifier(Base):
     # Hypertable
-    # Segments by modifierId and createdAt
+    # For hypertable specs, see alembic revision `cc29b89156db'
+
     __tablename__ = "item_modifier"
 
-    itemId: Mapped[int] = mapped_column(
+    modifierId: Mapped[int] = mapped_column(
         BigInteger,
-        ForeignKey("item.itemId", ondelete="CASCADE", onupdate="CASCADE"),
-        nullable=False,
-        primary_key=True,  # Dummy pk since we removing the pks on hypertable creation
-        index=True,
-    )
-    modifierId: Mapped[int] = mapped_column(BigInteger, nullable=False, index=True)
-    roll: Mapped[float | None] = mapped_column(Float(4))
-    createdAt: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=func.now(), nullable=False
-    )
-
-    __table_args__ = (
-        ForeignKeyConstraint(
-            [modifierId],
-            ["modifier.modifierId"],
+        ForeignKey(
+            "modifier.modifierId",
             ondelete="CASCADE",
             onupdate="CASCADE",
+        ),
+        nullable=False,
+    )
+    createdHoursSinceLaunch: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    itemId: Mapped[int] = mapped_column(
+        BigInteger,
+        nullable=False,
+    )
+    roll: Mapped[float | None] = mapped_column(
+        Float(4),
+        primary_key=True,  # Dummy pk since we removing the pks on hypertable creation Doesn't have FK to item, since both are hypertable
+    )
+    __table_args__ = (
+        Index(
+            "ix_item_modifierId_createdHoursSinceLaunch_itemId",
+            "modifierId",
+            "createdHoursSinceLaunch",
+            "itemId",
         ),
     )
 
@@ -213,24 +234,25 @@ class ItemModifier(Base):
 class User(Base):
     __tablename__ = "pom_user"
 
-    userId: Mapped[UUID] = mapped_column(
+    userId: Mapped[UUID] = mapped_column(  # type: ignore
         UUID(as_uuid=True),
         primary_key=True,
-        index=True,
         default=uuid.uuid4,
         nullable=False,
     )
     username: Mapped[str] = mapped_column(
-        String, unique=True, index=True, nullable=False
+        String(30), unique=True, index=True, nullable=False
     )
-    hashedPassword: Mapped[str] = mapped_column(String, nullable=False)
-    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
-    isActive: Mapped[bool | None] = mapped_column(Boolean)
-    isSuperuser: Mapped[bool | None] = mapped_column(Boolean)
+    email: Mapped[str] = mapped_column(
+        String(255), unique=True, index=True, nullable=False
+    )
     rateLimitTier: Mapped[int | None] = mapped_column(
         SmallInteger
     )  # 0 = basic limit usage
+    isActive: Mapped[bool | None] = mapped_column(Boolean)
+    isSuperuser: Mapped[bool | None] = mapped_column(Boolean)
     isBanned: Mapped[bool | None] = mapped_column(Boolean)
+    hashedPassword: Mapped[str] = mapped_column(String, nullable=False)
     createdAt: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), default=func.now(), nullable=False
     )
