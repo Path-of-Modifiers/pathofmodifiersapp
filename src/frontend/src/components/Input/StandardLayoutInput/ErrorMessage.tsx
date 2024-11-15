35aeff1701f61075a323acee94a559a5700555ae
Bogadisa <magnus@hoddevik.eu>
2024-11-15 14:01:21 +0100

    #246 Made it easier to customize error message

--- src/frontend/src/components/Input/StandardLayoutInput/ErrorMessage.tsx
+++ src/frontend/src/components/Input/StandardLayoutInput/ErrorMessage.tsx
@@ -3,18 +3,29 @@ import {
   AlertDescription,
   AlertIcon,
   AlertTitle,
+  AlertProps,
 } from "@chakra-ui/alert";
+import { IconBaseProps } from "react-icons/lib";
 
 interface ErrorMessageProps {
   alertTitle: string;
   alertDescription: string;
+  alertIcon?: React.ElementType;
+  iconProps?: IconBaseProps;
+  alertProps?: AlertProps;
 }
 
 export const ErrorMessage = (props: ErrorMessageProps) => {
+  let Icon;
+  if (props.alertIcon != null) {
+    Icon = props.alertIcon;
+  } else {
+    Icon = AlertIcon;
+  }
   return (
     <>
-      <Alert status="error">
-        <AlertIcon />
+      <Alert {...props.alertProps} status="error">
+        <Icon {...props.iconProps} />
         <AlertTitle>{props.alertTitle}</AlertTitle>
         <AlertDescription>{props.alertDescription}</AlertDescription>
       </Alert>
