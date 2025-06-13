1d758a3ea6939163f376aeb478a165224c17d3c3
Bogadisa <magnus@hoddevik.eu>
2025-06-13 11:31:42 +0200

    #27 Updated the frontend api schemas and models

---
+++ src/frontend/src/client/models/UnidentifiedItem.ts
@@ -0,0 +1,19 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type UnidentifiedItem = {
+    name?: (string | null);
+    league: string;
+    itemBaseTypeId: number;
+    ilvl: number;
+    rarity: string;
+    identified?: boolean;
+    currencyAmount?: (number | null);
+    currencyId?: (number | null);
+    createdHoursSinceLaunch: number;
+    itemId: number;
+    nItems: number;
+    aggregated: boolean;
+};
+
--- src/frontend/src/client/services/LatestItemIdService.ts
+++ src/frontend/src/client/services/LatestItemIdService.ts
@@ -20,4 +20,18 @@ export class LatestItemIdService {
             url: '/api/api_v1/item/latest_item_id/',
         });
     }
+    /**
+     * Get Latest Item Id
+     * Get the latest "itemId"
+     *
+     * Can only be used safely on an empty table or directly after an insertion.
+     * @returns any Successful Response
+     * @throws ApiError
+     */
+    public static getLatestItemId1(): CancelablePromise<(number | null)> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/api_v1/unidentifiedItem/latest_item_id/',
+        });
+    }
 }
