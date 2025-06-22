2e15b08e146cd3ef03692b6faff404867a60471c
Bogadisa <magnus@hoddevik.eu>
2025-06-22 02:29:30 +0200

    #729 Temporary process plotting data file

--- src/frontend/src/hooks/graphing/processPlottingData.tsx
+++ src/frontend/src/hooks/graphing/processPlottingData.tsx
@@ -31,40 +31,40 @@ function useGetPlotData(plotQuery: PlotQuery): {
   }, [isError, isFetched, error, showToast]);
 
   if (plotData !== undefined) {
-    const cleanData: Datum[] = [];
-    const upperBoundryChaos = findWinsorUpperBound(plotData.valueInChaos);
-    const upperBoundrySecondary = findWinsorUpperBound(
-      plotData.valueInMostCommonCurrencyUsed
-    );
-    for (let i = 0; i < plotData?.hoursSinceLaunch.length; i++) {
-      cleanData.push({
-        timestamp: plotData.hoursSinceLaunch[i],
-        valueInChaos: plotData.valueInChaos[i],
-        valueInMostCommonCurrencyUsed:
-          plotData.valueInMostCommonCurrencyUsed[i],
-        confidence: plotData.confidence[i],
-      });
-    }
-    const data: Datum[] = cleanData.reduce((prev, curVal) => {
-      if (prev.length === 0) {
-        return [curVal];
-      }
-      const prevVal = prev[prev.length - 1];
-      const hourGap = curVal.timestamp - prevVal.timestamp;
-      if (hourGap > 1) {
-        const gapFiller: Datum[] = [];
-        for (let j = 0; j < hourGap; j++) {
-          gapFiller.push({
-            timestamp: prevVal.timestamp + j + 1,
-            valueInChaos: null,
-            valueInMostCommonCurrencyUsed: null,
-            confidence: null,
-          });
-        }
-        return [...prev, ...gapFiller, curVal];
-      }
-      return [...prev, curVal];
-    }, [] as Datum[]);
+    // const cleanData: Datum[] = [];
+    // const upperBoundryChaos = findWinsorUpperBound(plotData.valueInChaos);
+    // const upperBoundrySecondary = findWinsorUpperBound(
+    //   plotData.valueInMostCommonCurrencyUsed
+    // );
+    // for (let i = 0; i < plotData?.hoursSinceLaunch.length; i++) {
+    //   cleanData.push({
+    //     timestamp: plotData.hoursSinceLaunch[i],
+    //     valueInChaos: plotData.valueInChaos[i],
+    //     valueInMostCommonCurrencyUsed:
+    //       plotData.valueInMostCommonCurrencyUsed[i],
+    //     confidence: plotData.confidence[i],
+    //   });
+    // }
+    // const data: Datum[] = cleanData.reduce((prev, curVal) => {
+    //   if (prev.length === 0) {
+    //     return [curVal];
+    //   }
+    //   const prevVal = prev[prev.length - 1];
+    //   const hourGap = curVal.timestamp - prevVal.timestamp;
+    //   if (hourGap > 1) {
+    //     const gapFiller: Datum[] = [];
+    //     for (let j = 0; j < hourGap; j++) {
+    //       gapFiller.push({
+    //         timestamp: prevVal.timestamp + j + 1,
+    //         valueInChaos: null,
+    //         valueInMostCommonCurrencyUsed: null,
+    //         confidence: null,
+    //       });
+    //     }
+    //     return [...prev, ...gapFiller, curVal];
+    //   }
+    //   return [...prev, curVal];
+    // }, [] as Datum[]);
     return {
       result: data,
       mostCommonCurrencyUsed: plotData.mostCommonCurrencyUsed,
